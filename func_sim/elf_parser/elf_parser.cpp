364960e63f4420a290c0b2a0f47aa45252a2c9a6
yuri.a.samarin <yuri.a.samarin>
2012-11-12 15:57:09 +0000

    [branch] elf_parser has been completed

--- func_sim/elf_parser/elf_parser.cpp
+++ func_sim/elf_parser/elf_parser.cpp
@@ -126,23 +126,39 @@ void ElfSection::extractSectionParams( Elf* elf, const char* section_name,
 
 uint64 ElfSection::read( uint64 addr, short num_of_bytes) const
 {
-    // insert here your implementation
-    assert(0);
-    return NO_VAL64; 
+    assert( num_of_bytes < sizeof( uint64));
+    assert( isInside( addr, num_of_bytes));
+    uint64 data = 0;
+    for ( short i = num_of_bytes; i > 0; --i)
+    {
+        short the_num_of_byte = addr - this->start_addr + i - 1;
+        data <<= 8;
+        data |= this->content[the_num_of_byte];
+    }
+    cout << "\n\nit my debug print!\ndata=" << hex << data << endl; 
+    return data; 
 }
 
 bool ElfSection::isInside( uint64 addr, short num_of_bytes) const
 {
-    // insert here your implementation
-    assert(0);
+    cout << "\nstart_addr = " << start_addr << "\naddr = "
+         << addr << endl;
+    assert( num_of_bytes);
+    if (this->start_addr > addr)
+    {
+        return false;
+    }
+    if ( this->start_addr + this->size >= addr + num_of_bytes)
+    {
+        return true;
+    }
     return false;
 }
 
 uint64 ElfSection::startAddr() const
 {
-    // insert here your implementation
-    assert(0);
-    return NO_VAL64;
+    assert(start_addr);
+    return start_addr;
 }
 
 string ElfSection::dump( string indent) const
