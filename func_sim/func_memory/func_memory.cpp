2d09284080b261880b2643e9b3bc30cb940d4d1b
petr.kurapov <petr.kurapov@gmail.com>
2013-11-27 23:08:46 +0000

    [trunk] Misscommit. Restoring trunk

--- func_sim/func_memory/func_memory.cpp
+++ func_sim/func_memory/func_memory.cpp
@@ -17,42 +17,23 @@ FuncMemory::FuncMemory( const char* executable_file_name,
                         uint64 page_bits,
                         uint64 offset_bits)
 {
-    //Parsing file
-    vector<ElfSection> section_array;
-    ElfSection::getAllElfSections( executable_file_name, section_array);
-    
-    //Filling map
-    for( int i = 0; i < section_array.size(); i++)
-    {
-        uint32 set_bits = addr_size - page_bits + offset_bits;
-        uint32 set = section_array[ i].start_addr >> ( page_bits + offset_bits);
-        uint32 page = section_array[ i].start_addr << set_bits;
-        page = page >> ( set_bits + offset_bits);
-        uint32 offset = section_array[ i].start_addr << ( set_bits + page_bits);
-        offset = offset >> ( set_bits + page_bits);
-
-        memory[set][page][offset].name = section_array[ i].name;
-        memory[set][page][offset].content = section_array[ i].content;
-
-    }
+    // put your code here
 }
 
 FuncMemory::~FuncMemory()
 {
-    //Cleaning memory
-    memory.clear();
+    // put your code here
 }
 
 uint64 FuncMemory::startPC() const
 {
-    
+    // put your code here
 }
 
 uint64 FuncMemory::read( uint64 addr, unsigned short num_of_bytes) const
 {
+    // put your code here
 
-    /*uint32 set_size = -1 >> page_bits + offset_bits;
-    uint32 page_size = -1 << set_size >> ( set_size + page_bits);*/
     return 0;
 }
 
@@ -63,24 +44,6 @@ void FuncMemory::write( uint64 value, uint64 addr, unsigned short num_of_bytes)
 
 string FuncMemory::dump( string indent) const
 {
-    string output;
-
-    /*for( int i = 0; i < memory.size(); i++)
-    {
-        for( int j = 0; j < memory.size(); j++)
-        {
-            for( int k = 0; k < memory.size(); k++)
-            {
-                if( memory[i][j][k]!= NULL)
-                {
-                    output+= "Dump ELF section \"%s\"\n";
-                    output+= "size = %d Bytes\n";
-                    output+= "start_addr = %x\n";
-                    output+= "Content:\n";
-                    output+= memory[i][j][k];
-                }
-            }
-        }
-    }*/
-    return output;
+    // put your code here
+    return string("ERROR: You need to implement FuncMemory!");
 }
