061d2b0880446e699d6bf35dbf04e0397775292b
AleksandrParamonov <aleksandr.paramonov@phystech.edu>
2015-11-24 21:50:46 +0300

    Alpha version
    -almost implemented func_instr.h

---
+++ func_sim/func_instr/func_instr.h
@@ -0,0 +1,140 @@
+#include <func_memory.h>
+using namespace std;
+class FuncInstr
+{
+public:
+    FuncInstr(uint32 bytes);
+    ~FuncInstr();
+    string Dump(std::string indent = " ") const;
+
+    enum Type
+    {
+        ADD = 0,
+        ADDU,
+        SUB,
+        SUBU,
+        ADDI,
+        ADDIU,
+        MULT,
+        MULTU,
+        DIV,
+        DIVU,
+        MFHI,
+        MTHI,
+        MFLO,
+        MTLO,
+        SLL,
+        SRL,
+        SRA,
+        SLLV,
+        SRLV,
+        SRAV,
+        LUI,
+        SLT,
+        SLTU,
+        SLTI,
+        SLTIU,
+        AND,
+        OR,
+        XOR,
+        NOR,
+        ANDI,
+        ORI,
+        XORI,
+        BEQ,
+        BNE,
+        BLEZ,
+        BGTZ,
+        J,
+        JAL, JR,
+        JALR,
+        LB,
+        LH,
+        LW,
+        LBU,
+        LHU,
+        SB,
+        SH,
+        SW,
+        SYSCALL,
+        BREAK,
+        TRAP,
+        NO_TYPE
+    } type;
+
+
+    enum FormatType
+    {
+        FORMAT_I,
+        FORMAT_J,
+        FORMAT_R
+    } format;
+
+    enum Registers
+    {
+        ZERO = 0,
+        AT,
+        V0, V1,
+        A0, A1, A2, A3,
+        T0, T1, T2, T3, T4, T5, T6, T7,
+        S0, S1, S2, S3, S4, S5, S6, S7,
+        T8, T9,
+        K0, K1,
+        GP,
+        SP,
+        S8,
+        RA,
+        REG_COUNT
+    };
+
+    union
+    {
+        struct
+        {
+            unsigned imm : 16;
+            unsigned rt : 5;
+            unsigned rs : 5;
+            unsigned op : 6
+        }asI;
+        struct
+        {
+            unsigned offset : 26;
+            unsigned op : 6
+        }asJ;
+        struct
+        {
+            unsigned funct : 6;
+            unsigned shamt : 5;
+            unsigned rd : 5;
+            unsigned rt : 5;
+            unsigned rs : 5;
+            unsigned op : 6
+        }asR;
+        uint32 raw;
+
+    } bytes;
+
+    struct ISAEntry
+    {
+        const char* name;
+
+        uint8 opcode;
+        uint8 func;
+
+        FuncInstr::FormatType format;
+        FuncInstr::Type type;
+    };
+
+    static const ISAEntry[] isaTable;
+    static const ISAEntry[] isaTable =
+        // name   opcode    func      format              type
+    { "add  ", 0x0,     0x20, FuncInstr::FORMAT_R, FuncInstr::ADD /*...*/ },
+        // more instructions ...
+
+
+
+    void ParseI(uint32 bytes);
+    void ParseJ(uint32 bytes);
+    void ParseR(uint32 bytes);
+
+};
