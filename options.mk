74e5338caff272e84921413201cacdc859d7dd47
George Korepanov <korepanov.gm@phystech.edu>
2017-04-05 18:35:39 +0300

    Added root Makefile with compiler and build options

---
+++ options.mk
@@ -0,0 +1,22 @@
+# C++ compiler flags
+CXXFLAGS= -std=c++11
+
+# Paths to look for headers
+vpath %.h $(TRUNK)/common
+vpath %.h $(TRUNK)/func_sim/
+vpath %.h $(TRUNK)/func_sim/elf_parser/
+vpath %.h $(TRUNK)/func_sim/func_instr/
+vpath %.h $(TRUNK)/func_sim/func_memory/
+vpath %.cpp $(TRUNK)/func_sim/
+vpath %.cpp $(TRUNK)/func_sim/elf_parser/
+vpath %.cpp $(TRUNK)/func_sim/func_instr/
+vpath %.cpp $(TRUNK)/func_sim/func_memory/
+
+# GoogleTest directories
+GTEST_DIR= $(TRUNK)/libs/googletest
+GTEST_INCL= -I $(GTEST_DIR)/lib/include
+GTEST_LIB= $(GTEST_DIR)/lib/libgtest.a
+
+# option for C++ compiler specifying directories 
+# to search for headers
+INCL= -I ./ -I $(TRUNK)/common/ -I $(TRUNK)/func_sim/elf_parser/ -I $(TRUNK)/func_sim/func_memory/ -I $(TRUNK)/func_sim/func_instr
