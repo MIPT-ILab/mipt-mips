edec322fd68c5f68a1912e73f951526b9e8cd650
Pavel Kryukov <kryukov@frtk.ru>
2017-04-09 14:16:10 +0300

    Move implementations to perf_sim.cpp

--- perf_sim/perf_sim.cpp
+++ perf_sim/perf_sim.cpp
@@ -65,6 +65,157 @@ void PerfMIPS::run( const std::string& tr, uint32 instrs_to_run)
     delete mem;
 }
 
+void PerfMIPS::clock_fetch( int cycle) {
+    sout << "fetch   cycle " << std::dec << cycle << ":";
+
+    bool is_stall = false;
+    rp_decode_2_fetch_stall->read( &is_stall, cycle);
+    if ( is_stall) 
+    {
+        sout << "bubble\n";
+        return;
+    }
+   
+    if (PC_is_valid)
+    {
+        uint32 module_data = mem->read(PC);
+        wp_fetch_2_decode->write( module_data, cycle);
+        
+        sout << std::hex << "0x" << module_data << std::endl;
+    }
+    else
+    {
+        sout << "bubble\n";
+    }
+}
+
+void PerfMIPS::clock_decode( int cycle) {
+    sout << "decode  cycle " << std::dec << cycle << ":";
+    
+    bool is_stall = false;
+    rp_execute_2_decode_stall->read( &is_stall, cycle);
+    if ( is_stall) {
+        wp_decode_2_fetch_stall->write( true, cycle);
+   
+        sout << "bubble\n";
+        return;
+    }
+
+    bool is_anything_from_fetch = rp_fetch_2_decode->read( &decode_data, cycle);
+    
+    FuncInstr instr( decode_data, PC);
+   
+    if ( instr.isJump() && is_anything_from_fetch)
+        PC_is_valid = false;
+
+    if ( !is_anything_from_fetch && !decode_next_time)
+    {
+        sout << "bubble\n";
+        return;
+    }
+
+    if ( rf->check( instr.get_src1_num()) && rf->check( instr.get_src2_num()))
+    {
+        rf->read_src1( instr);
+        rf->read_src2( instr);
+        rf->invalidate( instr.get_dst_num());
+        wp_decode_2_execute->write( instr, cycle);
+        
+        decode_next_time = false;
+        
+        if (!instr.isJump())
+            PC += 4;
+
+        sout << instr << std::endl;
+    }   
+    else
+    {
+        wp_decode_2_fetch_stall->write( true, cycle);
+        decode_next_time = true;
+        sout << "bubble\n";
+    }
+}
+
+void PerfMIPS::clock_execute( int cycle)
+{
+    std::ostringstream oss;
+    sout << "execute cycle " << std::dec << cycle << ":";
+
+    bool is_stall = false;
+    rp_memory_2_execute_stall->read( &is_stall, cycle);
+    if ( is_stall)
+    {
+        wp_execute_2_decode_stall->write( true, cycle);
+        
+        sout << "bubble\n";
+        return;
+    }
+
+    FuncInstr instr;
+    if ( !rp_decode_2_execute->read( &instr, cycle))
+    {
+        sout << "bubble\n";
+        return;
+    }
+
+    instr.execute();
+    wp_execute_2_memory->write( instr, cycle);
+
+    sout << instr << std::endl;
+}
+
+void PerfMIPS::clock_memory( int cycle)
+{
+    sout << "memory  cycle " << std::dec << cycle << ":"; 
+
+    bool is_stall = false;
+    rp_writeback_2_memory_stall->read( &is_stall, cycle);
+    if ( is_stall)
+    {
+        wp_memory_2_execute_stall->write( true, cycle);
+        sout << "bubble\n";
+        return;
+    }
+
+    FuncInstr instr;
+    if ( !rp_execute_2_memory->read( &instr, cycle))
+    {
+        sout << "bubble\n";
+        return;
+    }
+
+    load_store(instr);
+    wp_memory_2_writeback->write( instr, cycle);
+
+    sout << instr << std::endl;
+}
+
+void PerfMIPS::clock_writeback( int cycle)
+{
+    sout << "wb      cycle " << std::dec << cycle << ":"; 
+
+    FuncInstr instr;
+    if ( !rp_memory_2_writeback->read( &instr, cycle))
+    {
+        sout << "bubble\n";
+        return;
+    }
+
+    if ( instr.isJump())
+    {
+        PC_is_valid = true;
+        PC = instr.get_new_PC();
+    }
+
+    rf->write_dst( instr);
+
+    sout << instr << std::endl;
+    std::cout << instr << std::endl;
+
+    ++executed_instrs;
+    last_writeback_cycle = cycle;
+}
+
 PerfMIPS::~PerfMIPS() {
     delete rf;
 
