2995ccc8e6627109a7c986d2030d6d0d6b22e7d9
Pavel Kryukov <kryukov@frtk.ru>
2017-04-15 14:15:42 +0300

    Improve Config::Value functionality to define new options in one line

--- perf_sim/config.cpp
+++ perf_sim/config.cpp
@@ -9,55 +9,33 @@
 /* Generic C++ */
 #include <iostream>
 
-/* boost - program option parsing */
-#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
-/* constructor and destructor */
-Config::Config()
+std::list<Config::BasicValue*> Config::BasicValue::values;
+
+template<>
+void Config::Value<bool>::reg(bod& d)
 {
-
+    namespace po = boost::program_options;
+    d.add_options()(name.c_str(),
+                    po::bool_switch( &value)->default_value( default_value),
+                    desc.c_str());
 }
-
-Config::~Config()
-{
-
-}
-
 
 /* basic method */
 int Config::handleArgs( int argc, char** argv)
 {
-    namespace po = boost::program_options;
-
     po::options_description description( "Allowed options");
 
-    description.add_options()
-        ( "binary,b",      po::value<std::string>( this->binary_filename.get_ptr())->required(),
-          "input binary file")
+    for ( Config::BasicValue* value : Config::BasicValue::get_values())
+         value->reg(description);
 
-        ( "numsteps,n",    po::value<int>( this->num_steps.get_ptr())->required(),
-          "number of instructions to run")
-
-        /* by default, the silent mode is on */
-        ( "disassembly,d", po::bool_switch( this->disassembly_on.get_ptr())->default_value( false),
-          "print disassembly")
-
-        /* by default, functional simulator is run */
-        ( "functional-only,f", po::bool_switch( this->functional_only.get_ptr())->default_value( false),
-          "run functional simulation only")
-
-        ( "help,h",
-          "print this help message");
-
-    po::positional_options_description posDescription;
-    posDescription.add( "binary", 1).add( "numsteps", 2);
     po::variables_map vm;
 
     try
     {
         po::store(po::command_line_parser(argc, argv).
                                     options(description).
-                                    positional(posDescription).
                                     run(),
                                     vm);
 
--- perf_sim/config.h
+++ perf_sim/config.h
@@ -7,30 +7,81 @@
 #define CONFIG_H
 
 #include <string>
+#include <list>
+
+/* boost - program option parsing */
+#include <boost/program_options.hpp>
+
+using bod = boost::program_options::options_description;
 
 class Config
 {
-    template<typename T>
-    class Value {
-        T value;
-        T* get_ptr() { return &value; }
+    class BasicValue {
         friend class Config;
+        static std::list<BasicValue*> values;
+        virtual void reg( bod& d) = 0;
     public:
-        operator const T&() const { return value; }
+        static std::list<BasicValue*>& get_values() { return values; }
+        BasicValue() { values.push_back( this); }
     };
+
+    template<typename T>
+    class Value : public BasicValue {
+        const std::string name;
+        const std::string desc;
+        const T default_value;
+        const bool is_required;
+
+        T value;
+
+
+        Value<T>() = delete;
+    public:
+        Value<T>( const char* name, const T& val, const char* desc, bool is_req = false)
+            : BasicValue()
+            , name( name)
+            , desc( desc)
+            , default_value( val)
+            , is_required( is_req)
+        { }
+
+        operator const T&() const { return value; }
+        void reg( bod&) final;
+    };
+
 public:
     /* variables */
-    Value<std::string> binary_filename;
-    Value<int>         num_steps;
-    Value<bool>        disassembly_on;
-    Value<bool>        functional_only;
+    Value<std::string> binary_filename = { "binary,b", "", "input binary file", true};
+    Value<int>         num_steps = { "numsteps,n", 1, "number of instructions to run", true};
+    Value<bool>        disassembly_on = { "disassembly,d", false, "print disassembly"};
+    Value<bool>        functional_only = { "functional-only,f", false, "run functional simulation only"};
 
     /* constructors */
-    Config();
-    ~Config();
+    Config()  { }
+    ~Config() { }
 
     /* methods */
     int handleArgs( int argc, char** argv);
 };
+
+template<>
+void Config::Value<bool>::reg(bod& d);
+
+template<typename T>
+void Config::Value<T>::reg(bod& d)
+{
+    namespace po = boost::program_options;
+    if (is_required)
+    {
+        d.add_options()(name.c_str(),
+                    po::value<T>( &value)->default_value( default_value)->required(),
+                    desc.c_str());
+    }
+    else {
+        d.add_options()(name.c_str(),
+                    po::value<T>( &value)->default_value( default_value),
+                    desc.c_str());
+    }
+}
 
 #endif  // CONFIG_H
