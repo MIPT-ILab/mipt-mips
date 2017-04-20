a7dc3f2be4477ad58e24efab824a5e0bfc17872c
Pavel Kryukov <kryukov@frtk.ru>
2017-04-20 22:36:45 +0300

    Keep all obj files in separate directory

--- options.mk
+++ options.mk
@@ -9,11 +9,6 @@ else
 endif
 
 # Paths to look for headers
-vpath %.h $(TRUNK)/common
-vpath %.h $(TRUNK)/func_sim/
-vpath %.h $(TRUNK)/func_sim/elf_parser/
-vpath %.h $(TRUNK)/func_sim/func_instr/
-vpath %.h $(TRUNK)/func_sim/func_memory/
 vpath %.cpp $(TRUNK)/common
 vpath %.cpp $(TRUNK)/common/ports
 vpath %.cpp $(TRUNK)/func_sim/
