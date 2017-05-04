390cc45f9d2515bfe8eb5f1bbfb0f777e8bc2195
Pavel Kryukov <kryukov@frtk.ru>
2017-05-04 23:51:21 +0300

    Use explicit paths for cpps in Makefile

--- simulator/options.mk
+++ simulator/options.mk
@@ -8,16 +8,6 @@ else
 	LDFLAGS = -flto
 endif
 
-# Paths to look for headers
-vpath %.cpp $(TRUNK)/infra
-vpath %.cpp $(TRUNK)/infra/ports
-vpath %.cpp $(TRUNK)/infra/config
-vpath %.cpp $(TRUNK)/infra/memory/
-vpath %.cpp $(TRUNK)/infra/cache/
-vpath %.cpp $(TRUNK)/infra/elf_parser/
-vpath %.cpp $(TRUNK)/mips/
-vpath %.cpp $(TRUNK)/func_sim/
-
 # GoogleTest directories
 GTEST_DIR= $(TRUNK)/../libs/googletest
 GTEST_INCL= -I $(GTEST_DIR)/lib/include
@@ -25,6 +15,6 @@ GTEST_LIB= $(GTEST_DIR)/lib/libgtest.a
 
 # option for C++ compiler specifying directories 
 # to search for headers
-INCL= -I $(TRUNK)/
+INCL= -I ./
 BOOST_INCL= -I /usr/local/include/boost/
 
