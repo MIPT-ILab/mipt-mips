016358ce4af83199aa682ea31a117f9db251cd2e
Pavel Kryukov <kryukov@frtk.ru>
2017-05-06 00:48:20 +0300

    Update options.mk

--- simulator/options.mk
+++ simulator/options.mk
@@ -12,7 +12,12 @@ ifeq ($(CXX), clang++)
 	CXXFLAGS+= -stdlib=libc++
 else ifeq ($(CXX), g++)
 	CXXVERSION:= $(shell g++ -dumpversion)
-	CXXFLAGS+=  --std=c++14
+	GCCVERSIONGTEQ4 := $(shell expr `g++ -dumpversion | cut -f1 -d.` \> 4)
+	ifeq "$(GCCVERSIONGTEQ4)" "1"
+		CXXFLAGS+=  --std=c++14
+	else
+		CXXFLAGS+=  --std=c++1y
+	endif
 endif
 
 LDFLAGS= # -static
