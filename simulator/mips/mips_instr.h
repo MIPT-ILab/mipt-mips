64ef2bb7234d2577a564d84813ae0da05b2a6d20
Pavel Kryukov <kryukov@frtk.ru>
2017-05-07 01:28:58 +0300

    Add FuncInstr::is_nop method

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -246,6 +246,7 @@ class FuncInstr
                                      operation == OUT_I_BRANCH; }
         bool is_load()  const { return operation == OUT_I_LOAD || operation == OUT_I_LOADU; }
         bool is_store() const { return operation == OUT_I_STORE; }
+        bool is_nop() const { return instr.raw == 0x0u; }
 
         void set_v_src1(uint32 value) { v_src1 = value; }
         void set_v_src2(uint32 value) { v_src2 = value; }
