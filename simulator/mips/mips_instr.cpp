bdafb3c18c2777a1421259281c1bf8cb7abaeeb4
Pavel Kryukov <kryukov@frtk.ru>
2017-05-16 20:18:10 +0300

    Update mips_instr.cpp

--- simulator/mips/mips_instr.cpp
+++ simulator/mips/mips_instr.cpp
@@ -279,6 +279,7 @@ void FuncInstr::initR()
             oss << " $" << regTableName(src1);
             break;
         case OUT_R_TRAP:
+            dst = REG_NUM_ZERO;
             src1 = static_cast<RegNum>(instr.asR.rs);
             src2 = static_cast<RegNum>(instr.asR.rt);
 
