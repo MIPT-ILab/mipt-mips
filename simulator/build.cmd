a3d3fc5d5310f65a19f81e2998fd30088a7ca79c
Pavel Kryukov <kryukov@frtk.ru>
2017-05-25 03:00:59 +0300

    Upload libelf.lib for MSVC build

--- simulator/build.cmd
+++ simulator/build.cmd
@@ -1,10 +1,14 @@
-cl /I. /EHsc libelf.lib /Femipt-mips ^
+cl /I. /EHsc ^
+   libelf.lib /D__LIBELF_INTERNAL__=1 ^
+   /Femipt-mips ^
    /W4 /WX /wd4505 /wd4244 /wd4996 ^
+   infra/elf_parser/elf_parser.cpp ^
    infra/memory/memory.cpp ^
    infra/config/config.cpp ^
    infra/ports/ports.cpp ^
    infra/cache/cache_tag_array.cpp ^
    mips/mips_instr.cpp ^
    func_sim/func_sim.cpp ^
-   perf_sim.cpp main.cpp ^
-   infra/elf_parser/elf_parser.cpp 
+   perf_sim.cpp ^
+   main.cpp ^
+
