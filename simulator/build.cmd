2b5c286fd954fe717cdf8e81060835c290066518
Pavel Kryukov <kryukov@frtk.ru>
2017-05-25 12:05:35 +0300

    Update build.cmd

--- simulator/build.cmd
+++ simulator/build.cmd
@@ -1,7 +1,6 @@
 cl /I. /EHsc ^
    ../libs/libelf.lib /D__LIBELF_INTERNAL__=1 ^
    /Femipt-mips ^
-   /DBOOST_ALL_NO_LIB ^
    /W4 /WX /wd4505 /wd4244 /wd4996 ^
    infra/elf_parser/elf_parser.cpp ^
    infra/memory/memory.cpp ^
