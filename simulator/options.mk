d6c1eb93c31f593a1b0f338a462859bf33113ccf
Pavel Kryukov <kryukov@frtk.ru>
2017-05-26 22:55:11 +0300

    Add googletest as submodule

--- simulator/options.mk
+++ simulator/options.mk
@@ -21,12 +21,6 @@ ifeq ($(UNAME), Msys)
     endif
 endif
 
-# GoogleTest directories
-GTEST_DIR:= ../libs/googletest
-GTEST_INCL:=  $(GTEST_DIR)/lib/include
-GTEST_LPATH:= $(GTEST_DIR)/lib
-GTEST_LIB:= $(GTEST_LPATH)/libgtest.a
-
 # Boost directories
 BOOST_INCL?=  /usr/local/include
 BOOST_LPATH?= /usr/local/lib
@@ -38,6 +32,6 @@ LIBELF_LPATH?=/usr/local/lib
 # option for C++ compiler specifying directories
 # to search for headers and libraries
 INCL+= -I. -isystem $(BOOST_INCL) -isystem $(LIBELF_INCL)
-LPATH:= -L $(BOOST_LPATH) -L $(GTEST_LPATH) -L $(LIBELF_LPATH)
+LPATH:= -L $(BOOST_LPATH) -L $(LIBELF_LPATH)
 
 TIDYFLAGS:=-header-filter=.* -checks=*,-google-readability-braces-around-statements,-readability-braces-around-statements,-cppcoreguidelines-pro-type-union-access,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-pro-type-vararg,-cppcoreguidelines-pro-bounds-constant-array-index,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-llvm-header-guard,-llvm-include-order,-modernize-pass-by-value
