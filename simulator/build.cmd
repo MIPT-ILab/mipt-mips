4830d2dd31f69a1626e01ff83944fb434103a74f
Pavel I. Kryukov <kryukov@frtk.ru>
2017-10-05 13:07:17 +0300

    Remove hacks for old versions of Boost

--- simulator/build.cmd
+++ simulator/build.cmd
@@ -9,7 +9,6 @@ del *.obj
 rem Build object files
 cl /I. /EHsc /c /nologo /MD ^
    /D__LIBELF_INTERNAL__=1 ^
-   /D_HAS_AUTO_PTR_ETC=1 ^
    /W4 /WX /wd4505 /wd4244 /wd4996 /wd4267 ^
    /std:c++17 ^
    infra/elf_parser/elf_parser.cpp ^
@@ -35,7 +34,7 @@ for %%G in (infra\elf_parser infra\memory mips func_sim bpu core) do (
     cd %%G\t
     cl /nologo unit_test.cpp %TRUNK%\*.obj %TRUNK%\..\libelf\lib\libelf.lib ^
        /EHsc /I %TRUNK%\..\googletest\googletest\include\ /I %TRUNK% /Fetest ^
-       /D_HAS_AUTO_PTR_ETC=1 /DGTEST_HAS_TR1_TUPLE=0 ^
+       /DGTEST_HAS_TR1_TUPLE=0 ^
        /std:c++17 ^
        /DTEST_PATH=\"%TRUNKX%\\..\\traces\\tt.core.out\" /MD || exit /b
     .\test.exe || exit /b
@@ -45,7 +44,6 @@ for %%G in (infra\elf_parser infra\memory mips func_sim bpu core) do (
 rem Build main.cpp
 cl /I. /EHsc /c /nologo /MD ^
    /D__LIBELF_INTERNAL__=1 ^
-   /D_HAS_AUTO_PTR_ETC=1 ^
    /W4 /WX /wd4505 /wd4244 /wd4996 /wd4267 ^
    /std:c++17 ^
    main.cpp || exit /b
