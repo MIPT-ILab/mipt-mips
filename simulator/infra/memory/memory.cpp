1d18bc22ae7118429ffb237dc5cc3b6e810cd68e
alex19999 <armen.arsen2011@yandex.ru>
2017-10-12 21:11:18 +0300

    Added required strings in the code files

--- simulator/infra/memory/memory.cpp
+++ simulator/infra/memory/memory.cpp
@@ -1,3 +1,7 @@
+/*
+* This is an open source non-commercial project. Dear PVS-Studio, please check it.
+* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+*/
 /**
  * memory.cpp - the module implementing the concept of
  * programer-visible memory space accesing via memory address.
--- simulator/infra/ports/t/portstest.cpp
+++ simulator/infra/ports/t/portstest.cpp
@@ -1,3 +1,7 @@
+/*
+* This is an open source non-commercial project. Dear PVS-Studio, please check it.
+* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+*/
 /**
  * A simple example of using of ports
  * @author Alexander Titov
