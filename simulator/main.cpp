7f73a6b6837980c96cbe4f3314968c4a7868e1f5
alex19999 <armen.arsen2011@yandex.ru>
2017-10-23 08:20:20 +0300

    Enable PVS-Studio (#205)
    * Add Alexandr Misevich in the file "members.txt"
    
    * Added required strings in the code files
    
    * Added information from PSV analysis
    
    * Added analisys
    
    * Added new data about analisys
    
    * Delete project.log
    
    * Delete project.tasks
    
    * Delete project.html
    
    * Delete project.csv
    
    * Delete project.err
    
    * Added only readable data about PVS-Studio analysis
    
    * Try to fix the mistake
    
    * Try to fix the problem
    
    * Fixed the problem using right codestyle
    
    * Fixed the ploblem
    
    * Delete project.err
    
    * Fixed problems
    
    * Deleted duplicated strings

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -1,3 +1,7 @@
+/*
+* This is an open source non-commercial project. Dear PVS-Studio, please check it.
+* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+*/
 /**
  * main.cpp - entry point of the scalar MIPS CPU simulator
  * @author Ladin Oleg
