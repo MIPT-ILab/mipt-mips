6b7ae1934b08c2fddb6e2dc5e4b7185bd356086d
denislos <de.loss.a.99@gmail.com>
2017-11-04 20:50:48 +0300

    Add mips/mips_memory.cpp

--- simulator/build.cmd
+++ simulator/build.cmd
@@ -18,6 +18,7 @@ cl /I. /EHsc /c /nologo /MD ^
    infra/ports/ports.cpp ^
    infra/cache/cache_tag_array.cpp ^
    mips/mips_instr.cpp ^
+   mips/mips_memory.cpp ^
    func_sim/func_sim.cpp ^
    core/perf_sim.cpp || exit /b
 
