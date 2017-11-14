8ab7fa1c3d55e6eb0b1304ca1e6def7810d64d2d
Pavel Kryukov <kryukov@frtk.ru>
2017-11-14 19:27:36 +0300

    Move all scripts to tools subfolder [skip appveyor]

--- simulator/tools/pvs.sh
+++ simulator/tools/pvs.sh
@@ -2,3 +2,4 @@
 make clean
 pvs-studio-analyzer trace -- make mipt-mips && pvs-studio-analyzer analyze -o ./project.log --compiler $CXX && plog-converter -a GA:1,2 -t tasklist -o ./project.tasks ./project.log
 cat ./project.tasks
+
