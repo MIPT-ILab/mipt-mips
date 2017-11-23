90c48f37669a1a17547703b0e28da5ee767bdf1b
YanLo <logovoya@tut.by>
2017-11-23 13:53:57 +0300

    movz and movn implemented

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -268,8 +268,8 @@ class FuncInstr
         void execute_ori()   { v_dst = v_src1 | zero_extend(v_imm); }
         void execute_xori()  { v_dst = v_src1 ^ zero_extend(v_imm); }
     
-        void execute_movn()  { execute_unknown(); }
-        void execute_movz()  { execute_unknown(); }
+        void execute_movn()  { if(v_src1 != 0) v_dst = v_src2; else v_dst = NO_VAL32;}
+        void execute_movz()  { if(v_src1 == 0) v_dst = v_src2; else v_dst = NO_VAL32;}
     
         void execute_tge()  { if ( ge() ) trap = TrapType::EXPLICIT_TRAP; }
         void execute_tgeu() { if ( geu()) trap = TrapType::EXPLICIT_TRAP; }
--- simulator/mips/mips_rf.h
+++ simulator/mips/mips_rf.h
@@ -82,9 +82,12 @@ class RF
 
         inline void write_dst( const FuncInstr& instr)
         {
-            RegNum reg_num = instr.get_dst_num();
-            if ( REG_NUM_ZERO != reg_num)
-                write( reg_num, instr.get_v_dst());
+            RegNum reg_num  = instr.get_dst_num();
+            uint32 v_dst    = instr.get_v_dst();
+            if ( REG_NUM_ZERO != reg_num && v_dst != NO_VAL32)
+                write( reg_num, v_dst);
+            else
+                write( reg_num, read(reg_num));
         }
 
         inline void cancel( const FuncInstr& instr)
