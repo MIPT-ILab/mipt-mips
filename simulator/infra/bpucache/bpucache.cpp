46024a24848df7b0d02a02db85312c38568cd06b
denislos <de.loss.a.99@gmail.com>
2017-12-03 15:22:06 +0300

    Add BPUCache and LRUTagCache

---
+++ simulator/infra/bpucache/bpucache.cpp
@@ -0,0 +1,73 @@
+/**
+ * BPU cache
+ * @author Oleg Ladin, Denis Los
+ */
+
+#include <cassert>
+
+
+#include <infra/bpucache/bpucache.h>
+#include <infra/macro.h>
+
+
+
+uint32 BPUCache::check_arguments( uint32 size, uint32 ways)
+{
+    if ( size == 0)
+        serr << "ERROR: Wrong arguments! Cache size should be greater than zero"
+             << std::endl << critical;
+    
+    if ( ways == 0)
+        serr << "ERROR: Wrong arguments! Num of ways should be greater than zero"
+             << std::endl << critical;
+    
+    if ( size < ways)
+        serr << "ERROR: Wrong arguments! Cache size should be greater"
+             << "than the number of ways" 
+             << std::endl << critical;
+
+    if ( !is_power_of_two( size))
+        serr << "ERROR: Wrong argumets! Cache size should be a power of 2"
+             << std::endl << critical;         
+    
+    if ( size % ways != 0)
+        serr << "ERROR: Wrong arguments! Cache size should be multiple of"
+             << "the number of ways"
+             << std::endl << critical;
+
+    return size / ways;
+}
+
+
+
+std::pair<Addr, uint32> BPUCache::read( Addr addr)
+{
+    uint32 num_set = set( addr);
+
+    auto result = cache[ num_set].find( addr);
+    if ( result.first)
+        cache[ num_set].update( addr); // update LRU if it's a hit
+
+    return result;    
+}
+
+
+
+std::pair<Addr, uint32> BPUCache::read_no_touch( Addr addr) const
+{
+    uint32 num_set = set( addr);
+    return cache[ num_set].find( addr);
+}
+
+
+
+uint32 BPUCache::write( Addr addr)
+{
+    uint32 num_set = set( addr);
+
+    cache[ num_set].update( addr);
+    const auto&[ is_hit, value] = cache[ num_set].find( addr);
+    assert( is_hit);
+
+    return value;
+}
---
+++ simulator/infra/bpucache/bpucache.h
@@ -0,0 +1,48 @@
+/**
+ * BPU cache
+ * @author Oleg Ladin, Denis Los
+ */
+
+#ifndef BPUCACHE_H
+#define BPUCACHE_H
+
+
+#include <infra/bpucache/LRUTagCache.h>
+#include <infra/types.h>
+#include <infra/log.h>
+
+#include <vector>
+#include <map>
+#include <utility>
+
+
+class BPUCache : private Log
+{
+    public:
+        BPUCache( uint32 size, uint32 ways)
+            : Log( false)
+            , number_of_sets( check_arguments( size, ways))
+            , number_of_ways( ways)
+            , size_in_entries( size)
+            , cache( number_of_sets, LRUTagCache<Addr, uint32>( number_of_ways))  
+        { }
+
+        std::pair<Addr, uint32> read( Addr addr);
+        std::pair<Addr, uint32> read_no_touch( Addr addr) const;
+        uint32 write( Addr addr);
+
+        uint32 set( Addr addr) const { return addr & (number_of_sets - 1); }
+    
+    private:
+        // If arguments are correct, it returns the number of sets
+        uint32 check_arguments( uint32 size, uint32 ways); 
+
+    private:
+        const uint32 number_of_sets;
+        const uint32 number_of_ways;
+        const uint32 size_in_entries;
+
+        std::vector<LRUTagCache<Addr, uint32>> cache;
+};
+
+#endif // BPUCACHE_H
