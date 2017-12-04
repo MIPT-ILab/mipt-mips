4aab01e357f33e60e4335d176ca1e661085aef4e
Pavel I. Kryukov <kryukov@frtk.ru>
2017-12-04 17:26:14 +0300

    Add popcount macro

--- simulator/infra/macro.h
+++ simulator/infra/macro.h
@@ -9,6 +9,9 @@
 #ifndef COMMON__MACRO_H
 #define COMMON__MACRO_H
 
+#include <bitset>
+#include <limits>
+#include <type_traits>
 #include <algorithm>
 
 /* Returns size of a static array */
@@ -30,5 +33,21 @@ constexpr size_t min_sizeof() noexcept { return std::min(sizeof(Args)...); }
 /* Find maximal sizeof */
 template<typename ... Args>
 constexpr size_t max_sizeof() noexcept { return std::max(sizeof(Args)...); }
+
+// sizeof(x)*CHAR_BIT
+template<typename T>
+constexpr auto bitwidth = std::numeric_limits<T>::digits + std::numeric_limits<T>::is_signed;
+
+// https://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+template<typename T,
+         typename = std::enable_if_t<std::is_integral<T>::value>,         // only integral
+         typename = std::enable_if_t<std::numeric_limits<T>::radix == 2>, // only binary
+         typename = std::enable_if_t<bitwidth<T> <= bitwidth<unsigned long long>> // only narrow
+       >
+auto popcount( T x) noexcept
+{
+    return std::bitset<bitwidth<T>>( static_cast<typename std::make_unsigned<T>::type>( x)).count();
+}
+
 
 #endif
