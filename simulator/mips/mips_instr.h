ddd03c21d833b0034984a80fda9644baa950d928
Pavel I. Kryukov <kryukov@frtk.ru>
2017-12-07 13:56:48 +0300

    Pass types to sizeof instead of variables
    MSVS doesn’t handle variable names @compile time properly

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -111,7 +111,7 @@ class FuncInstr
 
         const union _instr
         {
-            const struct
+            const struct AsR
             {
                 uint32 funct  :6;
                 uint32 shamt  :5;
@@ -120,14 +120,14 @@ class FuncInstr
                 uint32 rs     :5;
                 uint32 opcode :6;
             } asR;
-            const struct
+            const struct AsI
             {
                 uint32 imm    :16;
                 uint32 rt     :5;
                 uint32 rs     :5;
                 uint32 opcode :6;
             } asI;
-            const struct
+            const struct AsJ
             {
                 uint32 imm    :26;
                 uint32 opcode :6;
@@ -138,10 +138,10 @@ class FuncInstr
             _instr() : raw(NO_VAL32) { };
             explicit _instr(uint32 bytes) : raw( bytes) { }
 
-            static_assert( sizeof( asR) == sizeof( raw));
-            static_assert( sizeof( asI) == sizeof( raw));
-            static_assert( sizeof( asJ) == sizeof( raw));
-            static_assert( sizeof( raw) == 4);
+            static_assert( sizeof( AsR) == sizeof( uint32));
+            static_assert( sizeof( AsI) == sizeof( uint32));
+            static_assert( sizeof( AsJ) == sizeof( uint32));
+            static_assert( sizeof( uint32) == 4);
         } instr;
 
         using Execute = void (FuncInstr::*)();
