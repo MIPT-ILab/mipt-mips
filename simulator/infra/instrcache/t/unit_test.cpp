da62fab050ebab539f40f6c156797895d4c33353
alex19999 <armen.arsen2011@yandex.ru>
2018-02-08 01:32:15 +0300

    Fixing unit tests(try 2)

--- simulator/infra/instrcache/t/unit_test.cpp
+++ simulator/infra/instrcache/t/unit_test.cpp
@@ -44,11 +44,11 @@ TEST( update_and_find_int, Update_Find_And_Check_Using_Int)
 
 TEST( update_and_find, Update_Find_And_Check)
 {
-    InstrCache<FuncInstr> instr_cache{};
+    InstrCache<MIPSInstr> instr_cache{};
 
     const uint32 instr_bytes = 0x3c010400;
     const Addr PC = 0x401c04;
-    const FuncInstr instr( instr_bytes, PC);
+    const MIPSInstr instr( instr_bytes, PC);
 
     instr_cache.update( PC, instr);
     ASSERT_TRUE( instr_cache.find( PC).first);
@@ -60,7 +60,7 @@ TEST( check_method_erase, Check_Method_Erase)
 
     const uint32 instr_bytes = 0x3c010400;
     const Addr PC = 0x401c04;
-    const FuncInstr instr( instr_bytes, PC);
+    const MIPSInstr instr( instr_bytes, PC);
 
     instr_cache.update( PC, instr);
     instr_cache.erase( PC);
@@ -88,7 +88,7 @@ TEST( check_method_size, Check_Method_Size)
 
     uint32 instr_bytes = 0x2484ae10;
     Addr PC = 0x30ae17;
-    const std::size_t SIZE = InstrCache<FuncInstr>::get_capacity() / 12;
+    const std::size_t SIZE = InstrCache<MIPSInstr>::get_capacity() / 12;
 
     for ( std::size_t i = 0; i < SIZE; ++i)
     {
