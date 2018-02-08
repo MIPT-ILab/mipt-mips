d9df61fe96a907b723482ed061abb54d56a17d5c
Pavel I. Kryukov <kryukov@frtk.ru>
2018-02-08 17:38:37 +0300

    Create risc_v.h [skip ci]

---
+++ simulator/risc_v/risc_v.h
@@ -0,0 +1,15 @@
+/**
+ * risc_v.h - all the aliases to RISC-V ISA
+ * @author Pavel Kryukov
+ * Copyright 2018 MIPT-MIPS
+ */ 
+
+#ifndef RISC_V_H_
+#define RISC_V_H_
+
+struct RISC_V
+{
+
+};
+
+#endif // RISC_V_H_
