b0179cc82039d2a335b29950794f74d2be6799be
Pavel I. Kryukov <kryukov@frtk.ru>
2018-02-28 22:36:11 +0300

    Use correct macro for NOP trace

--- simulator/func_sim/t/unit_test.cpp
+++ simulator/func_sim/t/unit_test.cpp
@@ -52,7 +52,7 @@ TEST( Func_Sim, Run_SMC_Trace)
 TEST( Func_Sim, Run_nop_trace)
 {
     FuncSim<MIPS> mips;
-    GTEST_ASSERT_NO_DEATH( mips.run( smc_code, smc_code_num_steps + 100)
+    ASSERT_EXIT( mips.run( smc_code, smc_code_num_steps + 100)
                  ::testing::ExitedWithCode( EXIT_FAILURE), "Bearings lost:.*");
 }
 
