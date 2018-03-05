504ce527b098d7b3636ab68c21ea4f64e5ec8b0c
Pavel I. Kryukov <kryukov@frtk.ru>
2018-03-05 12:31:10 +0300

    Update tidy-check.sh

--- simulator/tools/tidy-check.sh
+++ simulator/tools/tidy-check.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-run-clang-tidy-5.0.py -header-filter=.* -checks='*,-google-readability-braces-around-statements,-readability-braces-around-statements,-cppcoreguidelines-pro-type-union-access,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-llvm-header-guard,-llvm-include-order,-readability-redundant-declaration,-cert-err58-cpp,-cppcoreguidelines-pro-bounds-constant-array-index,-android-cloexec-fopen' *.cpp
-
+run-clang-tidy-5.0.py -header-filter=.* -checks='*,-google-readability-braces-around-statements,-readability-braces-around-statements,-cppcoreguidelines-pro-type-union-access,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-llvm-header-guard,-llvm-include-order,-readability-redundant-declaration,-cert-err58-cpp,-cppcoreguidelines-pro-bounds-constant-array-index,-android-cloexec-fopen'
