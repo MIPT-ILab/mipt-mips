0144418d7c904ea8f07bf47e2617c476ea7de5e5
SoshinK <soshinkonstantinv@gmail.com>
2018-03-05 04:05:51 +0300

    Add APPLE variable and removed tidy-check target. Tidy-check in tools/simfreq.sh

--- simulator/tools/simfreq.sh
+++ simulator/tools/simfreq.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+run-clang-tidy-5.0.py -header-filter=.* -checks=*,-google-readability-braces-around-statements,-readability-braces-around-statements,-cppcoreguidelines-pro-type-union-access,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-llvm-header-guard,-llvm-include-order,-readability-redundant-declaration,-cert-err58-cpp,-cppcoreguidelines-pro-bounds-constant-array-index,-android-cloexec-fopen 
 ./mipt-mips -b ./../traces/fib.out -n 1000000 | grep "sim freq"
