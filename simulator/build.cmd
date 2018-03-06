c349e46fe1106a3e245a5c843063ff016109eea0
Alexandr <armen.arsen2011@yandex.ru>
2018-03-06 16:12:40 +0200

    Add unit tests to MIPSRegister (#307)

--- simulator/build.cmd
+++ simulator/build.cmd
@@ -21,7 +21,7 @@ cl /I. /EHsc /c /nologo /MD ^
    bypass/data_bypass.cpp ^
    fetch/fetch.cpp ^
    mips/mips_instr.cpp ^
-   mips/mips_register.cpp ^
+   mips/mips_register/mips_register.cpp ^
    func_sim/func_sim.cpp ^
    core/perf_sim.cpp ^
    writeback/writeback.cpp ^
@@ -36,7 +36,7 @@ set TRUNK=%cd%
 set TRUNKX=%TRUNK:\=\\%
 
 rem Build and run all the tests
-for %%G in (infra\cache infra\elf_parser infra\config infra\memory infra\ports infra\string mips infra\instrcache func_sim bpu core) do (
+for %%G in (infra\cache infra\elf_parser infra\config infra\memory infra\ports infra\string mips mips\mips_register infra\instrcache func_sim bpu core) do (
     echo Testing %%G
     cd %%G\t
     cl /nologo unit_test.cpp %TRUNK%\*.obj %TRUNK%\..\libelf\lib\libelf.lib ^
