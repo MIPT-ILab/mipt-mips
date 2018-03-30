a4e7d0ff8ea247ca828e0f337abad7949dd45f21
Pavel I. Kryukov <kryukov@frtk.ru>
2018-03-31 02:16:23 +0300

    Move wide types into types.h (#380)

--- simulator/infra/types.h
+++ simulator/infra/types.h
@@ -34,32 +34,63 @@ using float32 = float;
 /* Double precision 64-bit float type */
 using float64 = double;
 
+// Use native GCC type if available, as Boost <= 1.60 + GCC 7 generate a bug
+#if defined(__GNUC__) && !defined(__clang__) && defined(__SIZEOF_INT128__)
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+
+/* Unsigned 128-bit integer type */
+using uint128 = unsigned __int128;
+
+/* Signed 128-bit integer type */
+using int128 = __int128;
+
+#pragma GCC diagnostic pop
+
+#else // __SIZEOF_INT128__
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+/* Unsigned 128-bit integer type */
+using uint128 = boost::multiprecision::uint128_t;
+
+/* Unsigned 128-bit integer type */
+using int128 = boost::multiprecision::int128_t;
+
+#endif // __SIZEOF_INT128__
+
 /* Convert signed type to unsigned type */
 template<typename> struct unsign;
-template<> struct unsign<int8>  { using type = uint8; };
-template<> struct unsign<int16> { using type = uint16; };
-template<> struct unsign<int32> { using type = uint32; };
-template<> struct unsign<int64> { using type = uint64; };
+template<> struct unsign<int8>   { using type = uint8; };
+template<> struct unsign<int16>  { using type = uint16; };
+template<> struct unsign<int32>  { using type = uint32; };
+template<> struct unsign<int64>  { using type = uint64; };
+template<> struct unsign<int128> { using type = uint128; };
 
 template<typename T> using unsign_t = typename unsign<T>::type;
 
 /* Convert unsigned type to signed type */
 template<typename> struct sign;
-template<> struct sign<uint8>  { using type = int8; };
-template<> struct sign<uint16> { using type = int16; };
-template<> struct sign<uint32> { using type = int32; };
-template<> struct sign<uint64> { using type = int64; };
+template<> struct sign<uint8>   { using type = int8; };
+template<> struct sign<uint16>  { using type = int16; };
+template<> struct sign<uint32>  { using type = int32; };
+template<> struct sign<uint64>  { using type = int64; };
+template<> struct sign<uint128> { using type = int128; };
 
 template<typename T> using sign_t = typename sign<T>::type;
 
 /* Convert type to 2x type */
 template<typename> struct doubled;
 template<> struct doubled<uint8>  { using type = uint16; };
-template<> struct doubled<int8>   { using type = int16;  };
 template<> struct doubled<uint16> { using type = uint32; };
-template<> struct doubled<int16>  { using type = int32;  };
 template<> struct doubled<uint32> { using type = uint64; };
+template<> struct doubled<uint64> { using type = uint128; };
+
+template<> struct doubled<int8>   { using type = int16;  };
+template<> struct doubled<int16>  { using type = int32;  };
 template<> struct doubled<int32>  { using type = int64;  };
+template<> struct doubled<int64>  { using type = int128;  };
 
 template<typename T> using doubled_t = typename doubled<T>::type;
 
@@ -72,8 +103,8 @@ using Addr = uint32;
 // or array index.
 static const uint8  NO_VAL8  = 0xBA;
 static const uint16 NO_VAL16 = 0xf00d;
-static const uint32 NO_VAL32 = 0xdeadbeeful;
-static const uint64 NO_VAL64 = 0xfeedfacecafebeafull;
+static const uint32 NO_VAL32 = 0xdead'beeful;
+static const uint64 NO_VAL64 = 0xfeed'face'cafe'beafull;
 
 static const uint8  MAX_VAL8  = UINT8_MAX;
 static const uint16 MAX_VAL16 = UINT16_MAX;
