ef040209d6732699f93aee1388312bc6f121e9c5
Pavel I. Kryukov <kryukov@frtk.ru>
2018-03-30 13:07:57 +0300

    Update bpu.h

--- simulator/bpu/bpu.h
+++ simulator/bpu/bpu.h
@@ -146,11 +146,11 @@ class BPFactory {
 
 public:
     BPFactory() :
-        map({ { "static_always_taken",   new BPCreator<BPEntryAlwaysTaken>},
-              { "static_backward_jumps", new BPCreator<BPEntryBackwardJumps>},
-              { "dynamic_one_bit",       new BPCreator<BPEntryOneBit>},
-              { "dynamic_two_bit",       new BPCreator<BPEntryTwoBit>},
-              { "adaptive_two_level",    new BPCreator<BPEntryAdaptive<2>>}
+        map({ { "static_always_taken"s,   new BPCreator<BPEntryAlwaysTaken>},
+              { "static_backward_jumps"s, new BPCreator<BPEntryBackwardJumps>},
+              { "dynamic_one_bit"s,       new BPCreator<BPEntryOneBit>},
+              { "dynamic_two_bit"s,       new BPCreator<BPEntryTwoBit>},
+              { "adaptive_two_level"s,    new BPCreator<BPEntryAdaptive<2>>}
             })
     { }
 
