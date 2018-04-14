320154dcfc1e7fda7fd7cbacaeaed02effe6b384
Pavel I. Kryukov <kryukov@frtk.ru>
2018-04-14 10:20:51 +0300

    Update macro_test.cpp

--- simulator/infra/macro_test.cpp
+++ simulator/infra/macro_test.cpp
@@ -68,9 +68,9 @@ static_assert(count_leading_zeroes<uint64>(0xFF) == 56);
 static_assert(count_leading_zeroes<uint8>(0x0) == 8);
 static_assert(count_leading_zeroes<uint32>(0x0) == 32);
 static_assert(count_leading_zeroes<uint64>(0x0) == 64);
-static_assert(count_leading_zeroes<uint8>(~0x0) == 0);
-static_assert(count_leading_zeroes<uint32>(~0x0) == 0);
-static_assert(count_leading_zeroes<uint64>(~0x0) == 0);
+static_assert(count_leading_zeroes<uint8>(~0x0u) == 0);
+static_assert(count_leading_zeroes<uint32>(~0x0u) == 0);
+static_assert(count_leading_zeroes<uint64>(~0x0u) == 0);
 
 /* Boost cannot instantiate count_leading_zeroes in constexpr context
 static_assert(count_leading_zeroes<uint128>(0x0) == 128);
