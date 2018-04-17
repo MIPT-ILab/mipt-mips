f3b5524b3732c0b4534de34f6e4b8203ab4022a5
Pavel I. Kryukov <kryukov@frtk.ru>
2018-04-17 22:07:59 +0300

    Fix mask calculation

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -293,8 +293,8 @@ class MIPSInstr
             // Endian specific
             calculate_load_addr();
             // Actually we read word LEFT to instruction pointer
-            mem_addr += 3;
             mask = ~bitmask<uint64>( ( 4 - mem_addr % 4) * 8);
+            mem_addr -= 3;
         }
 
         Execute function = &MIPSInstr::execute_unknown;
