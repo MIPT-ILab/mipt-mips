c166e22f7a7db0489c5fcf0dc01b901850430ac5
Pavel I. Kryukov <kryukov@frtk.ru>
2018-05-16 01:21:36 +0300

    Remove unused code from config (#435)

--- simulator/infra/config/config.cpp
+++ simulator/infra/config/config.cpp
@@ -15,25 +15,25 @@ static AliasedSwitch help_option = { "h", "help", "print help"};
 /* basic method */
 void handleArgs( int argc, const char* argv[])
 {
-    BaseValue::values().parse( argc, argv);
+    BaseValue::options().parse( argc, argv);
 
     /* parsing help */
     if ( help_option)
     {
         std::cout << "Functional and performance simulators for MIPS-based CPU."
                   << std::endl << std::endl
-                  << BaseValue::values() << std::endl;
+                  << BaseValue::options() << std::endl;
         std::exit( EXIT_SUCCESS);
     }
-    else if ( !BaseValue::values().unknown_options().empty() && !BaseValue::values().non_option_args().empty()) {
+    else if ( !BaseValue::options().unknown_options().empty() && !BaseValue::options().non_option_args().empty()) {
         std::cerr << "Unknown options:" << std::endl;
-        for ( const auto& opt: BaseValue::values().unknown_options())
+        for ( const auto& opt: BaseValue::options().unknown_options())
             std::cerr << opt << std::endl;
-        for ( const auto& opt: BaseValue::values().non_option_args())
+        for ( const auto& opt: BaseValue::options().non_option_args())
             std::cerr << opt << std::endl;
         std::cerr << "Correct options are:"
             << std::endl << std::endl
-            << BaseValue::values() << std::endl;
+            << BaseValue::options() << std::endl;
         throw std::exception();
     }
 }
