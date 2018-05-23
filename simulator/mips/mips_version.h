47837e5d0a4db857fca90b991d74c563e1530605
Pavel I. Kryukov <kryukov@frtk.ru>
2018-05-23 15:41:26 +0300

    Add const qualifiers to MIPSVersionMask variables

--- simulator/mips/mips_version.h
+++ simulator/mips/mips_version.h
@@ -21,8 +21,8 @@ template<> struct MIPSRegisterUIntGen<MIPSVersion::II>  { using type = uint32; }
 template<> struct MIPSRegisterUIntGen<MIPSVersion::III> { using type = uint64; };
 template<> struct MIPSRegisterUIntGen<MIPSVersion::IV>  { using type = uint64; };
 template<> struct MIPSRegisterUIntGen<MIPSVersion::V>   { using type = uint64; };
-template<> struct MIPSRegisterUIntGen<MIPSVersion::v32>  { using type = uint32; };
-template<> struct MIPSRegisterUIntGen<MIPSVersion::v64>  { using type = uint64; };
+template<> struct MIPSRegisterUIntGen<MIPSVersion::v32> { using type = uint32; };
+template<> struct MIPSRegisterUIntGen<MIPSVersion::v64> { using type = uint64; };
 
 template<MIPSVersion V> using MIPSRegisterUInt = typename MIPSRegisterUIntGen<V>::type;
 
@@ -31,7 +31,7 @@ class MIPSVersionMask {
 public:
     constexpr MIPSVersionMask() = default;
     constexpr bool is_supported( MIPSVersion version) const {
-        return ((mask >> static_cast<unsigned>(version)) & 0x1) != 0;
+        return ((mask >> static_cast<unsigned>(version)) & 0x1u) != 0;
     }
     constexpr MIPSVersionMask operator|( MIPSVersion rhs) const {
         MIPSVersionMask result = *this;
@@ -45,15 +45,15 @@ public:
     }
 };
 
-static constexpr MIPSVersionMask MIPS_64_Instr  = MIPSVersionMask() | MIPSVersion::v64;
-static constexpr MIPSVersionMask MIPS_32_Instr  = MIPS_64_Instr | MIPSVersion::v32;
+static constexpr const MIPSVersionMask MIPS_64_Instr  = MIPSVersionMask() | MIPSVersion::v64;
+static constexpr const MIPSVersionMask MIPS_32_Instr  = MIPS_64_Instr | MIPSVersion::v32;
 // MIPS64 is a superset of MIPSV
-static constexpr MIPSVersionMask MIPS_V_Instr   = MIPS_64_Instr | MIPSVersion::V;
+static constexpr const MIPSVersionMask MIPS_V_Instr   = MIPS_64_Instr | MIPSVersion::V;
 // MIPSI-V are supersets of precedessors
-static constexpr MIPSVersionMask MIPS_IV_Instr  = MIPS_V_Instr   | MIPSVersion::IV;
-static constexpr MIPSVersionMask MIPS_III_Instr = MIPS_IV_Instr  | MIPSVersion::III;
+static constexpr const MIPSVersionMask MIPS_IV_Instr  = MIPS_V_Instr   | MIPSVersion::IV;
+static constexpr const MIPSVersionMask MIPS_III_Instr = MIPS_IV_Instr  | MIPSVersion::III;
 // MIPS32 is a superset of MIPSII
-static constexpr MIPSVersionMask MIPS_II_Instr  = MIPS_III_Instr | MIPS_32_Instr | MIPSVersion::II;
-static constexpr MIPSVersionMask MIPS_I_Instr   = MIPS_II_Instr  | MIPSVersion::I;
+static constexpr const MIPSVersionMask MIPS_II_Instr  = MIPS_III_Instr | MIPS_32_Instr | MIPSVersion::II;
+static constexpr const MIPSVersionMask MIPS_I_Instr   = MIPS_II_Instr  | MIPSVersion::I;
 
 #endif // MIPS_VERSION_H
