a7930c216161aff6957e7a796fbbda1f82bb99cc
Pavel I. Kryukov <kryukov@frtk.ru>
2018-07-17 17:27:41 +0300

    Update get-binutils.sh

--- get-binutils.sh
+++ get-binutils.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.29.tar.bz2
-tar xjf binutils-2.29.tar.bz2
-cd binutils-2.29
+# Copyright (C) 2018 Pavel Kryukov, MIPT-MIPS Project
+#
+# This script is intended to be used with CI systems, please do not run it manually.
+# To get MIPS GNU Binutils, please follow this instruction: 
+# https://github.com/MIPT-ILab/mipt-mips/wiki/MIPS-binutils
+#
+git clone git://sourceware.org/git/binutils-gdb.git --branch binutils-2_31
+cd binutils-gdb
 ./configure --target=mips-linux-gnu --prefix=$1 --disable-gdb --disable-gprof > /dev/null
 make all install MAKEINFO=true > /dev/null
 cd ..
