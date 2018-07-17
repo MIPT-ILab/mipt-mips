ab5bb0208e0401dfee75cd72553647690123415e
Pavel Kryukov <kryukov@frtk.ru>
2018-07-17 15:33:24 +0300

    Fixes

--- get-binutils.sh
+++ get-binutils.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 export BINUTILS_VER=2.31
 wget http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.bz2
+mv binutils-$BINUTILS_VER.tar.bz2 binutils.tar.bz2
 tar xjf binutils-$BINUTILS_VER.tar.bz2
-cd binutils-$BINUTILS_VER
-export BINUTILS=`pwd`/cross/mips
-./configure --target=mips-linux-gnu --prefix=$BINUTILS --disable-gdb --disable-gprof --with-system-zlib > /dev/null
+cd binutils
+./configure --target=mips-linux-gnu --prefix=$1 --disable-gdb --disable-gprof --with-system-zlib > /dev/null
 make all install MAKEINFO=true > /dev/null
-export PATH=$PATH:$BINUTILS/bin
 cd ..
