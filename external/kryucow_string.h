11b0a1e9e705e27aa67e5d09b3cfca76734a4ce3
Pavel I. Kryukov <kryukov@frtk.ru>
2018-08-05 00:39:24 +0300

    Update COW string

--- external/kryucow_string.h
+++ external/kryucow_string.h
@@ -40,10 +40,6 @@
 // Support XCode without C++17
 #if __has_include("string_view")
 #  include <string_view>
-#elif __has_include("experimental/string_view")
-#  include <experimental/string_view>
-#else
-#  error "KryuCowString requires std::string_view"
 #endif
 
 // This class implements COW string with fixed maximum size.
@@ -51,85 +47,48 @@
 // but modify them rarely.
 // List of non-implemented features:
 //   * add more std::string compatibility by demand
-template<typename CharT, typename Traits = std::char_traits<CharT>>
+template<typename CharT, typename Traits = std::char_traits<CharT>, typename Allocator = std::allocator<CharT>>
 class BasicKryuCowString { // ha-ha, what a pun
 private:
-#if __has_include("string_view")
-    using View_t = std::basic_string_view<CharT, Traits>;
-#else
-    using View_t = std::expertimental::basic_string_view<CharT, Traits>;
-#endif
-    using String_t = std::basic_string<CharT, Traits>;
+    using String = std::basic_string<CharT, Traits, Allocator>;
 
-    // We don't want to initialize arena here, so suppress ClangTidy
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init, hicpp-member-init)
-    struct InternalString {
-        using SizeType = std::make_unsigned_t<CharT>;
-        static constexpr const size_t SIZE = 127;
-        static_assert((1ull << (sizeof(SizeType) * 8)) > SIZE);
+    std::shared_ptr<String> pointer = nullptr;
 
-        SizeType size = 0;
-        CharT arena[SIZE];
-
-        auto get_string_view() const { return View_t( arena, size); }
-    };
-
-    // I would like to have a static string of cache line granularity (64B typically)
-    static_assert(sizeof(InternalString) % 64 == 0);
-
-    std::shared_ptr<InternalString> pointer = nullptr;
-
-    // Allocate new memory
-    void init() { pointer = std::make_shared<InternalString>(); }
-
-    // Appends new pointer to any string
-    void append( const CharT* ptr, size_t app_size)
+    bool allocated() const { return pointer.use_count() != 0; }
+    bool owned() const { return pointer.use_count() == 1; }
+    bool shared() const { return allocated() && !owned(); }
+    
+    template<typename ... Args>
+    void assign(Args&& ... args)
     {
-        // The string is empty. Need to allocate memory first.
-        if (pointer.use_count() == 0)
-            init();
-
-        const auto my_size = pointer->size; // May be zero as well
-        if ( my_size + app_size > InternalString::SIZE)
-            throw std::length_error("KryuCowSting exceeded length\n");
-
-        // The string has a full copy. That's the place 'copy-on-write' occurs
-        if (pointer.use_count() > 1) {
-            const auto old_pointer = pointer; // Keep a pointer to old pointer for a moment
-            init(); // Allocate new memory
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic) Use pointer arithmetic
-            std::copy( old_pointer->arena, old_pointer->arena + my_size, pointer->arena);
-        }
-
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic) Use pointer arithmetic
-        std::copy( ptr, ptr + app_size, pointer->arena + my_size);
-        pointer->size = my_size + app_size; // update size
+        if (owned()) // Do not reallocate memory
+            *pointer = String(std::forward<Args>(args)...);
+        else
+            *this = BasicKryuCowString(std::forward<Args>(args)...);
+    }
+    
+    // Gets a detouched instance of string
+    // Modification of detouched instance does not affect other strings
+    void detouch()
+    {
+        if (!allocated())
+            *this = BasicKryuCowString(String());
+        else if (shared()) // The string has a full copy. That's the place 'copy-on-write' occurs
+            *this = BasicKryuCowString(*pointer);
     }
 
-    // Append wrappers
-    void append( const CharT* ptr) { append( ptr, Traits::length( ptr)); }
-
+    static bool is_empty(const CharT* val) { return Traits::length( val) == 0; }
+    
     template<typename T>
-    void append( const T& v) { append( v.data(), v.length());   }
-
-    // Get string view object
-    // const std::string_view is fancy here as it may be
-    // easily converted to std::string or const char*
-    // and has a lot of useful operators already (==, != etc.)
-    auto get_string_view() const noexcept {
-        if (pointer.use_count() != 0)
-            return pointer->get_string_view();
-
-        return View_t(nullptr, 0);
-    }
+    static bool is_empty(const T& val) { return val.empty(); }
 
 public:
     // std::basic_string typedefs
     using traits_type = Traits;
     using value_type  = CharT;
-    // using allocator_type = Allocator;
-    using size_type       = std::size_t;
-    using difference_type = std::ptrdiff_t;
+    using allocator_type = Allocator;
+    using size_type       = typename String::size_type;
+    using difference_type = typename String::difference_type;
     using reference       = value_type&;
     using const_reference = const value_type&;
     // using pointer;
@@ -139,67 +98,76 @@ public:
     // using reverse_iterator;
     // using const_reverse_iterator
 
-    // ctor
-    BasicKryuCowString() = default;
+    BasicKryuCowString() = default; // Do nothing
+    // Big rule of zero: everything is handled by std::shared_ptr
 
     // ctor from any type: just append the value
     // to an empty string
-    template<typename T>
-    explicit BasicKryuCowString( const T& value) {
-        append( value);
+    template<typename ... Args>
+    explicit BasicKryuCowString(Args&& ... args) {
+        pointer = std::make_shared<String>(std::forward<Args>(args)...);
     }
 
     // assignment: clear existing string and
     // append the value to the empty string
     template<typename T>
-    BasicKryuCowString& operator=( const T& value) {
+    BasicKryuCowString& operator=(T&& value) {
         clear();
-        append( value);
+        assign(std::forward<T>(value));
         return *this;
     }
 
-    // appending: see the method above
-    template<typename T>
-    auto& operator+=( const T& value) {
-        append( value);
+    // Appending: detouch current object and append internal string
+    template<typename T,
+             typename = typename std::enable_if<!std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, BasicKryuCowString>>::type>
+    auto& operator+=(T&& value) {
+        detouch();
+        *pointer += std::forward<T>(value);
         return *this;
     }
 
-    // convert to std::string_view like std::string does
-    explicit operator View_t() const noexcept {
-        return get_string_view();
+    auto& operator+=(const BasicKryuCowString& value) {
+        if (value.allocated())
+            return *this += *(value.pointer);
+        return *this;
     }
+
+#if __has_include("string_view")
+    explicit operator std::basic_string_view<CharT, Traits>() const noexcept {
+        return std::basic_string_view<CharT, Traits>( c_str(), size());
+    }
+#endif
 
     // capacity
     bool empty() const {
-        return pointer.use_count() == 0 || pointer->size == 0;
+        return !allocated() || pointer->empty();
     }
 
     size_t size() const noexcept {
-        return pointer.use_count() == 0 ? 0 : pointer->size;
+        return !allocated() ? 0 : pointer->size();
     }
 
     size_t length() const noexcept {
         return size();
     }
 
-    constexpr size_type max_size() const noexcept {
-        return InternalString::SIZE;
+    size_type max_size() const noexcept {
+        return pointer->max_size();
     }
 
-    constexpr size_type capacity() const noexcept {
-        return InternalString::SIZE;
+    size_type capacity() const noexcept {
+        return !allocated() ? 0 : pointer->capacity();
     }
 
     // only CONSTANT get operators
     const CharT& operator[]( size_t value) const noexcept {
-        return pointer->arena[value];
+        return (*pointer)[value];
     }
 
     const CharT& at( size_t value) const {
-        if ( empty() || value > pointer->size)
-            throw std::out_of_range("KryuCowString index is out of range\n");
-        return operator[](value);
+        if (!allocated())
+            throw std::out_of_range("KryuCowString out of range");
+        return pointer->at(value);
     }
 
     const CharT& front() const noexcept {
@@ -211,7 +179,7 @@ public:
     }
 
     const CharT* data() const noexcept {
-        return pointer.use_count() == 0 ? nullptr : pointer->arena;
+        return pointer->data();
     }
 
     const CharT* c_str() const noexcept {
@@ -221,34 +189,47 @@ public:
     // Equality comparators for obvious types
     // Actually I would like to generalize it with some conversion
     // of RHS types to std::string_view
-    bool operator==( const BasicKryuCowString& rhs) const noexcept { return get_string_view() == rhs.get_string_view(); }
-    bool operator==( const String_t& rhs) const noexcept { return get_string_view() == static_cast<decltype(get_string_view())>(rhs); }
-    bool operator==( const View_t& rhs) const noexcept { return get_string_view() == rhs; }
-    bool operator==( const CharT* rhs) const { return get_string_view() == View_t( rhs, Traits::length(rhs)); }
+    bool operator==(const BasicKryuCowString& rhs) const noexcept {
+        if (empty() || is_empty( rhs))
+            return empty() == is_empty( rhs);
+
+        return *pointer == *(rhs.pointer);
+    }
+
+    template<typename T>
+    bool operator==(const T& rhs) const noexcept {
+        if (empty() || is_empty( rhs))
+            return empty() == is_empty( rhs);
+
+        return *pointer == rhs;
+    }
 
     // Generate inequality comparator
     template<typename T>
-    bool operator!=( const T& rhs) const { return !(*this == rhs); }
+    bool operator!=(const T& rhs) const { return !(*this == rhs); }
 
     // Makes a string empty
     void clear() {
-        if (pointer.use_count() > 1) {
+        if (shared()) {
             // Someone else uses the pointer, so we
             // must allocate a new pointer block
             // Let's do it on demand and keep
             // nullptr
             pointer = nullptr;
         }
-        else if (pointer.use_count() == 1) {
+        else if (owned()) {
             // We own the pointer, so we may clear it
             // and do not allocate new pointer block
-            pointer->size = 0;
+            pointer->clear();
         }
     }
 
     // Dump operator. Very nice that we may reuse std::string_view here.
-    friend auto operator<<( std::basic_ostream<CharT, Traits>& out, const BasicKryuCowString& value) -> decltype(out) {
-        return out << value.get_string_view();
+    friend auto operator<<(std::basic_ostream<CharT, Traits>& out, const BasicKryuCowString& value) -> decltype(out) {
+        if (!value.allocated())
+            return out;
+
+        return out << *(value.pointer);
     }
 };
 
@@ -258,15 +239,14 @@ public:
 // explicit operator must be use instead
 template<typename T, typename CharT, typename Traits,
          typename = typename std::enable_if_t<!std::is_same_v<std::remove_reference_t<T>, BasicKryuCowString<CharT, Traits>>>>
-bool operator==( const T& lhs, const BasicKryuCowString<CharT, Traits>& rhs) { return rhs == lhs; }
+bool operator==(const T& lhs, const BasicKryuCowString<CharT, Traits>& rhs) { return rhs == lhs; }
 
 template<typename T, typename CharT, typename Traits,
          typename = typename std::enable_if_t<!std::is_same_v<std::remove_reference_t<T>, BasicKryuCowString<CharT, Traits>>>>
-bool operator!=( const T& lhs, const BasicKryuCowString<CharT, Traits>& rhs) { return rhs != lhs; }
+bool operator!=(const T& lhs, const BasicKryuCowString<CharT, Traits>& rhs) { return rhs != lhs; }
 
 // Typedefs for simple KryuCowString and wide-char version
 using KryuCowString = BasicKryuCowString<char>;
 using WKryuCowString = BasicKryuCowString<wchar_t>;
 
 #endif // KRYUCOW_STRING_H
-
