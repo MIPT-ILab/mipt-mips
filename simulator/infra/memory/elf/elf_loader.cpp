d524becbadf100ea5c73148d98722cb3d99239b6
Pavel I. Kryukov <kryukov@frtk.ru>
2018-10-08 19:44:15 +0300

    Update elf_loader.cpp

--- simulator/infra/memory/elf/elf_loader.cpp
+++ simulator/infra/memory/elf/elf_loader.cpp
@@ -11,9 +11,6 @@
 
 static void load_elf_section( FuncMemory* memory, const ELFIO::section& section)
 {
-    if ( section->get_address() == 0)
-        return;
-
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast) Connecting ELFIO to our guidelines
     memory->memcpy_host_to_guest( section.get_address(), reinterpret_cast<const Byte*>(section.get_data()), section.get_size());
 }
@@ -35,8 +32,8 @@ static ELFIO::elfio get_elfio_reader( const std::string& filename)
 {
     ELFIO::elfio reader;
 
-    if ( !reader.load( executable_file_name))
-        throw InvalidElfFile( executable_file_name);
+    if ( !reader.load( filename))
+        throw InvalidElfFile( filename);
 
     return reader;
 }
