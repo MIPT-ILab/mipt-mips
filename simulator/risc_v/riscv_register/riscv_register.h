174c9efead8a3fdbbbeec2bf7ec9dfe83b5d5d2b
Pavel I. Kryukov <kryukov@frtk.ru>
2018-10-27 13:47:10 +0300

    Hide RISCV register ctor

--- simulator/risc_v/riscv_register/riscv_register.h
+++ simulator/risc_v/riscv_register/riscv_register.h
@@ -28,11 +28,6 @@ class RISCVRegister {
 public:
     static constexpr const size_t MAX_REG = MAX_VAL_RegNum;
 
-    explicit RISCVRegister( uint8 id) : RISCVRegister( static_cast<RegNum>( id))
-    {
-        assert( id < 32u);
-    }
-
     friend std::ostream& operator<<( std::ostream& out, const RISCVRegister& rhs)
     {
         return out << regTable.at( rhs.value);
@@ -41,10 +36,7 @@ public:
     bool is_zero()                 const { return value == RISCV_REG_zero; }
     constexpr bool is_mips_hi()    const { return false; }
     constexpr bool is_mips_lo()    const { return false; }
-    static RISCVRegister from_cpu_index( uint8 id) {
-        assert( id < 32u);
-        return RISCVRegister( static_cast<RegNum>( id));
-    }
+    static RISCVRegister from_cpu_index( uint8 id) { return RISCVRegister( static_cast<RegNum>( id)); }
     size_t to_rf_index()           const { return value; }
 
     static const RISCVRegister zero;
