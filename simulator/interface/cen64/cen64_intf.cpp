50ae47431afcefd3ff822cf3f2cb468bfb691d1f
Pavel I. Kryukov <kryukov@frtk.ru>
2018-10-29 17:31:56 +0300

    Fix Clang-Tidy warnings in CEN64 MI registers code

--- simulator/interface/cen64/cen64_intf.cpp
+++ simulator/interface/cen64/cen64_intf.cpp
@@ -6,8 +6,8 @@
 
 #include "cen64_intf.h" 
 #include <memory/memory.h>
-#include <modules/core/perf_sim.h>
 #include <mips/mips.h>
+#include <modules/core/perf_sim.h>
 
 extern std::shared_ptr<FuncMemory> generate_cen64_memory( bus_controller* bus_ptr);
 
@@ -26,17 +26,17 @@ struct vr4300 : private PerfSim<MIPS64>
     using PerfSim<MIPS64>::clock;
     using PerfSim<MIPS64>::read_cpu_register;
 
-    std::array<int32, NUM_MI_REGISTERS> mi_regs = {};
+    std::array<uint32, NUM_MI_REGISTERS> mi_regs = {};
 
     template<typename ... Args>
-    vr4300( Args ... args) : PerfSim<MIPS64>( std::move( args)...) { }
+    explicit vr4300( Args ... args) : PerfSim<MIPS64>( std::move( args)...) { }
 
     int init( struct bus_controller* bus_ptr)
     {
         set_memory( generate_cen64_memory( bus_ptr));
         set_pc( 0x1fc00000ull);
-        mi_regs[MI_VERSION_REG] = 0x01010101;
-        mi_regs[MI_INIT_MODE_REG] = 0x80;
+        mi_regs[MI_VERSION_REG] = 0x01010101u;
+        mi_regs[MI_INIT_MODE_REG] = 0x80u;
         return 0;
     }
 
@@ -55,7 +55,7 @@ struct vr4300 : private PerfSim<MIPS64>
     // Checks for interrupts, possibly sets the cause bit.
     void check_for_interrupts()
     {
-        if (mi_regs[MI_INTR_REG] & mi_regs[MI_INTR_MASK_REG])
+        if ((mi_regs[MI_INTR_REG] & mi_regs[MI_INTR_MASK_REG]) != 0)
             apply_mask_to_cause( 0x400);
         else
             reset_mask_to_cause( 0x400);
@@ -85,20 +85,23 @@ uint64 vr4300_get_pc(struct vr4300* /* vr4300 */) { return 0; }
 
 int read_mi_regs( void* opaque, uint32_t address, uint32_t* word)
 {
-    auto reg = static_cast<MiRegister>( ( address - MI_REGS_BASE_ADDRESS) >> 2);
-    *word = reinterpret_cast<const vr4300*>( opaque)->mi_regs[reg];
+    // NOLINTNEXTLINE(ppcoreguidelines-pro-type-reinterpret-cast) Need to fix signature in CEN64
+    auto cpu = reinterpret_cast<const vr4300*>( opaque);
+    auto reg = static_cast<MiRegister>( ( address - MI_REGS_BASE_ADDRESS) / 4);
+    *word = ->mi_regs[reg];
     return 0;
 }
 
 int write_mi_regs( void* opaque, uint32 address, uint32 word, uint32 dqm)
 {
+    // NOLINTNEXTLINE(ppcoreguidelines-pro-type-reinterpret-cast) Need to fix signature in CEN64
     auto vr4300 = reinterpret_cast<struct vr4300*>( opaque);
-    auto reg = static_cast<MiRegister>( ( address - MI_REGS_BASE_ADDRESS) >> 2);
+    auto reg = static_cast<MiRegister>( ( address - MI_REGS_BASE_ADDRESS) / 4);
 
     switch (reg) {
     case MI_INIT_MODE_REG: vr4300->write_mi_init_mode_reg( word); break;
     case MI_INTR_MASK_REG: vr4300->write_mi_intr_mask_reg( word); break;
-    default: vr4300->mi_regs[reg] = (vr4300->mi_regs[reg] & ~dqm) | word; break;
+    default: vr4300->mi_regs.at( reg) = ( vr4300->mi_regs.at( reg) & ~dqm) | word; break;
     }
 
     return 0;
@@ -124,26 +127,26 @@ void vr4300::write_mi_init_mode_reg( uint32 word)
     static const constexpr uint64 MI_EBUS_TEST_MODE = 0x0080;
     static const constexpr uint64 MI_INIT_MODE = 0x0100;
     static const constexpr uint64 MI_RDRAM_REG_MODE = 0x0200;
-    uint32 result = word & 0x3FF;
+    uint32 result = word & 0x3FFu;
 
-    if (word & 0x0080)
+    if ((word & 0x0080) != 0)
         result &= ~MI_INIT_MODE;
-    else if (word & 0x0100)
+    else if ((word & 0x0100) != 0)
         result |= MI_INIT_MODE;
 
-    if (word & 0x0200)
+    if ((word & 0x0200) != 0)
         result &= ~MI_EBUS_TEST_MODE;
-    else if (word & 0x0400)
+    else if ((word & 0x0400) != 0)
         result |= MI_EBUS_TEST_MODE;
 
-    if (word & 0x0800) {
+    if ((word & 0x0800) != 0) {
         mi_regs[MI_INTR_REG] &= ~MI_INTR_DP;
         check_for_interrupts();
     }
 
-    if (word & 0x1000)
+    if ((word & 0x1000) != 0)
         result &= ~MI_RDRAM_REG_MODE;
-    else if (word & 0x2000)
+    else if ((word & 0x2000) != 0)
         result |= MI_RDRAM_REG_MODE;
 
     mi_regs[MI_INIT_MODE_REG] = result; 
@@ -151,34 +154,34 @@ void vr4300::write_mi_init_mode_reg( uint32 word)
 
 void vr4300::write_mi_intr_mask_reg( uint32 word)
 {
-    if (word & 0x0001)
+    if ((word & 0x0001) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_SP;
-    else if (word & 0x0002)
+    else if ((word & 0x0002) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_SP;
 
-    if (word & 0x0004)
+    if ((word & 0x0004) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_SI;
-    else if (word & 0x0008)
+    else if ((word & 0x0008) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_SI;
 
-    if (word & 0x0010)
+    if ((word & 0x0010) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_AI;
-    else if (word & 0x0020)
+    else if ((word & 0x0020) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_AI;
 
-    if (word & 0x0040)
+    if ((word & 0x0040) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_VI;
-    else if (word & 0x0080)
+    else if ((word & 0x0080) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_VI;
 
-    if (word & 0x0100)
+    if ((word & 0x0100) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_PI;
-    else if (word & 0x0200)
+    else if ((word & 0x0200) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_PI;
 
-    if (word & 0x0400)
+    if ((word & 0x0400) != 0)
         mi_regs[MI_INTR_MASK_REG] &= ~MI_INTR_DP;
-    else if (word & 0x0800)
+    else if ((word & 0x0800) != 0)
         mi_regs[MI_INTR_MASK_REG] |= MI_INTR_DP;
 
     check_for_interrupts();
