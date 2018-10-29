22c2b732a471fa523faf3fbc0f124c3e9f2e9e41
Pavel Kryukov <kryukov@frtk.ru>
2018-10-29 18:45:11 +0300

    Add missing virtual dtor to Kernel interface

--- simulator/kernel/kernel.h
+++ simulator/kernel/kernel.h
@@ -7,10 +7,9 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-/* Simulator modules */
 #include <memory/memory.h>
 #include <simulator.h>
-/* Generic C++ */
+
 #include <memory>
 
 class Kernel {
@@ -21,12 +20,13 @@ public:
         return std::make_shared<Kernel>();
     }
 
-    void set_simulator( std::shared_ptr<Simulator> s) { sim = s; }
+    void set_simulator( std::shared_ptr<Simulator> s) { sim = std:move( s); }
     void set_memory( std::shared_ptr<FuncMemory> m) { mem = std::move( m); }
     /* Return false if simulator should be stopped, e.g. on 'exit' syscall */
     virtual bool execute() { return true; }
 
     Kernel() = default;
+    virtual ~Kernel() = default;
     Kernel( const Kernel&) = delete;
     Kernel( Kernel&&) = delete;
     Kernel operator=( const Kernel&) = delete;
