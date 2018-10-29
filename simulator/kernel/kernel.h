d6e3744c55e6d39a8081512f5ed6638f38005ed6
trexxet <atomnik07@gmail.com>
2018-10-29 15:48:27 +0300

    Implement Kernel class

---
+++ simulator/kernel/kernel.h
@@ -0,0 +1,35 @@
+/*
+ * kernel.h - base kernel class
+ * @author Vyacheslav Kompan kompan.vo@phystech.edu
+ * Copyright 2015 MIPT-MIPS
+ */
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+/* Simulator modules */
+#include <memory/memory.h>
+#include <simulator.h>
+/* Generic C++ */
+#include <memory>
+
+class Kernel {
+    std::weak_ptr<Simulator> sim;
+    std::shared_ptr<FuncMemory> mem;
+public:
+    static std::shared_ptr<Kernel> create_kernel() {
+        return std::make_shared<Kernel>();
+    }
+
+    void set_simulator( std::shared_ptr<Simulator> sim) { this->sim = sim; }
+    void set_memory( std::shared_ptr<FuncMemory> mem) { this->mem = std::move( mem); }
+    virtual bool execute() { return true; }
+
+    Kernel() = default;
+    Kernel( const Kernel&) = delete;
+    Kernel( Kernel&&) = delete;
+    Kernel operator=( const Kernel&) = delete;
+    Kernel operator=( Kernel&&) = delete;
+};
+
+#endif //KERNEL_H
