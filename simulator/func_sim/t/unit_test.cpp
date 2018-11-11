422fa1e540a7c76743afcd0f43b159a0a437e01a
Arsen Davtyan <davt.arsen@phystech.edu>
2018-11-12 02:12:09 +0300

    Bring back test cases

--- simulator/func_sim/t/unit_test.cpp
+++ simulator/func_sim/t/unit_test.cpp
@@ -41,6 +41,54 @@ TEST_CASE( "FuncSim: get lost without pc")
     CHECK_THROWS_AS( sim.run_no_limit(), BearingLost);
 }
 
+TEST_CASE( "Process_Wrong_Args_Of_Constr: Func_Sim_init_and_load")
+{
+    // Call constructor and init
+    FuncSim<MIPS32> sim;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( valid_elf_file);
+    elf.load_to( mem.get());
+    CHECK_NOTHROW( sim.set_pc( elf.get_startPC()) );
+}
+
+TEST_CASE( "Make_A_Step: Func_Sim")
+{
+    FuncSim<MIPS32> sim;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( valid_elf_file);
+    elf.load_to( mem.get());
+    sim.set_pc( elf.get_startPC());
+
+    CHECK( sim.step().string_dump().find("lui $at, 0x41\t [ $at = 0x410000 ]") != std::string::npos);
+}
+
+TEST_CASE( "FuncSim: make a step with checker")
+{
+    FuncSim<MIPS32> sim;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( valid_elf_file);
+    elf.load_to( mem.get());
+    sim.set_pc( elf.get_startPC());
+    sim.init_checker();
+
+    CHECK( sim.step().string_dump().find("lui $at, 0x41\t [ $at = 0x410000 ]") != std::string::npos);
+}
+
+TEST_CASE( "Run one instruction: Func_Sim")
+{
+    FuncSim<MIPS32> sim;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( smc_code);
+    elf.load_to( mem.get());
+    sim.set_pc( elf.get_startPC());
+
+    CHECK( sim.run( 1) == Trap::NO_TRAP);
+}
+
 TEST_CASE( "FuncSim: Register R/W")
 {
     FuncSim<MIPS32> sim;
@@ -57,4 +105,57 @@ TEST_CASE( "FuncSim: Register size")
 {
     CHECK( FuncSim<MIPS32>().sizeof_register() == bytewidth<uint32>);
     CHECK( FuncSim<MIPS64>().sizeof_register() == bytewidth<uint64>);
+}
+
+TEST_CASE( "Run_SMC_trace: Func_Sim")
+{
+    FuncSim<MIPS32> sim;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( smc_code);
+    elf.load_to( mem.get());
+    sim.set_pc( elf.get_startPC());
+
+    CHECK_NOTHROW( sim.run_no_limit() );
+}
+
+TEST_CASE( "Torture_Test: Func_Sim")
+{
+    // MIPS 32 Little-endian
+    FuncSim<MIPS32> simMIPS32;
+    auto mem = FuncMemory::create_hierarchied_memory();
+    simMIPS32.set_memory( mem);
+
+    ElfLoader elfCoreUniversal( TEST_PATH "/tt.core.universal.out");
+    elfCoreUniversal.load_to( mem.get());
+    simMIPS32.set_pc( elfCoreUniversal.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    ElfLoader elfCore32( TEST_PATH "/tt.core32.out");
+    elfCore32.load_to( mem.get());
+    simMIPS32.set_pc( elfCore32.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    ElfLoader elfCore32le( TEST_PATH "/tt.core32.le.out");
+    elfCore32le.load_to( mem.get());
+    simMIPS32.set_pc( elfCore32le.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    // MIPS 64 Little-Endian
+    FuncSim<MIPS64> simMIPS64;
+    simMIPS64.set_memory( mem);
+
+    elfCoreUniversal.load_to( mem.get());
+    simMIPS64.set_pc( elfCoreUniversal.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
+
+    ElfLoader elfCore64( TEST_PATH "/tt.core64.out");
+    elfCore64.load_to( mem.get());
+    simMIPS64.set_pc( elfCore64.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
+
+    ElfLoader elfCore64le( TEST_PATH "/tt.core64.le.out");
+    elfCore64le.load_to( mem.get());
+    simMIPS64.set_pc( elfCore64le.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
 }
--- simulator/modules/core/t/unit_test.cpp
+++ simulator/modules/core/t/unit_test.cpp
@@ -24,6 +24,41 @@ TEST_CASE( "PerfSim: create empty memory and get lost")
     CHECK_THROWS_AS( sim.run_no_limit(), Deadlock);
 }
 
+TEST_CASE( "Perf_Sim_init: Process_Correct_Args_Of_Constr")
+{
+    // Just call a constructor
+    PerfSim<MIPS32> sim( false);
+    auto mem = FuncMemory::create_hierarchied_memory();
+    CHECK_NOTHROW(sim.set_memory( mem));
+}
+
+TEST_CASE( "Perf_Sim_init: Make_A_Step")
+{
+    // Call constructor and run one instr
+    PerfSim<MIPS32> sim( false);
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( valid_elf_file);
+    elf.load_to( mem.get());
+    sim.init_checker();
+    sim.set_pc( elf.get_startPC());
+    CHECK_NOTHROW( sim.run( 1) );
+}
+
+TEST_CASE( "Perf_Sim_init: Process_Wrong_Args")
+{
+    // Do bad init
+    PerfSim<MIPS32> sim( false);
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( "./1234567890/qwertyuop");
+    elf.load_to( mem.get());
+    sim.init_checker();
+    sim.set_pc( elf.get_startPC());
+
+    CHECK_THROWS_AS( sim.run( 1), InvalidElfFile);
+}
+
 TEST_CASE( "Perf_Sim: Register R/W")
 {
     PerfSim<MIPS32> sim( false);
@@ -42,6 +77,54 @@ TEST_CASE( "Perf_Sim: Register size")
     CHECK( PerfSim<MIPS64>( false).sizeof_register() == bytewidth<uint64>);
 }
 
+TEST_CASE( "Torture_Test: Perf_Sim")
+{
+    // MIPS 32 Little-Endian
+    PerfSim<MIPS32> simMIPS32( false);
+    auto mem = FuncMemory::create_hierarchied_memory();
+    simMIPS32.set_memory( mem);
+
+    ElfLoader elfCoreUniversal( TEST_PATH "/tt.core.universal.out");
+    elfCoreUniversal.load_to( mem.get());
+    simMIPS32.init_checker();
+    simMIPS32.set_pc( elfCoreUniversal.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    ElfLoader elfCore32( TEST_PATH "/tt.core32.out");
+    elfCore32.load_to( mem.get());
+    simMIPS32.init_checker();
+    simMIPS32.set_pc( elfCore32.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    ElfLoader elfCore32le( TEST_PATH "/tt.core32.le.out");
+    elfCore32le.load_to( mem.get());
+    simMIPS32.init_checker();
+    simMIPS32.set_pc( elfCore32le.get_startPC());
+    CHECK_NOTHROW( simMIPS32.run_no_limit() );
+
+    // MIPS 64 Little-Endian
+    PerfSim<MIPS64> simMIPS64( false);
+    simMIPS64.set_memory( mem);
+
+    elfCoreUniversal.load_to( mem.get());
+    simMIPS64.init_checker();
+    simMIPS64.set_pc( elfCoreUniversal.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
+
+    ElfLoader elfCore64( TEST_PATH "/tt.core64.out");
+    elfCore64.load_to( mem.get());
+    simMIPS64.init_checker();
+    simMIPS64.set_pc( elfCore64.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
+
+    ElfLoader elfCore64le( TEST_PATH "/tt.core64.le.out");
+    elfCore64le.load_to( mem.get());
+    simMIPS64.init_checker();
+    simMIPS64.set_pc( elfCore64le.get_startPC());
+    CHECK_NOTHROW( simMIPS64.run_no_limit() );
+
+}
+
 TEST_CASE( "Perf_Sim: Run_SMC_Trace_WithoutChecker")
 {
     PerfSim<MIPS32> sim( false);
@@ -51,4 +134,17 @@ TEST_CASE( "Perf_Sim: Run_SMC_Trace_WithoutChecker")
     elf.load_to( mem.get());
     sim.set_pc( elf.get_startPC());
     CHECK( sim.run_no_limit( ) == Trap::NO_TRAP);
+}
+
+TEST_CASE( "Perf_Sim: Run_SMC_Trace_WithChecker")
+{
+    PerfSim<MIPS32> sim( false);
+    auto mem = FuncMemory::create_hierarchied_memory();
+    sim.set_memory( mem);
+    ElfLoader elf( smc_code);
+    elf.load_to( mem.get());
+    sim.init_checker();
+    sim.set_pc( elf.get_startPC());
+
+    CHECK_THROWS_AS( sim.run_no_limit( ), CheckerMismatch);
 }
