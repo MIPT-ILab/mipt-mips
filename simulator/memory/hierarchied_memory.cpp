1124bc95cf949a32dd09fa8d1e33bac9eeb78915
Pavel I. Kryukov <kryukov@frtk.ru>
2018-11-16 19:23:42 +0300

    Update hierarchied_memory.cpp

--- simulator/memory/hierarchied_memory.cpp
+++ simulator/memory/hierarchied_memory.cpp
@@ -27,7 +27,7 @@ class HierarchiedMemory : public FuncMemory
         std::string dump() const final;
         size_t memcpy_host_to_guest( Addr dst, const Byte* src, size_t size) final;
         size_t memcpy_guest_to_host( Byte* dst, Addr src, size_t size) const noexcept final;
-        void duplicate_to( std::shared_ptr<FuncMemory> target) const final;
+        void duplicate_to( std::shared_ptr<WriteableMemory> target) const final;
 
     private:
         const uint32 page_bits;
