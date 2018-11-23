d5ae96983fb7cab957e1145be512acbce4e824ba
Pavel I. Kryukov <kryukov@frtk.ru>
2018-11-24 01:00:22 +0300

    Remove some complicated PerfSim tests

--- simulator/modules/core/t/unit_test.cpp
+++ simulator/modules/core/t/unit_test.cpp
@@ -13,16 +13,7 @@
 #include <mips/mips.h>
 #include <modules/writeback/writeback.h>
 
-static const std::string valid_elf_file = TEST_PATH "/tt.core32.out";
 static const std::string smc_code = TEST_PATH "/smc.out";
-
-TEST_CASE( "PerfSim: create empty memory and get lost")
-{
-    auto m = FuncMemory::create_hierarchied_memory();
-    PerfSim<MIPS32> sim( false);
-    sim.set_memory( m);
-    CHECK_THROWS_AS( sim.run_no_limit(), Deadlock);
-}
 
 TEST_CASE( "Perf_Sim_init: Process_Correct_Args_Of_Constr")
 {
@@ -32,29 +23,36 @@ TEST_CASE( "Perf_Sim_init: Process_Correct_Args_Of_Constr")
     CHECK_NOTHROW(sim.set_memory( mem));
 }
 
-TEST_CASE( "Perf_Sim_init: Make_A_Step")
+TEST_CASE( "Perf_Sim_init: push a nop")
 {
-    // Call constructor and run one instr
     PerfSim<MIPS32> sim( false);
     auto mem = FuncMemory::create_hierarchied_memory();
     sim.set_memory( mem);
-    ElfLoader elf( valid_elf_file);
-    elf.load_to( mem.get());
     sim.init_checker();
-    sim.set_pc( elf.get_startPC());
+    sim.set_pc( 0x10);
     CHECK_NOTHROW( sim.run( 1) );
 }
 
-TEST_CASE( "Perf_Sim: Register R/W")
+TEST_CASE( "PerfSim: create empty memory and get lost")
+{
+    auto m = FuncMemory::create_hierarchied_memory();
+    PerfSim<MIPS32> sim( false);
+    sim.set_memory( m);
+    CHECK_THROWS_AS( sim.run_no_limit(), Deadlock);
+}
+
+TEST_CASE( "Perf_Sim: unsigned register R/W")
 {
     PerfSim<MIPS32> sim( false);
-
-    /* Signed */
-    sim.write_cpu_register( 1, narrow_cast<uint64>( -1337));
-    CHECK( narrow_cast<int32>( sim.read_cpu_register( 1)) == -1337 );
-    /* Unsigned */
     sim.write_cpu_register( 1, uint64{ MAX_VAL32});
     CHECK( sim.read_cpu_register( 1) == MAX_VAL32 );
+}
+
+TEST_CASE( "Perf_Sim: signed register R/W")
+{
+    PerfSim<MIPS32> sim( false);
+    sim.write_cpu_register( 1, narrow_cast<uint64>( -1337));
+    CHECK( narrow_cast<int32>( sim.read_cpu_register( 1)) == -1337 );
 }
 
 TEST_CASE( "Perf_Sim: Register size")
@@ -68,7 +66,7 @@ TEST_CASE( "Torture_Test: Perf_Sim , MIPS 64, Core 64")
     PerfSim<MIPS64> sim( false);
     auto mem = FuncMemory::create_hierarchied_memory();
     sim.set_memory( mem);
-    ElfLoader elf( TEST_PATH "/tt.core64.out");
+    ElfLoader elf( valid_elf_file);
     elf.load_to( mem.get());
     sim.init_checker();
     sim.set_pc( elf.get_startPC());
