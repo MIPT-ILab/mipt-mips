c77a906017d39e3cfdb04708be1ff2fb75478905
Pavel I. Kryukov <kryukov@frtk.ru>
2018-11-24 21:22:19 +0000

    Handle MARS syscalls 13,14,15,16: file I/O

--- simulator/kernel/mars/mars_kernel.cpp
+++ simulator/kernel/mars/mars_kernel.cpp
@@ -8,6 +8,8 @@
 
 #include <fstream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 class MARSKernel : public Kernel {
     void print_integer();
@@ -16,10 +18,23 @@ class MARSKernel : public Kernel {
     void read_character();
     void print_string();
     void read_string();
+    void open_file();
+    void read_from_file();
+    void write_to_file();
+    void close_file();
+
+    std::fstream* find_user_file_by_descriptor( uint64 descriptor);
+    std::istream* find_in_file_by_descriptor( uint64 descriptor);
+    std::ostream* find_out_file_by_descriptor( uint64 descriptor);
+    void io_failure();
 
     std::istream& instream;
     std::ostream& outstream;
     std::ostream& errstream;
+
+    std::unordered_map<uint64, std::fstream> files;
+    static const constexpr uint64 first_user_descriptor = 3;
+    uint64 next_descriptor = first_user_descriptor;
 
 public:
     SyscallResult execute() final;
@@ -35,9 +50,9 @@ std::shared_ptr<Kernel> create_mars_kernel( std::istream& instream, std::ostream
 static const constexpr uint8 v0 = 2;
 static const constexpr uint8 a0 = 4;
 static const constexpr uint8 a1 = 5;
+static const constexpr uint8 a2 = 6;
 
 SyscallResult MARSKernel::execute () {
-    (void)errstream; // w/a for Clang warning
     auto cpu = sim.lock();
     uint64 syscall_code = sim.lock()->read_cpu_register( v0);
     switch (syscall_code) {
@@ -48,6 +63,10 @@ SyscallResult MARSKernel::execute () {
         case 10: return { SyscallResult::HALT, 0};
         case 11: print_character(); break;
         case 12: read_character(); break;
+        case 13: open_file(); break;
+        case 14: read_from_file(); break;
+        case 15: write_to_file(); break;
+        case 16: close_file(); break;
         case 17: return { SyscallResult::HALT, cpu->read_cpu_register( a0)};
         default: return { SyscallResult::UNSUPPORTED, 0};
     }
@@ -108,3 +127,112 @@ void MARSKernel::read_string() {
     mem->write_string_limited( input, buffer_ptr, chars_to_read);
 }
 
+static auto get_openmode( uint64 value) {
+    switch ( value) {
+    case 0: return std::ios_base::in  | std::ios_base::binary;
+    case 1: return std::ios_base::out | std::ios_base::binary;
+    case 9: return std::ios_base::out | std::ios_base::binary | std::ios_base::app;
+    default: return std::ios_base::openmode{};
+    }
+}
+
+void MARSKernel::io_failure()
+{
+    sim.lock()->write_cpu_register( v0, all_ones<uint64>());
+}
+
+void MARSKernel::open_file() {
+    auto cpu = sim.lock();
+    uint64 filename_ptr = cpu->read_cpu_register( a0);
+    uint64 flags = cpu->read_cpu_register( a1);
+    auto filename = mem->read_string( filename_ptr);
+    std::fstream file( filename, get_openmode( flags));
+    if ( !file.is_open()) {
+        io_failure();
+        return;
+    }
+
+    files.emplace( next_descriptor, std::move( file));
+    cpu->write_cpu_register( v0, next_descriptor);
+    ++next_descriptor;
+}
+
+void MARSKernel::close_file() {
+    auto cpu = sim.lock();
+    uint64 descriptor = cpu->read_cpu_register( a0);
+    if ( descriptor < first_user_descriptor)
+        return;
+
+    auto it = files.find( descriptor);
+    if ( it == files.end())
+        return;
+
+    files.erase( it);
+}
+
+std::fstream* MARSKernel::find_user_file_by_descriptor(uint64 descriptor) {
+    auto it = files.find( descriptor);
+    return it == files.end() ? nullptr : &(it->second);
+}
+
+std::istream* MARSKernel::find_in_file_by_descriptor(uint64 descriptor) {
+    switch ( descriptor) {
+    case 0:  return &instream;
+    case 1:  return nullptr;
+    case 2:  return nullptr;
+    default: return find_user_file_by_descriptor( descriptor);
+    }
+}
+
+std::ostream* MARSKernel::find_out_file_by_descriptor(uint64 descriptor) {
+    switch ( descriptor) {
+    case 0:  return nullptr;
+    case 1:  return &outstream;
+    case 2:  return &errstream;
+    default: return find_user_file_by_descriptor( descriptor);
+    }
+}
+
+void MARSKernel::write_to_file() {
+    auto cpu = sim.lock();
+    uint64 descriptor = cpu->read_cpu_register( a0);
+    uint64 buffer_ptr = cpu->read_cpu_register( a1);
+    uint64 chars_to_write = cpu->read_cpu_register( a2);
+    auto file = find_out_file_by_descriptor( descriptor);
+    if (file == nullptr) {
+        io_failure();
+        return;
+    }
+
+    auto data = mem->read_string_limited( buffer_ptr, chars_to_write);
+    auto current_pos = file->tellp();
+    file->write( data.c_str(), data.size());
+    if ( !file->bad())
+        cpu->write_cpu_register( v0, file->tellp() - current_pos);
+    else
+        io_failure();
+}
+
+void MARSKernel::read_from_file() {
+    auto cpu = sim.lock();
+    uint64 descriptor = cpu->read_cpu_register( a0);
+    uint64 buffer_ptr = cpu->read_cpu_register( a1);
+    uint64 chars_to_read = cpu->read_cpu_register( a2);
+    auto file = find_in_file_by_descriptor( descriptor);
+    if (file == nullptr) {
+        io_failure();
+        return;
+    }
+
+    std::vector<char> buffer( chars_to_read);
+    auto current_pos = file->tellg();
+    file->read( buffer.data(), chars_to_read);
+    if ( file->bad()) {
+        io_failure();
+        return;
+    }
+
+    auto chars_read = file->tellg() - current_pos;
+    cpu->write_cpu_register( v0, chars_read);
+    mem->memcpy_host_to_guest( buffer_ptr, reinterpret_cast<const Byte*>( buffer.data()), chars_to_read);
+}
