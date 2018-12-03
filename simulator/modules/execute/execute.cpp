1cee71921f7797c844b216f38acd61a705e9a6d5
Yauheni Sharamed <SHaramed.EI@phystech.edu>
2018-12-03 13:39:34 +0300

    Rename prediction port to branch port

--- simulator/modules/execute/execute.cpp
+++ simulator/modules/execute/execute.cpp
@@ -19,7 +19,7 @@ Execute<ISA>::Execute( bool log)
 {
     wp_mem_datapath = make_write_port<Instr>("EXECUTE_2_MEMORY", PORT_BW, PORT_FANOUT);
     wp_writeback_datapath = make_write_port<Instr>("EXECUTE_2_WRITEBACK", PORT_BW, PORT_FANOUT);
-    wp_prediction_datapath = make_write_port<Instr>("EXECUTE_2_BRANCH", PORT_BW, PORT_FANOUT);
+    wp_branch_datapath = make_write_port<Instr>("EXECUTE_2_BRANCH", PORT_BW, PORT_FANOUT);
     rp_datapath = make_read_port<Instr>("DECODE_2_EXECUTE", PORT_LATENCY);
 
     if (config::long_alu_latency < 2)
@@ -126,7 +126,7 @@ void Execute<ISA>::clock( Cycle cycle)
         if ( instr.is_mem_stage_required())
         {
             wp_mem_datapath->write( std::move( instr), cycle);
-            wp_prediction_datapath->write( std::move( instr) ,cycle);
+            wp_branch_datapath->write( std::move( instr) ,cycle);
         }
         else
             wp_writeback_datapath->write( std::move( instr), cycle);
