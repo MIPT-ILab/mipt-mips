d623fab517c043a33ff77904c24af6c5f119c7b8
Pavel I. Kryukov <kryukov@frtk.ru>
2019-01-31 23:40:58 +0300

    Include popl.hpp to a single file to increase compilation speed (#776)

--- simulator/infra/config/config.cpp
+++ simulator/infra/config/config.cpp
@@ -3,9 +3,9 @@
  * Copyright 2017-2018 MIPT-MIPS
  */
 
+ 
+#include <popl.hpp>
 #include <sstream>
-
-/* Simulator modules */
 #include "config.h"
 
 namespace config {
@@ -21,5 +21,39 @@ void handleArgs( int argc, const char* const argv[], int start_index)
         throw HelpOption( BaseValue::options().help());
 }
 
-} // namespace config
+popl::OptionParser& BaseValue::options()
+{
+    static popl::OptionParser instance( "Allowed options");
+    return instance;
+}
 
+template<typename T>
+AliasedRequiredValue<T>::AliasedRequiredValue( const std::string& alias, const std::string& name, const std::string& desc)
+    : BaseTValue<T>( T())
+{
+    BaseValue::options().template add<popl::Value<T>, popl::Attribute::required>(alias, name, desc, T(), &this->value);
+}
+
+template<typename T>
+AliasedValue<T>::AliasedValue( const std::string& alias, const std::string& name, const T& val, const std::string& desc)
+    : BaseTValue<T>( val)
+{
+    BaseValue::options().template add<popl::Value<T>>(alias, name, desc, val, &this->value);
+}
+
+AliasedSwitch::AliasedSwitch( const std::string& alias, const std::string& name, const std::string& desc)
+    : BaseTValue<bool>( false)
+{
+    BaseValue::options().add<popl::Switch>(alias, name, desc, &this->value);
+}
+
+template struct AliasedRequiredValue<std::string>;
+template struct AliasedValue<std::string>;
+
+template struct AliasedRequiredValue<uint64>;
+template struct AliasedValue<uint64>;
+
+template struct AliasedRequiredValue<uint32>;
+template struct AliasedValue<uint32>;
+
+} // namespace config
