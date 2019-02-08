2d39e1daf6d4e8c5f69919fe428e533930caccbb
Pavel I. Kryukov <kryukov@frtk.ru>
2019-02-08 16:00:12 +0300

    Update writeback.cpp

--- simulator/modules/writeback/writeback.cpp
+++ simulator/modules/writeback/writeback.cpp
@@ -43,7 +43,7 @@ auto Writeback<ISA>::read_instructions( Cycle cycle)
 {
     auto ports = { rp_branch_datapath.get(), rp_mem_datapath.get(), rp_execute_datapath.get() };
     std::vector<Instr> result;
-    result.reserve( ports.size());
+    result.reserve( 1); // depends on WB througput
 
     for ( auto& port : ports)
         if ( port->is_ready( cycle))
