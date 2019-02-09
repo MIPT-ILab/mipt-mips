1afad7fb8361ac3eb04c6e5ed12f49fd1f4acfe4
Pavel I. Kryukov <kryukov@frtk.ru>
2019-02-09 14:16:52 +0300

    Support alignment by PortQueue

--- simulator/infra/ports/port_queue/port_queue.h
+++ simulator/infra/ports/port_queue/port_queue.h
@@ -10,19 +10,25 @@
 #ifndef PORT_QUEUE_H
 #define PORT_QUEUE_H
 
+#include <boost/align/aligned_alloc.hpp>
 #include <memory>
 #include <type_traits>
 
 template<typename T>
 class PortQueue
 {
+    void* allocate( size_t size)
+    {
+        return boost::alignment::aligned_alloc( alignof(T), sizeof(T) * size);
+    }
+
     struct Deleter
     {
-        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-no-malloc, hicpp-no-malloc)
-        void operator()(T *p) { std::free(p); }
+        void operator()(void *p) { boost::alignment::aligned_free(p); }
     };
 
-    std::unique_ptr<T, Deleter> arena = nullptr;
+    std::unique_ptr<void, Deleter> arena = nullptr;
+    T* arena_start = nullptr;
     const T* arena_end = nullptr;
     T* p_front = nullptr;
     T* p_back = nullptr;
@@ -33,7 +39,7 @@ class PortQueue
         // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         ++( this->*p);
         if (this->*p == arena_end)
-            this->*p = arena.get();
+            this->*p = arena_start;
     }
 
     void clear()
@@ -58,10 +64,10 @@ public:
     void resize( size_t size)
     {
         clear();
-        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, hicpp-no-malloc)
-        arena = std::unique_ptr<T, Deleter>(static_cast<T*>(std::malloc(sizeof(T) * size)));
-        arena_end = arena.get() + size;
-        p_front = p_back = arena.get();
+        arena = std::unique_ptr<void, Deleter>( allocate( size));
+        arena_start = static_cast<T*>( arena.get());
+        arena_end = arena_start + size;
+        p_front = p_back = arena_start;
         occupied = 0;
     }
 
@@ -74,7 +80,7 @@ public:
 
     bool full() const noexcept
     {
-        return arena.get() + occupied == arena_end;
+        return arena_start + occupied == arena_end;
     }
 
     void pop() noexcept
