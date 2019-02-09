65f8c2bd05f9e877b46322844573916fb44361c2
Pavel I. Kryukov <kryukov@frtk.ru>
2019-02-10 01:40:10 +0300

    Use arena allocation for LRUCache
    std::unordered_map can perform badly if the stored value is a heavy structure
    due to relocations and moreover alignment problems. Therefore, we shall keep
    the data in a memory arena, while std::unordered_map simply has to handle
    the indices.

--- simulator/infra/instrcache/LRUCache.h
+++ simulator/infra/instrcache/LRUCache.h
@@ -8,19 +8,26 @@
 
 #include <cassert>
 #include <list>
+#include <memory>
 #include <unordered_map>
 #include <utility>
 
+#include <boost/align/aligned_alloc.hpp>
 #include <infra/types.h>
 
 template <typename Key, typename Value, size_t CAPACITY>
 class LRUCache
 {
+        struct Deleter;
     public:
         LRUCache()
         {
             data.reserve( CAPACITY);
             lru_hash.reserve( CAPACITY);
+            for (size_t i = 0; i < CAPACITY; ++i)
+                free_list.emplace_back(i);
+            arena = std::unique_ptr<void, Deleter>( allocate_memory());
+            storage = static_cast<Value*>( arena.get());
         }
 
         static auto get_capacity() { return CAPACITY; }
@@ -33,7 +40,9 @@ class LRUCache
         auto find( const Key& key) const
         {
             auto result = data.find( key);
-            return std::pair<bool, const Value&>( result != data.end(), result->second);
+            bool found = result != data.end();
+            size_t index = found ? result->second : CAPACITY;
+            return std::pair<bool, const Value&>(found, storage[index]);
         }
 
         void touch( const Key& key)
@@ -57,6 +66,8 @@ class LRUCache
             if ( data_it != data.end())
             {
                 assert( !empty());
+                free_list.emplace_front( data_it->second);
+                storage[data_it->second].~Value();
                 data.erase( data_it);
                 lru_list.erase( lru_it->second);
                 lru_hash.erase( lru_it);
@@ -70,14 +81,30 @@ class LRUCache
                 erase( lru_list.back());
 
             // Add a new element
-            data.emplace( key, value);
+            assert( !free_list.empty());
+            auto index = free_list.front();
+            free_list.pop_front();
+            new (&storage[index]) Value( value);
+            data.emplace( key, index);
             auto ptr = lru_list.insert( lru_list.begin(), key);
             lru_hash.emplace( key, ptr);
         }
 
-        std::unordered_map<Key, Value> data{};
+        static void* allocate_memory()
+        {
+            return boost::alignment::aligned_alloc( alignof(Value), sizeof(Value) * CAPACITY);
+        }
 
+        struct Deleter
+        {
+            void operator()(void *p) { boost::alignment::aligned_free(p); }
+        };
+
+        std::unordered_map<Key, size_t> data{};
         std::list<Key> lru_list{};
+        std::list<size_t> free_list{};
+        std::unique_ptr<void, Deleter> arena = nullptr;
+        Value* storage = nullptr;
         std::unordered_map<Key, typename std::list<Key>::const_iterator> lru_hash{};
 };
 
