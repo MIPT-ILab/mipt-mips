9b9aab864ebf680ed7741bc4c72c78a6a68b6e4f
Pavel I. Kryukov <kryukov@frtk.ru>
2019-02-09 14:44:24 +0300

    Align BaseMIPSInstr to 64 bytes; support by PortQueue (#824)

--- simulator/infra/ports/port_queue/port_queue.h
+++ simulator/infra/ports/port_queue/port_queue.h
@@ -10,19 +10,29 @@
 #ifndef PORT_QUEUE_H
 #define PORT_QUEUE_H
 
+// std::aligned_alloc is available only in GCC 7.4
+// see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=83662
+#include <stdlib.h>
 #include <memory>
 #include <type_traits>
 
 template<typename T>
 class PortQueue
 {
+    void* allocate( size_t size)
+    {
+        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, hicpp-no-malloc)
+        return ::aligned_alloc( alignof(T), sizeof(T) * size);
+    }
+
     struct Deleter
     {
         // NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-no-malloc, hicpp-no-malloc)
-        void operator()(T *p) { std::free(p); }
+        void operator()(void *p) { std::free(p); }
     };
 
-    std::unique_ptr<T, Deleter> arena = nullptr;
+    std::unique_ptr<void, Deleter> arena = nullptr;
+    T* arena_start = nullptr;
     const T* arena_end = nullptr;
     T* p_front = nullptr;
     T* p_back = nullptr;
@@ -33,7 +43,7 @@ class PortQueue
         // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         ++( this->*p);
         if (this->*p == arena_end)
-            this->*p = arena.get();
+            this->*p = arena_start;
     }
 
     void clear()
@@ -58,10 +68,10 @@ public:
     void resize( size_t size)
     {
         clear();
-        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, hicpp-no-malloc)
-        arena = std::unique_ptr<T, Deleter>(static_cast<T*>(std::malloc(sizeof(T) * size)));
-        arena_end = arena.get() + size;
-        p_front = p_back = arena.get();
+        arena = std::unique_ptr<void, Deleter>( allocate( size));
+        arena_start = static_cast<T*>( arena.get());
+        arena_end = arena_start + size;
+        p_front = p_back = arena_start;
         occupied = 0;
     }
 
@@ -74,7 +84,7 @@ public:
 
     bool full() const noexcept
     {
-        return arena.get() + occupied == arena_end;
+        return arena_start + occupied == arena_end;
     }
 
     void pop() noexcept
