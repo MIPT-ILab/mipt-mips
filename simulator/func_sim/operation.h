a3b2632a1b210ee63befb5c19d92f538f34da81c
Pavel I. Kryukov <kryukov@frtk.ru>
2019-02-23 19:33:09 +0300

    Create operation.h

---
+++ simulator/func_sim/operation.h
@@ -0,0 +1,33 @@
+/**
+ * operation.h - list of basic operation types
+ * @author Pavel Kryukov pavel.kryukov@phystech.edu
+ * Copyright 2019 MIPT-MIPS
+ */
+ 
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#include <infra/types.h>
+
+enum OperationType : uint8
+{
+    OUT_ARITHM,
+    OUT_R_ACCUM,
+    OUT_R_CONDM,
+    OUT_R_JUMP,
+    OUT_SYSCALL,
+    OUT_BREAK,
+    OUT_R_SUBTR,
+    OUT_BRANCH,
+    OUT_TRAP,
+    OUT_LOAD,
+    OUT_LOADU,
+    OUT_PARTIAL_LOAD,
+    OUT_STORE,
+    OUT_J_JUMP,
+    OUT_J_SPECIAL,
+    OUT_UNKNOWN
+};
+
+#endif // OPERATION_H
+ 
