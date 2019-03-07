91f535c30576fa132fe0d7a6feab63320fc15f36
Pavel I. Kryukov <kryukov@frtk.ru>
2019-03-08 00:40:13 +0300

    Update unit_test.cpp

--- simulator/func_sim/t/unit_test.cpp
+++ simulator/func_sim/t/unit_test.cpp
@@ -34,18 +34,18 @@ TEST_CASE( "FuncSim MIPS32: create empty memory and get lost")
     CHECK_THROWS_AS( sim.run_no_limit(), BearingLost);
 }
 
-TEST_CASE( "FuncSim RISC-V: create empty memory and find unknown instruction")
+TEST_CASE( "FuncSim RISC-V: create empty memory and get lost")
 {
     FuncSim<RISCV32> sim( false);
     sim.set_memory( FuncMemory::create_hierarchied_memory());
-    CHECK( sim.run_until_trap( 200) == Trap::UNKNOWN_INSTRUCTION);
+    CHECK_THROWS_AS( sim.run_no_limit(), BearingLost);
 }
 
-TEST_CASE( "FuncSim RISC-V 128 bit: create empty memory and find unknown instruction")
+TEST_CASE( "FuncSim RISC-V 128 bit: create empty memory and get lost")
 {
     FuncSim<RISCV128> sim( false);
     sim.set_memory( FuncMemory::create_hierarchied_memory());
-    CHECK( sim.run_until_trap( 200) == Trap::UNKNOWN_INSTRUCTION);
+    CHECK_THROWS_AS( sim.run_no_limit(), BearingLost);
 }
 
 TEST_CASE( "FuncSim: get lost without pc")
