fb0c98072c828c1dc83eff58a1aa9af9094f76ba
Pukhov Vsevolod <pukhov.vyu@phystech.edu>
2019-04-01 02:28:38 +0300

    (#602) New trap_types interface prototype

--- simulator/func_sim/trap_types.h
+++ simulator/func_sim/trap_types.h
@@ -9,17 +9,61 @@
 // MIPT-MIPS modules
 #include <infra/types.h>
 
-enum class Trap : uint8
+#include <unordered_map>
+
+
+// Now there are no such enumeration in mipt-mips
+enum GDB_TrapType : uint8
 {
-    NO_TRAP,
-    HALT,
-    EXPLICIT_TRAP,
-    BREAKPOINT,
-    SYSCALL,
-    INTEGER_OVERFLOW,
-    UNSUPPORTED_SYSCALL,
-    UNALIGNED_ADDRESS,
-    UNKNOWN_INSTRUCTION
+    GDB_SIGNAL_0    = 0,
+    GDB_SIGNAL_TRAP = 5,
+    GDB_SIGNAL_SYS  = 12,
+    GDB_SIGNAL_BUS  = 10,
+    GDB_SIGNAL_ILL  = 4,
+};
+
+
+class Trap {
+    public:
+        enum TrapType : uint8
+        {
+            NO_TRAP,
+            HALT,
+            EXPLICIT_TRAP,
+            BREAKPOINT,
+            SYSCALL,
+            INTEGER_OVERFLOW,
+            UNSUPPORTED_SYSCALL,
+            UNALIGNED_ADDRESS,
+            UNKNOWN_INSTRUCTION
+        };
+
+        constexpr Trap(TrapType id) : value(id) { }
+
+        bool operator==(Trap trap) const { return value == trap.value; }
+        bool operator!=(Trap trap) const { return value != trap.value; }
+
+        uint8 to_gdb_format()
+        {
+            static const std::unordered_map<TrapType, uint8> gdb_trap_conv =
+            {
+                { Trap::NO_TRAP,             GDB_SIGNAL_0    },  // New
+                { Trap::EXPLICIT_TRAP,       GDB_SIGNAL_TRAP },
+                { Trap::BREAKPOINT,          GDB_SIGNAL_TRAP },
+                { Trap::UNSUPPORTED_SYSCALL, GDB_SIGNAL_SYS  },  // Not implemented in gdb_interface
+                { Trap::UNALIGNED_ADDRESS,   GDB_SIGNAL_BUS  },
+                { Trap::UNKNOWN_INSTRUCTION, GDB_SIGNAL_ILL  },  // Not implemented in gdb_interface
+            }
+    };
+
+    auto it = gdb_trap_conv.find( value);
+    if ( it == gdb_trap_conv.end())
+        return GDB_SIGNAL_0;
+    return it->second;
+} 
+
+    private:
+        TrapType value;
 };
 
 #endif //TRAP_TYPES_H
