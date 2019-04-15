0a3684209337218a2beaf8ba6732243b9337c9c2
Pavel I. Kryukov <kryukov@frtk.ru>
2019-04-16 00:35:49 +0300

    Extract Arena from InstrCache and PortQueue

---
+++ simulator/infra/arena.h
@@ -0,0 +1,73 @@
+/**
+ * arena.h - non-allocating container.
+ * Used for critical units with type erasure
+ * to speed up compilation time;
+ * therefore, we use C-style memory
+ * management here.
+ * @author Pavel Kryukov
+ * Copyright 2019 MIPT-MIPS team
+ */
+ 
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <cstdlib>
+#include <memory>
+
+// WARNING
+// This is not a RAII class. You have to destroy objects manually!
+
+template<typename T>
+class Arena
+{
+public:
+    void allocate( std::size_t capacity)
+    {
+        arena = std::unique_ptr<void, Deleter>( allocate_memory( capacity));
+        storage = get_aligned_storage( capacity);
+    }
+
+    template<typename... Args> void emplace( std::size_t position, Args&& ... args)
+        noexcept( std::is_nothrow_constructible<T, Args...>::value)
+    {
+        new (storage + position) T( std::forward<Args>( args)...);
+    }
+
+    void destroy( std::size_t position)
+    {
+        (storage + position)->~T();
+    }
+
+    const T& operator[]( std::size_t position) const noexcept
+    {
+        return storage[position];
+    }
+private:
+    static constexpr std::size_t get_space( std::size_t capacity) noexcept
+    {
+        return sizeof(T) * capacity;
+    }
+
+    auto get_aligned_storage( std::size_t capacity) const noexcept
+    {
+        void* ptr = arena.get();
+        auto space = get_space( capacity);
+        auto space_margin = space + sizeof(T);
+        return static_cast<T*>(std::align( alignof(T), space, ptr, space_margin));
+    }
+
+    static void* allocate_memory( std::size_t capacity)
+    {
+        return std::malloc( get_space( capacity) + sizeof(T));
+    }
+
+    struct Deleter
+    {
+        void operator()(void *p) { std::free(p); }
+    };
+
+    std::unique_ptr<void, Deleter> arena = nullptr;
+    T* storage = nullptr;
+};
+
+#endif // ARENA_H
--- simulator/infra/instrcache/instr_cache.h
+++ simulator/infra/instrcache/instr_cache.h
@@ -6,36 +6,33 @@
 #ifndef INSTRCACHE_H
 #define INSTRCACHE_H
 
+#include <bitset>
 #include <cassert>
-#include <list>
 #include <memory>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 
+#include <infra/arena.h>
 #include <infra/types.h>
 #include <infra/replacement/cache_replacement.h>
 
 template <typename Key, typename Value, size_t CAPACITY>
 class InstrCache
 {
-        struct Deleter;
     public:
         InstrCache()
         {
             lru_module = create_cache_replacement( "LRU", CAPACITY);
             data.reserve( CAPACITY);
-            for (size_t i = 0; i < CAPACITY; ++i)
-                free_list.emplace_back(i);
-            arena = std::unique_ptr<void, Deleter>( allocate_memory());
-            void* ptr = arena.get();
-            size_t space = sizeof(Value) * (CAPACITY + 1);
-            storage = static_cast<Value*>(std::align( alignof(Value), sizeof(Value) * CAPACITY, ptr, space));
+            storage.allocate( CAPACITY);
         }
 
         ~InstrCache()
         {
-            for (const auto& e : data)
-                storage[ e.second].~Value();
+            for ( size_t i = 0; i < CAPACITY; ++i)
+                if ( valid_elements[i])
+                    storage.destroy( i);
         }
 
         InstrCache(const InstrCache&) = delete;
@@ -76,8 +73,8 @@ class InstrCache
             auto data_it = data.find( key);
             if ( data_it != data.end())
             {
-                free_list.emplace_front( data_it->second);
-                storage[data_it->second].~Value();
+                storage.destroy( data_it->second);
+                valid_elements[ data_it->second] = false;
                 data.erase( data_it);
                 lru_module->set_to_erase( key);
             }
@@ -91,27 +88,25 @@ class InstrCache
                 erase( narrow_cast<Key>( lru_module->update()));
 
             // Add a new element
-            auto index = free_list.front();
-            free_list.pop_front();
-            new (&storage[index]) Value( value);
+            auto index = get_first_empty();
+            storage.emplace( index, value);
+            valid_elements[index] = true;
             data.emplace( key, index);
             lru_module->allocate( key);
         }
 
-        static void* allocate_memory()
+        auto get_first_empty() const noexcept
         {
-            return std::malloc( sizeof(Value) * (CAPACITY + 1));
+            for ( size_t i = 0; i < CAPACITY; ++i)
+                if ( !valid_elements[i])
+                    return i;
+
+            return CAPACITY;
         }
 
-        struct Deleter
-        {
-            void operator()(void *p) { std::free(p); }
-        };
-
         std::unordered_map<Key, size_t> data{};
-        std::list<size_t> free_list{};
-        std::unique_ptr<void, Deleter> arena = nullptr;
-        Value* storage = nullptr;
+        Arena<Value> storage{};
+        std::bitset<CAPACITY> valid_elements{};
         std::unique_ptr<CacheReplacementInterface> lru_module;
 };
 
