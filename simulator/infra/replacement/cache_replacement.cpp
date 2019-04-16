63e55b893d15e26b6d261c68a6b402ee80b877a8
agrachiv99 <agrachiv99@gmail.com>
2019-04-16 07:36:29 +0300

    Implement pseudo-LRU into functional simulator

--- simulator/infra/replacement/cache_replacement.cpp
+++ simulator/infra/replacement/cache_replacement.cpp
@@ -9,12 +9,13 @@
 #include <list>
 #include <unordered_map>
 #include <cassert>
+#include <cmath>
+#include "tree.hh"
 
 class LRUCacheInfo : public CacheReplacementInterface
 {
     public:
         explicit LRUCacheInfo( std::size_t ways);
-
         void touch( std::size_t way) override ;
         void set_to_erase( std::size_t way) override ;
         void allocate( std::size_t way) override ;
@@ -32,7 +33,6 @@ LRUCacheInfo::LRUCacheInfo( std::size_t ways)
     : ways( ways)
 {
     assert( ways != 0u);
-
     lru_hash.reserve( ways);
 
     for ( std::size_t i = 0; i < ways; i++)
@@ -84,10 +84,113 @@ void LRUCacheInfo::erase_lru_element() {
     lru_hash.erase( lru_elem);
 }
 
+////////////////////////////////////////////////////////////////////////////////////
+
+class Pseudo_LRUCacheInfo : public CacheReplacementInterface
+{
+    public:
+        explicit Pseudo_LRUCacheInfo( std::size_t ways);
+        void touch( std::size_t way) override;
+        std::size_t update() override;
+        std::size_t get_ways() const override { return ways; }
+
+        void set_to_erase( std::size_t ) override;
+        void allocate( std::size_t ) override;
+
+    private:
+        enum flags { LEFT, RIGHT};
+
+        tree<std::size_t> lru_tree;
+        void construct_tree( tree<std::size_t>::iterator node_it, int depth);
+        void construct_leaf_layer();
+        int calculate_depth() const;
+        std::size_t reverse_flag( std::size_t flag);
+
+        const std::size_t ways;
+        size_t leaf_iterator = 0;
+};
+
+Pseudo_LRUCacheInfo::Pseudo_LRUCacheInfo( std::size_t ways)
+    : ways( ways)
+{
+    lru_tree.insert( lru_tree.begin(), LEFT);
+    construct_tree( lru_tree.begin(), calculate_depth());
+}
+
+int Pseudo_LRUCacheInfo::calculate_depth() const
+{
+    int i = 0;
+    for (; pow(2, i) < ways; ++i);
+    return i;
+}
+
+void Pseudo_LRUCacheInfo::construct_tree( tree<std::size_t>::iterator node_it, int depth)
+{
+    if (lru_tree.depth( node_it) < depth)
+    {
+        if ( lru_tree.depth( node_it) != depth - 1)
+        {
+            auto left_it = lru_tree.append_child( node_it, LEFT);
+            auto right_it = lru_tree.append_child( node_it, LEFT);
+            construct_tree( left_it, depth);
+            construct_tree( right_it, depth);
+        }
+        else
+        {
+            if ( leaf_iterator != ways)
+                lru_tree.append_child(node_it, leaf_iterator++);
+            if ( leaf_iterator != ways)
+                lru_tree.append_child(node_it, leaf_iterator++);
+        }
+    }
+}
+
+std::size_t Pseudo_LRUCacheInfo::reverse_flag( std::size_t flag)
+{
+    if ( flag == LEFT)
+        return RIGHT;
+    else
+        return LEFT;
+}
+
+void Pseudo_LRUCacheInfo::touch( std::size_t way)
+{
+    if ( way >= ways)
+        throw CacheReplacementException( "Requsted way number ( " + std::to_string( way) + ") is out of the capacity (" + std::to_string( ways) +")\n");
+    auto found_it = std::find(lru_tree.begin(), lru_tree.end(), way);
+    for ( auto i = found_it; i != lru_tree.begin(); i = lru_tree.parent( i))
+        if ( lru_tree.index( i) == *lru_tree.parent( i))
+            lru_tree.replace( lru_tree.parent( i), reverse_flag( *lru_tree.parent( i)));
+}
+
+std::size_t Pseudo_LRUCacheInfo::update()
+{
+    auto node_it = lru_tree.begin();
+    while ( lru_tree.number_of_children( node_it) != 0)
+    {
+        if (*node_it == RIGHT && lru_tree.number_of_children( node_it) == 2)
+            node_it = lru_tree.child( node_it, 1); //go to the right child if it exists
+        else
+            node_it = lru_tree.child( node_it, 0); //go tot he left child
+    }
+    return *node_it;
+}
+
+void Pseudo_LRUCacheInfo::set_to_erase( std::size_t ) { throw CacheReplacementException( "Set_to_erase method is not supposed to be used in perfomance simulation"); }
+void Pseudo_LRUCacheInfo::allocate( std::size_t ) { throw CacheReplacementException( "Allocate method is not supposed to be used in perfomance simulation"); }
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
 std::unique_ptr<CacheReplacementInterface> create_cache_replacement( const std::string& name, std::size_t ways)
 {
     if (name == "LRU")
         return std::make_unique<LRUCacheInfo>( ways);
-    //if (name == "pseudo-LRU")
-    throw UndefinedCacheReplacementPolicyName("\"" + name + "\" replacement policy is not defined, supported polices are:\nLRU\npseudo-LRU\n");
+    if (name == "Pseudo-LRU")
+        return std::make_unique<Pseudo_LRUCacheInfo>( ways);
+    else
+        throw CacheReplacementException("\"" + name + "\" replacement policy is not defined, supported polices are:\nLRU\npseudo-LRU\n");
 }
+
+
--- simulator/infra/replacement/t/unit_test.cpp
+++ simulator/infra/replacement/t/unit_test.cpp
@@ -1,5 +1,5 @@
 /**
- * Tests for the least recently used line replacement module.
+ * Tests for the line replacement modules.
  * @author Andrey Agrachev
  */
 
@@ -7,7 +7,7 @@
 
 #include <catch.hpp>
 
-TEST_CASE( "Check_simple_touch_method")
+TEST_CASE( "LRU: Check_touch_method")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     test_lru_module->touch( 0);
@@ -16,7 +16,7 @@ TEST_CASE( "Check_simple_touch_method")
     CHECK( test_lru_module->update() == 0);
 }
 
-TEST_CASE( "Check_another_touch_method")
+TEST_CASE( "LRU: Check_touch_method_2")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     test_lru_module->touch( 0);
@@ -26,7 +26,7 @@ TEST_CASE( "Check_another_touch_method")
     CHECK( test_lru_module->update() == 1);
 }
 
-TEST_CASE( "Check_set_to_erase_method")
+TEST_CASE( "LRU: Check_set_to_erase_method")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     test_lru_module->touch( 0);
@@ -36,7 +36,7 @@ TEST_CASE( "Check_set_to_erase_method")
     CHECK( test_lru_module->update() == 2);
 }
 
-TEST_CASE( "Check_Untouched_way")
+TEST_CASE( "LRU: Check_Untouched_way")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     test_lru_module->touch( 0);
@@ -45,25 +45,69 @@ TEST_CASE( "Check_Untouched_way")
 }
 
 
-TEST_CASE( "Check_allocate_method")
+TEST_CASE( "LRU: Check_allocate_method")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     CHECK_NOTHROW( test_lru_module->allocate( 0));
 }
 
-TEST_CASE( "Check_erase_lru_element_method")
+TEST_CASE( "LRU: Check_erase_lru_element_method")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
     CHECK_NOTHROW(test_lru_module->allocate(1));
 }
 
-TEST_CASE( "Check_get_ways_method")
+TEST_CASE( "LRU && Pseudo-LRU: Check_get_ways_method")
 {
     auto test_lru_module = create_cache_replacement( "LRU", 3);
+    auto test_pseudo_lru_module = create_cache_replacement( "Pseudo-LRU", 3);
     CHECK(test_lru_module->get_ways() == 3);
+    CHECK(test_pseudo_lru_module->get_ways() == 3);
 }
 
-TEST_CASE( "Check_bad_string_pass_to_factory_method")
+TEST_CASE( "LRU: Check_bad_string_pass_to_factory_method")
 {
-    CHECK_THROWS_AS( create_cache_replacement( "BAD STRING", 3), UndefinedCacheReplacementPolicyName);
+    CHECK_THROWS_AS( create_cache_replacement( "BAD STRING", 3), CacheReplacementException);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_forbidden_to_call_methods")
+{
+    auto test_pseudo_lru_module = create_cache_replacement( "Pseudo-LRU", 3);
+    CHECK_THROWS_AS( test_pseudo_lru_module->allocate( 3), CacheReplacementException);
+    CHECK_THROWS_AS( test_pseudo_lru_module->set_to_erase( 3), CacheReplacementException);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_bad_way_number_in_touch_method")
+{
+    auto test_pseudo_lru_module = create_cache_replacement( "Pseudo-LRU", 3);
+    CHECK_THROWS_AS( test_pseudo_lru_module->touch( 4), CacheReplacementException);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_update_method")
+{
+    auto test_lru_module = create_cache_replacement( "Pseudo-LRU", 4);
+    CHECK( test_lru_module->update() == 0);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_touch_method")
+{
+    auto test_lru_module = create_cache_replacement( "Pseudo-LRU", 4);
+    test_lru_module->touch( 1);
+    CHECK( test_lru_module->update() == 2);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_touch_method_2")
+{
+    auto test_lru_module = create_cache_replacement( "Pseudo-LRU", 4);
+    test_lru_module->touch( 2);
+    test_lru_module->touch( 1);
+    CHECK( test_lru_module->update() == 3);
+}
+
+TEST_CASE( "Pseudo-LRU: Check_touch_method_with_odd_number_of elements")
+{
+    auto test_lru_module = create_cache_replacement( "Pseudo-LRU", 3);
+    test_lru_module->touch( 2);
+    test_lru_module->touch( 1);
+    CHECK( test_lru_module->update() == 2); //way number 3 doesn't exist
 }
