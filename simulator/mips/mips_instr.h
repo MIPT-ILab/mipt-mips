af52d83c3b74a85e23fa3ad19303211879a88be1
Pavel I. Kryukov <kryukov@frtk.ru>
2019-04-30 10:53:49 +0300

    Integrate dense hash map to InstrCache

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -23,7 +23,7 @@
 template<typename I>
 struct MIPSTableEntry;
 
-template <typename Key, typename Value, size_t CAPACITY>
+template <typename Key, typename Value, size_t CAPACITY, Key INVALID_KEY>
 class InstrCache;
 
 template<typename R>
@@ -62,7 +62,7 @@ class BaseMIPSInstr : public BaseInstruction<R, MIPSRegister>
         }
     private:
         using MyDatapath = typename BaseInstruction<R, MIPSRegister>::MyDatapath;
-        using DisasmCache = InstrCache<uint32, std::string, 8192>;
+        using DisasmCache = InstrCache<uint32, std::string, 8192, all_ones<uint32>()>;
 
         const uint32 raw;
         const bool raw_valid = false;
