63e3304981db4b81e8bc0b92ab22e9fb626ba0d8
Pavel I. Kryukov <kryukov@frtk.ru>
2019-05-01 14:36:31 +0300

    Merge print128 helpers to a single class

--- simulator/infra/print128.cpp
+++ simulator/infra/print128.cpp
@@ -30,41 +30,40 @@ struct Separator
     auto get_hi_part( uint128 v) const noexcept { return v / value; }
 };
 
-// RAII wrapper to restore std::ostream flags
-class FlagsKeeper
+template<size_t BASE>
+class Dumper128
 {
 public:
-    explicit FlagsKeeper( std::ostream& out) : out( out), flags( out.flags()) { }
-    ~FlagsKeeper() { out.flags( flags); }
-    FlagsKeeper( const FlagsKeeper&) = delete;
-    FlagsKeeper( FlagsKeeper&&) = delete;
-    FlagsKeeper& operator=( const FlagsKeeper&) = delete;
-    FlagsKeeper& operator=( FlagsKeeper&&) = delete;
+    explicit Dumper128( std::ostream& out) : out( out), flags( out.flags()) { }
+    ~Dumper128() { out.flags( flags); }
+    Dumper128( const Dumper128&) = delete;
+    Dumper128( Dumper128&&) = delete;
+    Dumper128& operator=( const Dumper128&) = delete;
+    Dumper128& operator=( Dumper128&&) = delete;
+
+    std::ostream& operator()( uint128 value) const
+    {
+        if ( value <= UINT64_MAX)
+            return out << narrow_cast<uint64>( value);
+
+        return this->operator()( separator.get_hi_part( value))
+            << std::setfill( '0') << std::setw( separator.power) << std::noshowbase
+            << separator.get_lo_part( value);
+    }
+
 private:
+    static const constexpr Separator separator = Separator( BASE);
     std::ostream& out;
     const std::ios_base::fmtflags flags;
 };
 
-template<size_t BASE>
-static std::ostream& dump( std::ostream& out, uint128 value)
-{
-    static const constexpr Separator separator( BASE);
-    if ( value <= UINT64_MAX)
-        return out << narrow_cast<uint64>( value);
-
-    FlagsKeeper flags_keeper( out);
-    return dump<BASE>( out, separator.get_hi_part( value))
-        << std::setfill( '0') << std::setw( separator.power) << std::noshowbase
-        << separator.get_lo_part( value);
-}
-
 std::ostream& operator<<( std::ostream& out, uint128 value)
 {
     if (( out.flags() & std::ios::dec) != 0)
-        return dump<10>( out, value);
+        return Dumper128<10>( out)( value);
     if (( out.flags() & std::ios::hex) != 0)    
-        return dump<16>( out, value);
-    return dump<8>( out, value);
+        return Dumper128<16>( out)( value);
+    return Dumper128<8>( out)( value);
 }
 
 #endif
