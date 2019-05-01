820755fb17529f28df9eb2de5d09d874d9767786
Rustem Yunusov <yunusov.rr@phystech.edu>
2019-05-01 23:00:33 +0300

    Add test for argc, argv[] loading

--- simulator/memory/argv_loader/t/unit_tests.cpp
+++ simulator/memory/argv_loader/t/unit_tests.cpp
@@ -20,9 +20,25 @@ TEST_CASE( "ArgvLoader: argv = nullptr")
 
 TEST_CASE( "ArgvLoader: argv[2] = nullptr while argc >= 3")
 {
-    const char* argv[3] = { "a", "b", "c"};
+    const char* argv[4] = { "a", "b", "c"};
     auto argv_loader = ArgvLoader( argv);
     argv[2] = nullptr;
     auto mem = FuncMemory::create_plain_memory( 20);
     CHECK_THROWS_AS( argv_loader.load_argv_to( mem, 0), ArgvLoaderError);
+}
+
+TEST_CASE( "ArgvLoader: load of argc and argv[]")
+{
+    const char* argv[4] = { "a", "b", "c"};
+    auto argv_loader = ArgvLoader( argv);
+    auto mem = FuncMemory::create_plain_memory( 20);
+    CHECK( argv_loader.load_argv_to( mem, 0) == 42);
+    CHECK( ( mem -> read<int, Endian::little>( 0)) == 3);
+    CHECK( ( mem -> read<uint64, Endian::little>( 4))  == uint64( argv[0]));
+    CHECK( ( mem -> read<uint64, Endian::little>( 12)) == uint64( argv[1]));
+    CHECK( ( mem -> read<uint64, Endian::little>( 20)) == uint64( argv[2]));
+    CHECK( ( mem -> read<uint64, Endian::little>( 28)) == 0);
+    CHECK( ( mem -> read<uint16, Endian::little>( 36)) == 'a');
+    CHECK( ( mem -> read<uint16, Endian::little>( 38)) == 'b');
+    CHECK( ( mem -> read<uint16, Endian::little>( 40)) == 'c');
 }
