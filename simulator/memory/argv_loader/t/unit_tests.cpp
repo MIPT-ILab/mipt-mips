eeb1d77dab04a6078a73b3748658b0c5432d8c92
Rustem Yunusov <yunusov.rr@phystech.edu>
2019-05-02 11:18:49 +0300

    Code style fixes

--- simulator/memory/argv_loader/t/unit_tests.cpp
+++ simulator/memory/argv_loader/t/unit_tests.cpp
@@ -33,14 +33,14 @@ TEST_CASE( "ArgvLoader: load of argc and argv[]")
     auto argv_loader = ArgvLoader( argv);
     auto mem = FuncMemory::create_plain_memory( 20);
     CHECK( argv_loader.load_argv_to( mem, 0) == 42);
-    CHECK( ( mem -> read<int, Endian::little>( 0)) == 3);
-    CHECK( ( mem -> read<uint64, Endian::little>( 4))  == uint64( argv[0]));
-    CHECK( ( mem -> read<uint64, Endian::little>( 12)) == uint64( argv[1]));
-    CHECK( ( mem -> read<uint64, Endian::little>( 20)) == uint64( argv[2]));
-    CHECK( ( mem -> read<uint64, Endian::little>( 28)) == 0);
-    CHECK( ( mem -> read<uint16, Endian::little>( 36)) == 'a');
-    CHECK( ( mem -> read<uint16, Endian::little>( 38)) == 'b');
-    CHECK( ( mem -> read<uint16, Endian::little>( 40)) == 'c');
+    CHECK( ( mem->read<int,    Endian::little>( 0)) == 3);
+    CHECK( ( mem->read<uint64, Endian::little>( 4))  == uint64( argv[0]));
+    CHECK( ( mem->read<uint64, Endian::little>( 12)) == uint64( argv[1]));
+    CHECK( ( mem->read<uint64, Endian::little>( 20)) == uint64( argv[2]));
+    CHECK( ( mem->read<uint64, Endian::little>( 28)) == 0);
+    CHECK( ( mem->read<uint16, Endian::little>( 36)) == 'a');
+    CHECK( ( mem->read<uint16, Endian::little>( 38)) == 'b');
+    CHECK( ( mem->read<uint16, Endian::little>( 40)) == 'c');
 }
 
 TEST_CASE( "ArgvLoader: load of argc, argv[] and envp[]")
