849396b90163729899da908f5bbcd3a6528d7b47
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2019-05-06 12:52:09 +0300

    MIPS: add Trap converting to/from MIPS/MARS conventions

--- simulator/func_sim/t/unit_test.cpp
+++ simulator/func_sim/t/unit_test.cpp
@@ -20,10 +20,28 @@
 
 #include <sstream>
 
-TEST_CASE( "Trap: check to GDB/RISCV formats conversions ")
+TEST_CASE( "Trap: check conversion to RISC-V ")
 {
-    CHECK( Trap(Trap::SYSCALL).to_riscv_format() == CAUSE_USER_ECALL);
-    CHECK( Trap(Trap::UNKNOWN_INSTRUCTION).to_gdb_format() == GDB_SIGNAL_ILL);
+    Trap trap( Trap::SYSCALL);
+    CHECK( trap.to_riscv_format() == CAUSE_USER_ECALL);
+}
+
+TEST_CASE( "Trap: check conversion to GDB ")
+{
+    Trap trap( Trap::UNKNOWN_INSTRUCTION);
+    CHECK( trap.to_gdb_format() == GDB_SIGNAL_ILL);
+}
+
+TEST_CASE( "Trap: check conversion to MIPS ")
+{
+    Trap trap( Trap::FP_DIV_BY_ZERO);
+    CHECK( trap.to_mips_format() == MIPS_EXC_FPE);
+}
+
+TEST_CASE( "Trap: check bad conversion to MIPS ")
+{
+    Trap trap( Trap::NO_TRAP);
+    CHECK_THROWS_AS( trap.to_mips_format(), std::out_of_range);
 }
 
 TEST_CASE( "Trap: check RISC-V initialization")
@@ -37,7 +55,14 @@ TEST_CASE( "Trap: check GDB initialization")
 {
     Trap trap( Trap::NO_TRAP);
     trap.set_from_gdb_format( GDB_SIGNAL_TRAP);
-    CHECK( trap == Trap( Trap::Trap::BREAKPOINT));
+    CHECK( trap == Trap( Trap::BREAKPOINT));
+}
+
+TEST_CASE( "Trap: check MIPS initialization")
+{
+    Trap trap( Trap::NO_TRAP);
+    trap.set_from_mips_format( MIPS_EXC_FPOVF);
+    CHECK( trap == Trap( Trap::FP_OVERFLOW));
 }
 
 TEST_CASE( "Trap: print")
--- simulator/func_sim/trap_types.cpp
+++ simulator/func_sim/trap_types.cpp
@@ -15,40 +15,25 @@ uint8 Trap::to_gdb_format()
 {
     static const std::unordered_map<TrapType, uint8> to_gdb_conv =
     {
-        { Trap::NO_TRAP,             GDB_SIGNAL_0    },
-        { Trap::HALT,                GDB_SIGNAL_0    },
-        { Trap::EXPLICIT_TRAP,       GDB_SIGNAL_TRAP },
-        { Trap::BREAKPOINT,          GDB_SIGNAL_TRAP },
-        { Trap::SYSCALL,             GDB_SIGNAL_0    },
-        { Trap::INTEGER_OVERFLOW,    GDB_SIGNAL_0    },
-        { Trap::UNSUPPORTED_SYSCALL, GDB_SIGNAL_SYS  },  // Not implemented in gdb_interface
-        { Trap::UNALIGNED_LOAD,      GDB_SIGNAL_BUS  },
-        { Trap::UNALIGNED_STORE,     GDB_SIGNAL_BUS  },
-        { Trap::UNALIGNED_FETCH,     GDB_SIGNAL_BUS  },
-        { Trap::UNKNOWN_INSTRUCTION, GDB_SIGNAL_ILL  },  // Not implemented in gdb_interface
+        { Trap::NO_TRAP,              GDB_SIGNAL_0    },
+        { Trap::HALT,                 GDB_SIGNAL_0    },
+        { Trap::EXPLICIT_TRAP,        GDB_SIGNAL_TRAP },
+        { Trap::BREAKPOINT,           GDB_SIGNAL_TRAP },
+        { Trap::SYSCALL,              GDB_SIGNAL_0    },
+        { Trap::INTEGER_OVERFLOW,     GDB_SIGNAL_0    },
+        { Trap::UNSUPPORTED_SYSCALL,  GDB_SIGNAL_SYS  },
+        { Trap::UNKNOWN_INSTRUCTION,  GDB_SIGNAL_ILL  },
+        { Trap::RESERVED_INSTRUCTION, GDB_SIGNAL_ILL  },
+        { Trap::COPROCESSOR_UNUSABLE, GDB_SIGNAL_ILL  },
+        { Trap::UNALIGNED_LOAD,       GDB_SIGNAL_BUS  },
+        { Trap::UNALIGNED_STORE,      GDB_SIGNAL_BUS  },
+        { Trap::UNALIGNED_FETCH,      GDB_SIGNAL_BUS  },
+        { Trap::FP_OVERFLOW,          GDB_SIGNAL_FPE  },
+        { Trap::FP_UNDERFLOW,         GDB_SIGNAL_FPE  },
+        { Trap::FP_DIV_BY_ZERO,       GDB_SIGNAL_FPE  },
     };
 
     return to_gdb_conv.at( value);
-}
-
-uint8 Trap::to_riscv_format()
-{
-    static const std::unordered_map<TrapType, uint8> to_riscv_conv =
-    {
-        { Trap::NO_TRAP,             0                         },
-        { Trap::HALT,                0                         },
-        { Trap::EXPLICIT_TRAP,       CAUSE_BREAKPOINT          },
-        { Trap::BREAKPOINT,          CAUSE_BREAKPOINT          },
-        { Trap::SYSCALL,             CAUSE_USER_ECALL          },
-        { Trap::INTEGER_OVERFLOW,    0                         },
-        { Trap::UNSUPPORTED_SYSCALL, 0                         },
-        { Trap::UNALIGNED_LOAD,      CAUSE_MISALIGNED_LOAD     },
-        { Trap::UNALIGNED_STORE,     CAUSE_MISALIGNED_STORE    },
-        { Trap::UNALIGNED_FETCH,     CAUSE_MISALIGNED_FETCH    },
-        { Trap::UNKNOWN_INSTRUCTION, CAUSE_ILLEGAL_INSTRUCTION },
-    };
-
-    return to_riscv_conv.at( value);
 }
 
 void Trap::set_from_gdb_format(uint8 id)
@@ -56,16 +41,43 @@ void Trap::set_from_gdb_format(uint8 id)
     static const std::unordered_map<uint8, TrapType> from_gdb_conv =
     {
         { GDB_SIGNAL_0,    Trap::NO_TRAP             },
+        { GDB_SIGNAL_INT,  Trap::BREAKPOINT          },
         { GDB_SIGNAL_TRAP, Trap::BREAKPOINT          },
-        { GDB_SIGNAL_SYS,  Trap::UNSUPPORTED_SYSCALL },  // Not implemented in gdb_interface
+        { GDB_SIGNAL_FPE,  Trap::FP_OVERFLOW         },
         { GDB_SIGNAL_BUS,  Trap::UNALIGNED_LOAD      },
-        { GDB_SIGNAL_ILL,  Trap::UNKNOWN_INSTRUCTION },  // Not implemented in gdb_interface
+        { GDB_SIGNAL_SYS,  Trap::UNSUPPORTED_SYSCALL },
+        { GDB_SIGNAL_ILL,  Trap::UNKNOWN_INSTRUCTION },
     };
 
     auto it = from_gdb_conv.find( id);
     if ( it == from_gdb_conv.end())
         value = Trap::NO_TRAP;
     value = it->second;
+}
+
+uint8 Trap::to_riscv_format()
+{
+    static const std::unordered_map<TrapType, uint8> to_riscv_conv =
+    {
+        { Trap::NO_TRAP,              0                         },
+        { Trap::HALT,                 0                         },
+        { Trap::EXPLICIT_TRAP,        CAUSE_BREAKPOINT          },
+        { Trap::BREAKPOINT,           CAUSE_BREAKPOINT          },
+        { Trap::SYSCALL,              CAUSE_USER_ECALL          },
+        { Trap::INTEGER_OVERFLOW,     0                         },
+        { Trap::UNSUPPORTED_SYSCALL,  0                         },
+        { Trap::UNKNOWN_INSTRUCTION,  CAUSE_ILLEGAL_INSTRUCTION },
+        { Trap::RESERVED_INSTRUCTION, CAUSE_ILLEGAL_INSTRUCTION },
+        { Trap::COPROCESSOR_UNUSABLE, CAUSE_ILLEGAL_INSTRUCTION },
+        { Trap::UNALIGNED_LOAD,       CAUSE_MISALIGNED_LOAD     },
+        { Trap::UNALIGNED_STORE,      CAUSE_MISALIGNED_STORE    },
+        { Trap::UNALIGNED_FETCH,      CAUSE_MISALIGNED_FETCH    },
+        { Trap::FP_OVERFLOW,          0                         },
+        { Trap::FP_UNDERFLOW,         0                         },
+        { Trap::FP_DIV_BY_ZERO,       0                         },
+    };
+
+    return to_riscv_conv.at( value);
 }
 
 void Trap::set_from_riscv_format(uint8 id)
@@ -90,4 +102,69 @@ void Trap::set_from_riscv_format(uint8 id)
     };
 
     value = from_riscv_conv.at( id);
+}
+
+/* MARS follows this */
+uint8 Trap::to_mips_format()
+{
+    static const std::unordered_map<TrapType, uint8> to_mips_conv =
+    {
+        // Trap::NO_TRAP              not presented
+        // Trap::HALT                 not presented
+        { Trap::EXPLICIT_TRAP,        MIPS_EXC_TR     },
+        { Trap::BREAKPOINT,           MIPS_EXC_BP     },
+        { Trap::SYSCALL,              MIPS_EXC_SYS    },
+        { Trap::INTEGER_OVERFLOW,     MIPS_EXC_OV     },
+        // Trap::UNSUPPORTED_SYSCALL  handled by kernel
+        { Trap::UNKNOWN_INSTRUCTION,  MIPS_EXC_ADEL   },
+        { Trap::RESERVED_INSTRUCTION, MIPS_EXC_RI     },
+        { Trap::COPROCESSOR_UNUSABLE, MIPS_EXC_CPU    },
+        { Trap::UNALIGNED_LOAD,       MIPS_EXC_ADEL   },
+        { Trap::UNALIGNED_STORE,      MIPS_EXC_ADES   },
+        { Trap::UNALIGNED_FETCH,      MIPS_EXC_ADEL   },
+        { Trap::FP_OVERFLOW,          MIPS_EXC_FPOVF  },
+        { Trap::FP_UNDERFLOW,         MIPS_EXC_FPUNDF },
+        { Trap::FP_DIV_BY_ZERO,       MIPS_EXC_FPE    },
+    };
+
+    return to_mips_conv.at( value);
+}
+
+/* MARS follows this */
+void Trap::set_from_mips_format(uint8 id)
+{
+    static const std::unordered_map<uint8, TrapType> from_mips_conv =
+    {
+        { MIPS_EXC_INT,      Trap::NO_TRAP              },
+        { MIPS_EXC_MOD,      Trap::NO_TRAP              },
+        { MIPS_EXC_TLBL,     Trap::NO_TRAP              },
+        { MIPS_EXC_TLBS,     Trap::NO_TRAP              },
+        { MIPS_EXC_ADEL,     Trap::UNALIGNED_LOAD       },
+        { MIPS_EXC_ADES,     Trap::UNALIGNED_STORE      },
+        { MIPS_EXC_IBE,      Trap::NO_TRAP              },
+        { MIPS_EXC_DBE,      Trap::NO_TRAP              },
+        { MIPS_EXC_SYS,      Trap::SYSCALL              },
+        { MIPS_EXC_BP,       Trap::BREAKPOINT           },
+        { MIPS_EXC_RI,       Trap::RESERVED_INSTRUCTION },
+        { MIPS_EXC_CPU,      Trap::COPROCESSOR_UNUSABLE },
+        { MIPS_EXC_OV,       Trap::INTEGER_OVERFLOW     },
+        { MIPS_EXC_TR,       Trap::EXPLICIT_TRAP        },
+        { MIPS_EXC_MSAFPE,   Trap::NO_TRAP              },
+        { MIPS_EXC_FPE,      Trap::FP_DIV_BY_ZERO       },
+        { MIPS_EXC_FPOVF,    Trap::FP_OVERFLOW          },
+        { MIPS_EXC_FPUNDF,   Trap::FP_UNDERFLOW         },
+        { MIPS_EXC_C2E,      Trap::NO_TRAP              },
+        { MIPS_EXC_TLBRI,    Trap::NO_TRAP              },
+        { MIPS_EXC_TLBXI,    Trap::NO_TRAP              },
+        { MIPS_EXC_MSADis,   Trap::NO_TRAP              },
+        { MIPS_EXC_MDMX,     Trap::NO_TRAP              },
+        { MIPS_EXC_WATCH,    Trap::NO_TRAP              },
+        { MIPS_EXC_MCHECK,   Trap::NO_TRAP              },
+        { MIPS_EXC_THREAD,   Trap::NO_TRAP              },
+        { MIPS_EXC_DSPDIS,   Trap::NO_TRAP              },
+        { MIPS_EXC_GE,       Trap::NO_TRAP              },
+        { MIPS_EXC_CACHEERR, Trap::NO_TRAP              },
+    };
+
+    value = from_mips_conv.at( id);
 }
