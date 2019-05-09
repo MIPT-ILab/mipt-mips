e625a97523432659d24fcf781c6ab601420bb8de
Pavel I. Kryukov <kryukov@frtk.ru>
2019-05-09 20:39:16 +0300

    Dedicate 'verbose' Driver option to Log class

--- simulator/func_sim/driver.h
+++ simulator/func_sim/driver.h
@@ -9,23 +9,19 @@
 
 #include "traps/trap.h"
 
+#include <infra/log.h>
+
 #include <memory>
 #include <string>
 
 class Simulator;
 
-class Driver
+class Driver : public Log
 {
 public:
-    static std::unique_ptr<Driver> construct( const std::string& mode, Simulator* sim);
+    explicit Driver( bool verbose) : Log( verbose) { }
+    static std::unique_ptr<Driver> construct( const std::string& mode, Simulator* sim, bool verbose);
     virtual Trap handle_trap( Trap trap) const = 0;
-
-    Driver() = default;
-    virtual ~Driver() = default;
-    Driver( const Driver&) = delete;
-    Driver( Driver&&) = delete;
-    Driver& operator=( const Driver&) = delete;
-    Driver& operator=( Driver&&) = delete;
 };
 
 #endif
