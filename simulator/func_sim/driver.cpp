e1342c37430eac023b121260f689baffb4fb38a0
Pavel I. Kryukov <kryukov@frtk.ru>
2019-05-09 23:14:07 +0300

    Implement MIPS32 exception handler

--- simulator/func_sim/driver.cpp
+++ simulator/func_sim/driver.cpp
@@ -6,6 +6,9 @@
 
 #include "driver.h"
 
+#include <mips/mips_register/mips_register.h>
+#include <simulator.h>
+
 class DriverNoOp : public Driver
 {
 public:
@@ -39,7 +42,30 @@ public:
     }
 };
 
-std::unique_ptr<Driver> Driver::construct( const std::string& mode, Simulator* /* sim */, bool log)
+class DriverMIPS32 : public Driver
+{
+public:
+    explicit DriverMIPS32( bool verbose, Simulator* sim) : Driver( verbose), cpu( sim) { }
+    Trap handle_trap_impl( Trap trap, Addr pc) const final
+    {
+        if ( trap == Trap::NO_TRAP)
+            return trap;
+
+        auto status = cpu->read_cpu_register( MIPSRegister::status().to_rf_index());
+        auto cause  = cpu->read_cpu_register( MIPSRegister::cause().to_rf_index());
+        status |= 0x2;
+        cause = (cause & ~(bitmask<uint64>(4) << 2)) | ((trap.to_mips_format() & bitmask<uint64>(4)) << 2);
+        cpu->write_cpu_register( MIPSRegister::status().to_rf_index(), status);
+        cpu->write_cpu_register( MIPSRegister::cause().to_rf_index(), cause);
+        cpu->write_cpu_register( MIPSRegister::epc().to_rf_index(), pc);
+        cpu->set_pc( 0x8'0000'0180);
+        return Trap( Trap::NO_TRAP);
+    }
+private:
+    Simulator* cpu = nullptr;
+};
+
+std::unique_ptr<Driver> Driver::construct( const std::string& mode, Simulator* sim, bool log)
 {
     if ( mode == "stop")
         return std::make_unique<DriverNoOp>( log);
@@ -49,5 +75,7 @@ std::unique_ptr<Driver> Driver::construct( const std::string& mode, Simulator* /
         return std::make_unique<DriverIgnore>( log);
     if ( mode == "critical")
         return std::make_unique<DriverCritical>( log);
+    if ( mode == "mips32")
+        return std::make_unique<DriverMIPS32>( log, sim);
     throw IncorrectDriver( mode);
 }
