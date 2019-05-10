584f5dae4989a64a329016905b88899c203f24ea
Pavel I. Kryukov <kryukov@frtk.ru>
2019-05-11 01:53:43 +0300

    Provide 'deleted key' to DenseHashMap (#1025)
    DenseHashMap operation requires a special 'deleted key' to support element erasure
    Found with assertion failures in debug build.

--- simulator/mips/mips_instr.h
+++ simulator/mips/mips_instr.h
@@ -22,7 +22,7 @@
 template<typename I>
 struct MIPSTableEntry;
 
-template <typename Key, typename Value, size_t CAPACITY, Key INVALID_KEY>
+template <typename Key, typename Value, size_t CAPACITY, Key INVALID_KEY, Key DELETED_KEY>
 class InstrCache;
 
 template<typename R>
@@ -61,7 +61,7 @@ class BaseMIPSInstr : public BaseInstruction<R, MIPSRegister>
         }
     private:
         using MyDatapath = typename BaseInstruction<R, MIPSRegister>::MyDatapath;
-        using DisasmCache = InstrCache<uint32, std::string, 8192, all_ones<uint32>()>;
+        using DisasmCache = InstrCache<uint32, std::string, 8192, all_ones<uint32>(), all_ones<uint32>() - 1>;
 
         const uint32 raw;
         const bool raw_valid = false;
