d5eb6fa16ce7d19cfd18b93c141d518e197811cc
Pavel I. Kryukov <kryukov@frtk.ru>
2019-05-11 23:44:48 +0300

    Extract CPUModel interface from Simulator interface

--- simulator/simulator.h
+++ simulator/simulator.h
@@ -25,23 +25,39 @@ struct InvalidISA final : Exception
     { }
 };
 
+class CPUModel : public Log
+{
+public:
+    explicit CPUModel( bool log = false) : Log( log) {}
+
+    void set_pc( Addr pc) { set_target( Target( pc, 0)); }
+    virtual void set_target( const Target& target) = 0;
+    virtual Addr get_pc() const = 0;
+    
+    virtual size_t sizeof_register() const = 0;
+
+    virtual uint64 read_cpu_register( size_t regno) const = 0;
+    virtual uint64 read_gdb_register( size_t regno) const = 0;
+    virtual void write_cpu_register( size_t regno, uint64 value) = 0;
+    virtual void write_gdb_register( size_t regno, uint64 value) = 0;
+    virtual void write_csr_register( std::string_view name, uint64 value) = 0;
+};
+
 class FuncMemory;
 class Kernel;
 
-class Simulator : public Log {
+class Simulator : public CPUModel
+{
 public:
-    explicit Simulator( bool log = false) : Log( log) {}
+    explicit Simulator( bool log = false) : CPUModel( log) {}
 
     virtual Trap run( uint64 instrs_to_run) = 0;
-    virtual void set_target( const Target& target) = 0;
     virtual void set_memory( std::shared_ptr<FuncMemory> m) = 0;
     virtual void set_kernel( std::shared_ptr<Kernel> k) = 0;
     virtual void setup_trap_handler( const std::string& mode) = 0;
     virtual void init_checker() = 0;
 
     Trap run_no_limit() { return run( MAX_VAL64); }
-    void set_pc( Addr pc) { set_target( Target( pc, 0)); }
-    virtual Addr get_pc() const = 0;
 
     static std::shared_ptr<Simulator> create_simulator( const std::string& isa, bool functional_only, bool log);
     static std::shared_ptr<Simulator> create_configured_simulator();
@@ -50,14 +66,6 @@ public:
     {
         return create_simulator( isa, true, log);
     }
-
-    virtual size_t sizeof_register() const = 0;
-
-    virtual uint64 read_cpu_register( size_t regno) const = 0;
-    virtual uint64 read_gdb_register( size_t regno) const = 0;
-    virtual void write_cpu_register( size_t regno, uint64 value) = 0;
-    virtual void write_gdb_register( size_t regno, uint64 value) = 0;
-    virtual void write_csr_register( std::string_view name, uint64 value) = 0;
 
     int get_exit_code() const { return exit_code; }
 
