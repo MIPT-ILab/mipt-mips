40bee9b377686e00addf897336060cdc2229f909
Pavel I. Kryukov <kryukov@frtk.ru>
2019-09-05 22:05:14 +0200

    Use std::vector for true LRU (#1086)

--- simulator/infra/replacement/cache_replacement.cpp
+++ simulator/infra/replacement/cache_replacement.cpp
@@ -7,8 +7,6 @@
 #include "infra/replacement/cache_replacement.h"
 #include "infra/macro.h"
 
-#include <sparsehash/dense_hash_map.h>
-
 #include <list>
 #include <vector>
 
@@ -20,50 +18,35 @@ class LRU : public CacheReplacement
         void touch( std::size_t way) override ;
         void set_to_erase( std::size_t way) override ;
         std::size_t update() override ;
-        std::size_t get_ways() const override { return ways; }
+        std::size_t get_ways() const override { return lru_hash.size(); }
 
     private:
         std::list<std::size_t> lru_list{};
-        const std::size_t ways;
-        const std::size_t impossible_key = SIZE_MAX;
-        google::dense_hash_map<std::size_t, decltype(lru_list.cbegin())> lru_hash{};
+        std::vector<decltype(lru_list.cbegin())> lru_hash{};
 };
 
-LRU::LRU( std::size_t ways) : ways( ways), lru_hash( ways)
+LRU::LRU( std::size_t ways) : lru_hash( ways)
 {
-    lru_hash.set_empty_key( impossible_key); //special dense_hash_map requirement
-    for ( std::size_t i = 0; i < ways; i++)
-    {
-        lru_list.push_front( i);
-        lru_hash.emplace( i, lru_list.begin());
-    }
+    for ( std::size_t i = 0; i < lru_hash.size(); i++)
+        lru_hash[i] = lru_list.insert( lru_list.begin(), i);
 }
 
 void LRU::touch( std::size_t way)
 {
-    const auto lru_it = lru_hash.find( way);
-    assert( lru_it != lru_hash.end());
     // Put the way to the head of the list
-    lru_list.splice( lru_list.begin(), lru_list, lru_it->second);
+    lru_list.splice( lru_list.begin(), lru_list, lru_hash[way]);
 }
 
 void LRU::set_to_erase( std::size_t way)
 {
-    const auto lru_it = lru_hash.find( way);
-    assert( lru_it != lru_hash.end());
-    lru_list.splice( lru_list.end(), lru_list, lru_it->second);
+    lru_list.splice( lru_list.end(), lru_list, lru_hash[way]);
 }
 
 std::size_t LRU::update()
 {
     // remove the least recently used element from the tail
     std::size_t lru_elem = lru_list.back();
-    lru_list.pop_back();
-
-    // put it to the head
-    auto ptr = lru_list.insert( lru_list.begin(), lru_elem);
-    lru_hash[ lru_elem] = ptr;
-
+    touch( lru_elem);
     return lru_elem;
 }
 
