c3d029632203a2a085354b9905aae7cab5600b6e
exucutional <exdevour@gmail.com>
2019-10-28 20:27:22 +0300

    implement topology dump with command --tdump

--- simulator/infra/ports/module.cpp
+++ simulator/infra/ports/module.cpp
@@ -41,9 +41,31 @@ void Module::enable_logging_impl( const std::unordered_set<std::string>& names)
         c->enable_logging_impl( names);
 }
 
+void Module::enable_dumping_impl(pt::ptree& topology) {
+    pt::ptree topology_module;
+    topology_module.put("module_name", name);
+    topology_module.add_child("write_ports", topology_write_ports);
+    topology_module.add_child("read_ports", topology_read_ports);
+    topology.push_back(std::make_pair("", topology_module));
+    for ( const auto& c : children)
+        c->enable_dumping_impl(topology);
+}
+
 void Root::enable_logging( const std::string& values)
 {
     std::unordered_set<std::string> tokens;
     boost::split( tokens, values, boost::is_any_of( std::string(",")));
     enable_logging_impl( tokens);
+}
+
+void Root::enable_dumping(bool dump)
+{
+    if (dump) {
+        pt::ptree topology;
+        enable_dumping_impl(topology);
+        pt::ptree result;
+        result.add_child("topology", topology);
+        pt::write_json("topology.json", result);
+        std::cout << std::endl << "Topology dumped in topology.json" << std::endl;
+    }
 }
--- simulator/infra/ports/module.h
+++ simulator/infra/ports/module.h
@@ -9,8 +9,12 @@
  
 #include <infra/log.h>
 #include <infra/ports/ports.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #include <unordered_set>
+
+namespace pt = boost::property_tree;
 
 class Module : public Log
 {
@@ -21,16 +25,25 @@ protected:
     template<typename T>
     auto make_write_port( std::string key, uint32 bandwidth) 
     {
+        pt::ptree write_port;
+        write_port.put("key", key);
+        write_port.put("bandwidth", bandwidth);
+        topology_write_ports.push_back(std::make_pair("", write_port));
         return std::make_unique<WritePort<T>>( get_portmap(), std::move(key), bandwidth);
     }
 
     template<typename T>
     auto make_read_port( std::string key, Latency latency)
     {
+        pt::ptree read_port;
+        read_port.put("key", key);
+        read_port.put("latency", latency);
+        topology_read_ports.push_back(std::make_pair("", read_port));
         return std::make_unique<ReadPort<T>>( get_portmap(), std::move(key), latency);
     }
 
     void enable_logging_impl( const std::unordered_set<std::string>& names);
+    void enable_dumping_impl( pt::ptree& topology);
 
 private:
     virtual std::shared_ptr<PortMap> get_portmap() const { return parent->get_portmap(); }
@@ -42,6 +55,8 @@ private:
     Module* const parent;
     std::vector<Module*> children;
     const std::string name;
+    pt::ptree topology_write_ports;
+    pt::ptree topology_read_ports;
 };
 
 class Root : public Module
@@ -55,6 +70,7 @@ public:
 protected:
     void init_portmap() { portmap->init(); }
     void enable_logging( const std::string& values);
+    void enable_dumping( bool dump );
 
 private:
     std::shared_ptr<PortMap> get_portmap() const final { return portmap; }
