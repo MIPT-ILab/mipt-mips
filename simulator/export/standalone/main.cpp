007384523d00d056562b183c8963f925c2d6385f
Pavel I. Kryukov <kryukov@frtk.ru>
2019-11-11 17:34:36 +0300

    Update main.cpp

--- simulator/export/standalone/main.cpp
+++ simulator/export/standalone/main.cpp
@@ -21,26 +21,28 @@ class Main : public MainWrapper
 {
     using MainWrapper::MainWrapper;
 private:
-    int impl( int argc, const char* argv[]) const final {
-        config::handleArgs( argc, argv, 1);
-        auto memory = FuncMemory::create_default_hierarchied_memory();
-
-        auto sim = Simulator::create_configured_simulator();
-        sim->set_memory( memory);
-        sim->write_csr_register( "mscratch", 0x400'0000);
-
-        auto kernel = Kernel::create_configured_kernel();
-        kernel->connect_memory( memory);
-        kernel->set_simulator( sim);
-        kernel->load_file( config::binary_filename);
-        sim->set_kernel( kernel);
-
-        sim->init_checker();
-        sim->set_pc( kernel->get_start_pc());
-        sim->run( config::num_steps);
-        return sim->get_exit_code();
-    }
+    int impl( int argc, const char* argv[]) const final; 
 };
+
+int Main::impl( int argc, const char* argv[]) const {
+    config::handleArgs( argc, argv, 1);
+    auto memory = FuncMemory::create_default_hierarchied_memory();
+
+    auto sim = Simulator::create_configured_simulator();
+    sim->set_memory( memory);
+    sim->write_csr_register( "mscratch", 0x400'0000);
+
+    auto kernel = Kernel::create_configured_kernel();
+    kernel->connect_memory( memory);
+    kernel->set_simulator( sim);
+    kernel->load_file( config::binary_filename);
+    sim->set_kernel( kernel);
+
+    sim->init_checker();
+    sim->set_pc( kernel->get_start_pc());
+    sim->run( config::num_steps);
+    return sim->get_exit_code();
+}
 
 int main( int argc, const char* argv[]) {
     return Main( "Functional and performance simulators for MIPS-based CPU.").run( argc, argv);
