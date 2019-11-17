b8e09135e0dd392ddcb0cbd4f2d2310d97578cef
Vladimir Prokhorov <prokhorov.va@phystech.edu>
2019-11-17 14:19:00 +0300

    add sbext implementation

--- simulator/func_sim/alu.h
+++ simulator/func_sim/alu.h
@@ -219,7 +219,7 @@ struct ALU
     template<typename I> static void movz( I* instr)  { move( instr); if (instr->v_src2 != 0) instr->mask = 0; }
     
     // Bit manipulations
-    template<typename I> static void sbext( I* instr) { instr->v_dst = 1 & ( instr->v_src1 >> ( shamt_v_src2<typename I::RegisterUInt>( instr)); }
+    template<typename I> static void sbext( I* instr) { instr->v_dst = 1 & ( instr->v_src1 >> ( shamt_v_src2<typename I::RegisterUInt>( instr))); }
 
     // Branches
     template<typename I, Predicate<I> p> static
