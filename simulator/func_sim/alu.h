fe9cf236d74d99fd4ba21fa12b4f4ea318cecfd4
YarOkatev <yar.ok@mail.ru>
2019-11-25 00:33:47 +0300

    Added a new instruction "clmul"

--- simulator/func_sim/alu.h
+++ simulator/func_sim/alu.h
@@ -240,6 +240,18 @@ struct ALU
     // Bit manipulations
     template<typename I> static void sbext( I* instr) { instr->v_dst = 1 & ( instr->v_src1 >> shamt_v_src2<typename I::RegisterUInt>( instr)); }
 
+    template<typename I, typename T>
+    static void clmul( I* instr)
+    {
+        instr->v_dst = 0;
+        std::size_t max = bitwidth<T> - count_leading_zeroes<T>( instr->v_src2);
+        for ( std::size_t index = 0; index < max; index++)
+        {
+            if ( (instr->v_src2 >> index) & 1)
+                instr->v_dst ^= instr->v_src1 << index;
+        }
+    }
+
     // Bit manipulations
     template<typename I, typename T> static void pack( I* instr)  { instr->v_dst = (instr->v_src1 & (bitmask<T>(half_bitwidth<T>))) | (instr->v_src2 << (half_bitwidth<T>)); }
 
