f8033d6979eb6c901e729629dccdc4f29ed7e450
Pavel I. Kryukov <kryukov@frtk.ru>
2019-11-26 01:09:43 +0300

    Fix typo

--- simulator/infra/t/unit_test.cpp
+++ simulator/infra/t/unit_test.cpp
@@ -171,7 +171,7 @@ static_assert( ones_rs<uint64>( all_ones<uint64>(), 63) == all_ones<uint64>());
 static_assert( arithmetic_rs<uint64>( 0xA, 1) == 0x5);
 static_assert( arithmetic_rs<uint64>( msb_set<uint64>(), 3) == ones_rs<uint64>( msb_set<uint64>(), 3));
 
-TEST_CASE("ones shift dynamic check*)
+TEST_CASE("ones shift dynamic check")
 {
     // Need that test to check VS behavior
     CHECK( ones_ls<uint32>( 0x8000'c000u, 15) == 0xffff'0001u);
