64ca5e1c6791a383c402c193f87eb2355c0e42d2
Eric Konks <exdevour@gmail.com>
2019-11-27 18:07:00 +0300

    fixes

--- external/tools/tgraph/src/index.js
+++ external/tools/tgraph/src/index.js
@@ -34,56 +34,11 @@ jsPlumb.ready(function() {
 
     window.jps = instance;
 
-    var canvas = document.getElementById("canvas");
-    var windows = jsPlumb.getSelector(".statemachine-demo .w");
-
     instance.bind("connection", function (info) {
         info.connection.getOverlay("label").setLabel(info.connection.id);
     });
 
-    var initNode = function(el) {
-
-        // initialise draggable elements.
-        instance.draggable(el);
-
-        instance.makeSource(el, {
-            filter: ".ep",
-            anchor: "Continuous",
-            connectorStyle: { stroke: "#5c96bc", strokeWidth: 2, outlineStroke: "transparent", outlineWidth: 4 },
-            connectionType:"basic0.5",
-            extract:{
-                "action":"the-action"
-            },
-            maxConnections: 5,
-            onMaxConnections: function (info, e) {
-                alert("Maximum connections (" + info.maxConnections + ") reached");
-            }
-        });
-
-        instance.makeTarget(el, {
-            dropOptions: { hoverClass: "dragHover" },
-            anchor: "Continuous",
-            allowLoopback: true
-        });
-    };
-
-    var newNode = function(x, y) {
-        var d = document.createElement("div");
-        var id = jsPlumbUtil.uuid();
-        d.className = "w";
-        d.id = id;
-        d.innerHTML = id.substring(0, 7) + "<div class=\"ep\"></div>";
-        d.style.left = x + "px";
-        d.style.top = y + "px";
-        instance.getContainer().appendChild(d);
-        initNode(d);
-        return d;
-    };
-    // suspend drawing and initialise.
     instance.batch(function () {
-        for (var i = 0; i < windows.length; i++) {
-            initNode(windows[i], true);
-        }
         $.getJSON('topology.json', function(data) {
             const topology = new Topology(data, instance);
             topology.configure();
@@ -94,4 +49,3 @@ jsPlumb.ready(function() {
         });
     });
 });
-
