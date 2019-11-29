c28e2a965560dadb52f99a526835f6e10ba30086
Eric Konks <exdevour@gmail.com>
2019-11-29 22:52:28 +0300

    more meaningful name

--- visualizer/src/topology.js
+++ visualizer/src/topology.js
@@ -97,12 +97,12 @@ module.exports = class Topology {
                     for (const targetName of this.modulesWithReadPort(portName, moduleName)) {
                         if (layoutmode) {
                             // Waiting fix : https://github.com/dagrejs/dagre/issues/236
-                            const childrenCheck = (graph, source, target) => {
+                            const haveNoChildren = (graph, source, target) => {
                                 let sourceC = Object.entries(graph._children[source]).length;
                                 let targetC = Object.entries(graph._children[target]).length;
                                 return sourceC === 0 && targetC === 0;
                             }
-                            if (childrenCheck(this.graph, moduleName, targetName)) {
+                            if (haveNoChildren(this.graph, moduleName, targetName)) {
                                 this.graph.setEdge(moduleName, targetName);
                             }
                         } else {
