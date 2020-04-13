6fcaf571cffc6e00b78cbeda88a0efe1c991d4f3
Pavel Kryukov <kryukov@frtk.ru>
2020-04-13 11:18:03 +0300

    Limit Kernel interaction

--- simulator/kernel/base_kernel.h
+++ simulator/kernel/base_kernel.h
@@ -14,16 +14,17 @@
 
 class BaseKernel : public Kernel
 {
-protected:
-    std::unique_ptr<CPUReplicant> sim;
-    std::unique_ptr<FuncMemoryReplicant> mem;
 public:
+    explicit BaseKernel( std::ostream& cerr) : Kernel( cerr) { }
     void set_simulator( const std::shared_ptr<CPUModel>& s) override { sim = std::make_unique<CPUReplicant>( s); }
     void connect_memory( std::shared_ptr<FuncMemory> m) override { mem = std::make_unique<FuncMemoryReplicant>( m); }
     void connect_exception_handler() override { }
     void add_replica_simulator( const std::shared_ptr<CPUModel>& s) override { sim->add_replica( s); }
     void add_replica_memory( const std::shared_ptr<FuncMemory>& s) override { mem->add_replica( s); }
     void load_file( const std::string& name) override;
+protected:
+    std::unique_ptr<CPUReplicant> sim;
+    std::unique_ptr<FuncMemoryReplicant> mem;
 };
 
 #endif // BASE_KERNEL_H
