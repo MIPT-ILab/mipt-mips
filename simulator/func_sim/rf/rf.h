f25787199f91e261253cc78d4295f33cffb9e966
Pavel I. Kryukov <kryukov@frtk.ru>
2020-08-28 23:11:22 +0300

    Clean up register file accumulation

--- simulator/func_sim/rf/rf.h
+++ simulator/func_sim/rf/rf.h
@@ -1,7 +1,7 @@
 /*
  * rf.h - register file
  * @author Pavel Kryukov pavel.kryukov@phystech.edu
- * Copyright 2015-2018 MIPT-MIPS
+ * Copyright 2015-2020 MIPT-MIPS
  */
 
 #ifndef RF_H
@@ -19,16 +19,6 @@ class RF
     using Register = typename FuncInstr::Register;
     using RegisterUInt = typename FuncInstr::RegisterUInt;
 
-    std::array<RegisterUInt, Register::MAX_REG> array = {};
-
-    auto& get_value( Register num) { return array.at( num.to_rf_index()); }
-    const auto& get_value( Register num) const { return array.at( num.to_rf_index()); }
-
-    static uint32 get_carry( uint32 x, uint32 y, int8 accumulation)
-    {
-        return (accumulation == +1 && MAX_VAL32 - x < y) || (accumulation == -1 && x < y) ? 1 : 0;
-    }
-
 public:
     RF() = default;
 
@@ -45,28 +35,16 @@ public:
 
     void write( Register num, RegisterUInt val, RegisterUInt mask)
     {
-        const auto new_val = ( get_value( num) & ~mask) | ( val & mask);
-        write( num, new_val);
+        write( num, apply_mask( num, val, mask));
     }
 
     void write( Register num, RegisterUInt val, RegisterUInt mask, int8 accumulation)
     {
-        if ( accumulation != 0) {
-            assert( num.is_mips_hi() || num.is_mips_lo());
-            const auto old_val = get_value( num);
-            // Handle carry to HI register
-            if ( num.is_mips_lo()) {
-                auto carry = get_carry( narrow_cast<uint32>( old_val), narrow_cast<uint32>( val), accumulation);
-                write( Register::mips_hi(), carry, mask, accumulation);
-            }
+        const auto [new_val, do_carry] = accumulate( num, val, accumulation);
+        if ( num.is_mips_lo() && do_carry)
+            carry( mask, accumulation);
 
-            if (accumulation == 1)
-                val = narrow_cast<uint32>(old_val) + narrow_cast<uint32>(val);
-            else
-                val = narrow_cast<uint32>(old_val) - narrow_cast<uint32>(val);
-        }
-
-        write( num, val, mask);
+        write( num, new_val, mask);
     }
 
     inline void read_source( FuncInstr* instr, size_t index) const
@@ -82,8 +60,44 @@ public:
 
     inline void write_dst( const FuncInstr& instr)
     {
-        write( instr.get_dst( 0), instr.get_v_dst( 0),  instr.get_mask(), instr.get_accumulation_type());
+        write( instr.get_dst( 0), instr.get_v_dst( 0), instr.get_mask(), instr.get_accumulation_type());
         write( instr.get_dst( 1), instr.get_v_dst( 1), all_ones<RegisterUInt>(), instr.get_accumulation_type());
+    }
+
+private:
+    std::array<RegisterUInt, Register::MAX_REG> array = {};
+
+    auto& get_value( Register num) { return array.at( num.to_rf_index()); }
+    const auto& get_value( Register num) const { return array.at( num.to_rf_index()); }
+
+    RegisterUInt apply_mask( Register num, RegisterUInt val, RegisterUInt mask) const
+    {
+        return (get_value( num) & ~mask) | ( val & mask);
+    }
+
+    static std::pair<RegisterUInt, bool> addition( uint32 x, uint32 y)
+    {
+        return std::pair{ x + y, MAX_VAL32 - x < y};
+    }
+
+    static std::pair<RegisterUInt, bool> subtraction( uint32 x, uint32 y)
+    {
+        return std::pair{ x - y, x < y};
+    }
+
+    std::pair<RegisterUInt, bool> accumulate( Register num, RegisterUInt val, int8 accumulation) const
+    {
+        switch (accumulation) {
+        case +1: return addition( narrow_cast<uint32>( get_value( num)), narrow_cast<uint32>( val));
+        case -1: return subtraction( narrow_cast<uint32>( get_value( num)), narrow_cast<uint32>( val));
+        default: return std::pair{val, false};
+        }
+    }
+
+    void carry( RegisterUInt mask, int8 accumulation)
+    {
+        auto num = Register::mips_hi();
+        write( num, accumulate( num, 1, accumulation).first, mask);
     }
 };
 
