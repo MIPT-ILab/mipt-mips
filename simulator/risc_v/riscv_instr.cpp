1465819ae685034e5afd17644e35869c763e2565
GELGOOG-STUTZER <anton.okley@mail.ru>
2020-11-04 01:33:24 +0300

    max and maxu instructions

--- simulator/risc_v/riscv_instr.cpp
+++ simulator/risc_v/riscv_instr.cpp
@@ -90,7 +90,7 @@ template<typename I> const auto execute_ctz = ALU::ctz<I, typename I::RegisterUI
 template<typename I> const auto execute_gorc = ALU::gorc<I>;
 template<typename I> const auto execute_grev = ALU::grev<I>;
 template<typename I> const auto execute_max = ALU::max<I>;
-template<typename I> const auto execute_maxu = ALU::maxu<I>;
+template<typename I> const auto execute_max = ALU::maxu<I>;
 template<typename I> const auto execute_orn = ALU::orn<I>;
 template<typename I> const auto execute_pack = ALU::pack<I, typename I::RegisterUInt>;
 template<typename I> const auto execute_pcnt = ALU::pcnt<I, typename I::RegisterUInt>;
@@ -325,7 +325,7 @@ static const std::vector<RISCVTableEntry<I>> cmd_desc =
     {'B', instr_clmul,      execute_clmul<I>,  OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_gorc,       execute_gorc<I>,   OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_max,        execute_max<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
-    {'B', instr_maxu,       execute_maxu<I>,   OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
+    {'B', instr_maxu,        execute_max<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_unshfl,     execute_unshfl<I>, OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64 | 128}, // NOLINT(hicpp-signed-bitwise) https://bugs.llvm.org/show_bug.cgi?id=44977
 };
 
--- simulator/risc_v/t/unit_test.cpp
+++ simulator/risc_v/t/unit_test.cpp
@@ -287,19 +287,18 @@ TEST_CASE( "RISV RV64 xnor")
 
 TEST_CASE( "RISV RV32 max")
 {
-    CHECK( RISCVInstr<uint32>(0xb2858b3).get_disasm() == "pack $a7, $a6, $a8");
+    CHECK( RISCVInstr<uint32>(0x0ae7d7b3).get_disasm() == "max $a5, $a5, $a4");
     RISCVInstr<uint32> instr( "max", 0);
-    instr.set_v_src( 0xffff'ffff, 0);
-    instr.set_v_src( 0x1111'1111, 1);
+    instr.set_v_src( 0x1111'1111, 0);
+    instr.set_v_src( 0xf111'111f, 1);
     instr.execute();
     CHECK( instr.get_v_dst( 0) == 0x1111'1111);
-
 }
 
 TEST_CASE( "RISV RV64 max")
 {
     RISCVInstr<uint64> instr( "max", 0);
-    instr.set_v_src( 0xffff'ffff'2222'2222, 0);
+    instr.set_v_src( 0xffff'ffff'ffff'ffff, 0);
     instr.set_v_src( 0x1111'1111'3333'3333, 1);
     instr.execute();
     CHECK( instr.get_v_dst( 0) == 0x1111'1111'3333'3333);
@@ -307,7 +306,7 @@ TEST_CASE( "RISV RV64 max")
 
 TEST_CASE( "RISV RV32 maxu")
 {
-    CHECK( RISCVInstr<uint32>(0xb2878b3).get_disasm() == "pack $a7, $a6, $a8");
+    CHECK( RISCVInstr<uint32>(0x0ae7f7b3).get_disasm() == "maxu $a5, $a5, $a4");
     RISCVInstr<uint32> instr( "maxu", 0);
     instr.set_v_src( 0xffff'ffff, 0);
     instr.set_v_src( 0x1111'1111, 1);
@@ -324,7 +323,6 @@ TEST_CASE( "RISV RV64 maxu")
     instr.execute();
     CHECK( instr.get_v_dst( 0) == 0xffff'ffff'2222'2222);
 }
-
 
 TEST_CASE( "RISCV sro32")
 {
