aa7b66e4715467fe58adf15d4d573901ae85abba
pr0kuk <shcherbakov.aa@phystech.edu>
2020-11-03 13:34:16 +0300

    Fixed 2 simple mistakes

--- simulator/risc_v/riscv_instr.cpp
+++ simulator/risc_v/riscv_instr.cpp
@@ -323,7 +323,7 @@ static const std::vector<RISCVTableEntry<I>> cmd_desc =
     {'B', instr_rol,        execute_rol<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_clmul,      execute_clmul<I>,  OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_gorc,       execute_gorc<I>,   OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
-    {'B', instr_gorci,      execute_gorci<I>,  OUT_ARITHM, ' ', Imm::ARITH,{ Src::RS1, Src::NO }, { Dst::RD }, 0, 32 | 64      },
+    {'B', instr_gorci,      execute_gorci<I>,  OUT_ARITHM, ' ', Imm::ARITH,{ Src::RS1, Src::ZERO }, { Dst::RD }, 0, 32 | 64    },
     {'B', instr_unshfl,     execute_unshfl<I>, OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64 | 128}, // NOLINT(hicpp-signed-bitwise) https://bugs.llvm.org/show_bug.cgi?id=44977
 };
 
