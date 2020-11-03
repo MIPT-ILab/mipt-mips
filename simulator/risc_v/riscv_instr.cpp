ba59cf09a39068516fd8d7edbf99cee2dcafb080
GELGOOG-STUTZER <anton.okley@mail.ru>
2020-11-04 02:20:42 +0300

    other files

--- simulator/risc_v/riscv_instr.cpp
+++ simulator/risc_v/riscv_instr.cpp
@@ -89,6 +89,8 @@ template<typename I> const auto execute_clz = ALU::clz<I, typename I::RegisterUI
 template<typename I> const auto execute_ctz = ALU::ctz<I, typename I::RegisterUInt>;
 template<typename I> const auto execute_gorc = ALU::gorc<I>;
 template<typename I> const auto execute_grev = ALU::grev<I>;
+template<typename I> const auto execute_max = ALU::max<I>;
+template<typename I> const auto execute_max = ALU::maxu<I>;
 template<typename I> const auto execute_orn = ALU::orn<I>;
 template<typename I> const auto execute_pack = ALU::pack<I, typename I::RegisterUInt>;
 template<typename I> const auto execute_pcnt = ALU::pcnt<I, typename I::RegisterUInt>;
@@ -98,8 +100,6 @@ template<typename I> const auto execute_slo = ALU::slo<I>;
 template<typename I> const auto execute_sro = ALU::sro<I>;
 template<typename I> const auto execute_unshfl = ALU::riscv_unshfl<I>;
 template<typename I> const auto execute_xnor = ALU::xnor<I>;
-template<typename I> const auto execute_max = ALU::max<I>;
-template<typename I> const auto execute_max = ALU::maxu<I>;
 
 using Src = Reg;
 using Dst = Reg;
@@ -324,8 +324,9 @@ static const std::vector<RISCVTableEntry<I>> cmd_desc =
     {'B', instr_rol,        execute_rol<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_clmul,      execute_clmul<I>,  OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_gorc,       execute_gorc<I>,   OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
+
     {'B', instr_max,        execute_max<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
-    {'B', instr_maxu,        execute_max<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
+    {'B', instr_maxu,       execute_max<I>,    OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64      },
     {'B', instr_unshfl,     execute_unshfl<I>, OUT_ARITHM, ' ', Imm::NO, { Src::RS1, Src::RS2 },  { Dst::RD }, 0, 32 | 64 | 128}, // NOLINT(hicpp-signed-bitwise) https://bugs.llvm.org/show_bug.cgi?id=44977
 };
 
