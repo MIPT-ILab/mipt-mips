daffa5362719dbc57efb11a6e82f2e1763657cc4
GELGOOG-STUTZER <anton.okley@mail.ru>
2020-11-04 15:08:44 +0300

    max works properly (finally 2)

--- simulator/func_sim/alu.h
+++ simulator/func_sim/alu.h
@@ -240,7 +240,7 @@ struct ALU
     template<typename I> static 
     void max( I* instr)
     { 
-        instr->v_dst[0] = narrow_cast<typename I::RegisterSInt>( instr->v_src[0]) >= narrow_cast<typename I::RegisterSInt>( instr->v_src[1]) ? instr->v_src[0] : instr->v_src[1];
+        instr->v_dst[0] = instr->v_src[ge( instr) ? 0 : 1];
     }
     
     template<typename I> static 
