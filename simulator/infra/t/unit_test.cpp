b804601be1ccf87c00bab65b08efee6ca9f44b68
Pavel I. Kryukov <kryukov@frtk.ru>
2020-11-20 13:32:38 +0300

    Enhance bit manipulating macro tests

--- simulator/infra/t/unit_test.cpp
+++ simulator/infra/t/unit_test.cpp
@@ -105,6 +105,30 @@ static_assert(count_leading_zeroes<uint8>(uint8{ 0xFF}) == 0);
 static_assert(count_leading_zeroes<uint32>(~uint32{ 0}) == 0);
 static_assert(count_leading_zeroes<uint64>(~uint64{ 0}) == 0);
 
+TEST_CASE("count_leading for 128 bit")
+{
+    CHECK( count_leading_zeroes<uint128>( 0) == 128);
+    CHECK( count_leading_zeroes<uint128>( 0xFF) == 120);
+    CHECK( count_leading_zeroes<uint128>( all_ones<uint128>()) == 0);
+    CHECK( count_leading_zeroes<uint128>( msb_set<uint128>()) == 0);
+    CHECK( count_leading_zeroes<uint128>( bitmask<uint128>( 65)) == 63);
+
+    CHECK( count_leading_ones<uint128>( 0) == 0);
+    CHECK( count_leading_ones<uint128>( 0xFF) == 0);
+    CHECK( count_leading_ones<uint128>( all_ones<uint128>()) == 128);
+    CHECK( count_leading_ones<uint128>( msb_set<uint128>()) == 1);
+    CHECK( count_leading_ones<uint128>( ~bitmask<uint128>( 65)) == 63);
+}
+
+TEST_CASE("count_trailing for 128 bit")
+{
+    CHECK( count_trailing_zeroes<uint128>( 0) == 128);
+    CHECK( count_trailing_zeroes<uint128>( 0xFF) == 0);
+    CHECK( count_trailing_zeroes<uint128>( all_ones<uint128>()) == 0);
+    CHECK( count_trailing_zeroes<uint128>( msb_set<uint128>()) == 127);
+    CHECK( count_trailing_zeroes<uint128>( ~bitmask<uint128>( 65)) == 65);
+}
+
 static_assert(find_first_set<uint64>(0) == 64);
 static_assert(find_first_set<uint32>(0) == 32);
 static_assert(find_first_set<uint8>(0) == 8);
@@ -170,12 +194,6 @@ TEST_CASE( "Byte swapping pointer access")
 static constexpr std::array<const char*, 4> some_argv = {"rm", "-rf", "/", nullptr};
 static_assert( count_argc( some_argv.data()) == 3);
 
-/* Boost cannot instantiate count_leading_zeroes in constexpr context
-static_assert(count_leading_zeroes<uint128>(0x0) == 128);
-static_assert(count_leading_zeroes<uint128>(0xFF) == 120);
-static_assert(count_leading_zeroes<uint128>(~0x0) == 0);
-*/
-
 static_assert( ones_ls<uint64>( 0x1, 1) == 0x3);
 static_assert( ones_ls<uint64>( 0x8, 4) == 0x8f);
 static_assert( ones_ls<uint64>( msb_set<uint64>(), 1) == 0x1);
@@ -216,6 +234,19 @@ TEST_CASE("ones shift dynamic check")
 }
 
 TEST_CASE("ones shift for 128 instructions")
+{
+    CHECK( ones_ls<uint128>( 0x1, 1) == 0x3);
+    CHECK( ones_ls<uint128>( 0x8, 4) == 0x8f);
+    CHECK( ones_ls<uint128>( msb_set<uint128>(), 1) == 0x1);
+
+    const size_t shift = 128 - 17;
+    CHECK( ones_rs( msb_set<uint128>() >> 1, 15) == (uint128{ 0x1fffd} << shift));
+    CHECK( ones_rs( msb_set<uint128>(), 16)      == (uint128{ 0x1ffff} << shift));
+    CHECK( arithmetic_rs( msb_set<uint128>() >> 1, 15) == (uint128{ 1}       << shift));
+    CHECK( arithmetic_rs( msb_set<uint128>(), 16)      == (uint128{ 0x1ffff} << shift));
+}
+
+TEST_CASE("circ shift for 128")
 {
     CHECK( ones_ls<uint128>( 0x1, 1) == 0x3);
     CHECK( ones_ls<uint128>( 0x8, 4) == 0x8f);
@@ -277,37 +308,28 @@ TEST_CASE("Find first set")
     CHECK( find_first_set( *val) == bitwidth<unsigned> );
 }
 
-TEST_CASE("Test uint32 circular left shift")
+static_assert( circ_ls<uint32>( 0xA0B0'C0D7, 0)  == 0xA0B0'C0D7);
+static_assert( circ_ls<uint32>( 0xA0B0'C0D7, 32) == 0xA0B0'C0D7);
+static_assert( circ_ls<uint32>( 0xA0B0'C0D7, 4)  == 0x7B0C'0D0A);
+
+static_assert( circ_ls<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 0)  == 0xA0B0'C0D0'A0B0'C0D7);
+static_assert( circ_ls<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 32) == 0xA0B0'C0D0'A0B0'C0D7);
+static_assert( circ_ls<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 4)  == 0x7B0C'0D0A'0B0C'0D0A);
+
+static_assert( circ_rs<uint32>( 0xA0B0'C0D7, 0)  == 0xA0B0'C0D7);
+static_assert( circ_rs<uint32>( 0xA0B0'C0D7, 32) == 0xA0B0'C0D7);
+static_assert( circ_rs<uint32>( 0xA0B0'C0D7, 4)  == 0x0B0C'0D7A);
+
+static_assert( circ_rs<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 0)  == 0xA0B0'C0D0'A0B0'C0D7);
+static_assert( circ_rs<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 32) == 0xA0B0'C0D0'A0B0'C0D7);
+static_assert( circ_rs<uint64>( 0xA0B0'C0D0'A0B0'C0D7, 4)  == 0x0B0C'0D7A'0B0C'0D7A);
+
+TEST_CASE("circular right shift for 128 bit")
 {
-    uint32 value = 0xA0B0'C0D0;
-    CHECK( value == circ_ls( value, 0));
-    CHECK( value == circ_ls( value, 32));
-    CHECK( 0x0B0C'0D0A == circ_ls( value, 4));
+    CHECK( unpack_to<uint64>( circ_rs<uint128>( 0xABCD, 8))[0] == 0xCD00'0000'0000'000);
+    CHECK( unpack_to<uint64>( circ_rs<uint128>( 0xABCD, 8))[1] == 0xAB);
 }
 
-TEST_CASE("Test uint64 circular left shift")
-{
-    uint64 value = 0xA0B0'C0D0'A0B0'C0D0;
-    CHECK( value == circ_ls( value, 0));
-    CHECK( value == circ_ls( value, 64));
-    CHECK( 0x0B0C'0D0A'0B0C'0D0A == circ_ls( value, 4));
-}
-
-TEST_CASE("Test uint32 circular right shift")
-{
-    uint32 value = 0xA0B0'C0D0;
-    CHECK( value == circ_rs( value, 0));
-    CHECK( value == circ_rs( value, 32));
-    CHECK( 0x0A0B'0C0D == circ_rs( value, 4));
-}
-
-TEST_CASE("Test uint64 circular right shift")
-{
-    uint64 value = 0xA0B0'C0D0'A0B0'C0D0;
-    CHECK( value == circ_rs( value, 0));
-    CHECK( value == circ_rs( value, 64));
-    CHECK( 0x0A0B'0C0D'0A0B'0C0D == circ_rs( value, 4));
-}
 TEST_CASE("Invalid target print")
 {
     std::ostringstream oss;
