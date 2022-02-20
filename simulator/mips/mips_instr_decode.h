bc60173f42a3e91b14dd1d257422d6b4b3894b35
Pavel I. Kryukov <kryukov@frtk.ru>
2022-02-20 17:05:53 +0300

    Disable Clang-Tidy check since C++20 allows signed bitwise operations (#1532)

--- simulator/mips/mips_instr_decode.h
+++ simulator/mips/mips_instr_decode.h
@@ -55,7 +55,6 @@ struct MIPSInstrDecoder
 
     static constexpr uint32 apply_mask(uint32 bytes, uint32 mask) noexcept
     {
-        // NOLINTNEXTLINE(hicpp-signed-bitwise) rhs must be positive
         return ( bytes & mask) >> std::countr_zero( mask);
     }
 
--- simulator/risc_v/riscv_decoder.h
+++ simulator/risc_v/riscv_decoder.h
@@ -263,7 +263,6 @@ struct RISCVInstrDecoder
 
     static constexpr uint32 apply_mask(uint32 bytes, uint32 mask) noexcept
     {
-        // NOLINTNEXTLINE(hicpp-signed-bitwise) rhs must be positive
         return ( bytes & mask) >> std::countr_zero( mask);
     }
 
