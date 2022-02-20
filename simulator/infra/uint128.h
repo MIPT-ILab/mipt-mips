995880d957c5102c75e4231cf4e39f5c1457bb62
Pavel I. Kryukov <kryukov@frtk.ru>
2022-02-20 15:35:42 +0300

    Use 256-bit integers for multiplication (#1529)

--- simulator/infra/uint128.h
+++ simulator/infra/uint128.h
@@ -17,15 +17,25 @@
 using int128  = boost::multiprecision::int128_t;
 using uint128 = boost::multiprecision::uint128_t;
 
+using int256  = boost::multiprecision::int256_t;
+using uint256 = boost::multiprecision::uint256_t;
+
 template<> inline constexpr bool is_wide_integer_v<int128> = true;
 template<> inline constexpr bool is_wide_integer_v<uint128> = true;
+template<> inline constexpr bool is_wide_integer_v<int256> = true;
+template<> inline constexpr bool is_wide_integer_v<uint256> = true;
 
 template<> inline constexpr bool is_wide_signed_integer_v<int128> = true;
+template<> inline constexpr bool is_wide_signed_integer_v<int256> = true;
 
 template<> struct unsigned_integer<128> { using type = uint128; };
 template<> struct signed_integer<128>   { using type = int128; };
+template<> struct unsigned_integer<256> { using type = uint256; };
+template<> struct signed_integer<256>   { using type = int256; };
 template<> inline constexpr size_t bitwidth<uint128> = 128U;
 template<> inline constexpr size_t bitwidth<int128> = 128U;
+template<> inline constexpr size_t bitwidth<uint256> = 256U;
+template<> inline constexpr size_t bitwidth<int256> = 256U;
 
 inline int128 signify( uint128 value)
 {
